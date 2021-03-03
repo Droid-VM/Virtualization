d997a1be4efc5465b75418f2b968ab9d1a6298a0
Victor Hsieh <victorhsieh@google.com>
2021-03-03 15:53:49 -0800

    authfs: Support write config/operation in fuse
    Bug: 171279640
    Test: atest
    Test: tools/device-test.sh
    
    Change-Id: Ic611f72d51a5522d9ec6e6fdc82c115b5782c4ac

--- authfs/src/file/remote_file.rs
+++ authfs/src/file/remote_file.rs
@@ -98,7 +98,6 @@ pub struct RemoteFileEditor {
 }
 
 impl RemoteFileEditor {
-    #[allow(dead_code)]
     pub fn new(service: Arc<Mutex<VirtFdService>>, file_fd: i32) -> Self {
         RemoteFileEditor { service, file_fd }
     }
