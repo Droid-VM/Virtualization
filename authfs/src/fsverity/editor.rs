f2390c7c29f2c8eaf7ff2436eedb76299c12d3f3
Victor Hsieh <victorhsieh@google.com>
2021-03-03 16:00:55 -0800

    authfs: Reorganize modules & rename flags/classes/vars
    This change adjusts the module/directory layout to make it easier to
    reason.
    
     - File "transports" are now under file/. Common traits are defined in
       file.rs.
    
     - All fs-verity related modules are now under fsverity/. This includes
       VerifiedFileReader, which implements traits in the file module to
       provide transparent verification.
    
     - Rename structs for better consistency.  Drop "Chunked" for
       simplicity.
    
        ChunkedFileReader          ->  LocalFileReader
        RemoteChunkedFileReader    ->  RemoteFileReader
        FsverityChunkedFileReader  ->  VerifiedFileReader
    
     - Move and rename get_local_service from remote_file.rs to file.rs.
    
    Also, rename command line flags and related class/field names in main.rs
    so that later the name of new read-writable file can fit in more consistently.
    
    New layours:
      src/
      |-- auth.rs
      |-- common.rs
      |-- crypto.hpp
      |-- crypto.rs
      |-- file.rs
      |-- file
      |   |-- local_file.rs
      |   `-- remote_file.rs
      |-- fsverity.rs
      |-- fsverity
      |   |-- builder.rs
      |   |-- common.rs
      |   |-- sys.rs
      |   |-- verifier.rs
      |   `-- editor.rs
      |-- fusefs.rs
      `-- main.rs
    
    Bug: 171279640
    Test: atest
    
    Change-Id: Ib257a37df89c6b813f4d97978678db3483d28b57

--- authfs/src/fsverity/editor.rs
+++ authfs/src/fsverity/editor.rs
@@ -57,36 +57,13 @@ use std::sync::{Arc, RwLock};
 use super::builder::MerkleLeaves;
 use crate::common::{ChunkedSizeIter, CHUNK_SIZE};
 use crate::crypto::{CryptoError, Sha256Hash, Sha256Hasher};
-use crate::reader::ReadOnlyDataByChunk;
+use crate::file::{RandomWrite, ReadOnlyDataByChunk};
 
 // Implement the conversion from `CryptoError` to `io::Error` just to avoid manual error type
 // mapping below.
 impl From<CryptoError> for io::Error {
     fn from(error: CryptoError) -> Self {
         io::Error::new(io::ErrorKind::Other, error)
-    }
-}
-
-/// A trait to write a buffer to the destination at a given offset. The implementation does not
-/// necessarily own or maintain the destination state.
-///
-/// NB: The trait is required in a member of `fusefs::AuthFs`, which is required to be Sync and
-/// immutable (this the member).
-pub trait RandomWrite {
-    /// Writes `buf` to the destination at `offset`. Returns the written size, which may not be the
-    /// full buffer.
-    fn write_at(&self, buf: &[u8], offset: u64) -> io::Result<usize>;
-
-    /// Writes the full `buf` to the destination at `offset`.
-    fn write_all_at(&self, buf: &[u8], offset: u64) -> io::Result<()> {
-        let mut input_offset = 0;
-        let mut output_offset = offset;
-        while input_offset < buf.len() {
-            let size = self.write_at(&buf[input_offset..], output_offset)?;
-            input_offset += size;
-            output_offset += size as u64;
-        }
-        Ok(())
     }
 }
 
