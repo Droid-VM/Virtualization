220cbb0c04a86724a6064525e47a8451db8ef3a1
Jiyong Park <jiyong@google.com>
2021-05-13 00:50:23 +0900

    Fix type error
    Bug: 187938225
    Test: m zipfuse on aosp_x86_64
    Change-Id: I14fe48a09a2b0f204e0a4e5c9e6cb012a7971792

--- zipfuse/src/main.rs
+++ zipfuse/src/main.rs
@@ -128,10 +128,8 @@ impl ZipFuse {
         let inode_data = self.find_inode(inode)?;
         let mut st = unsafe { std::mem::MaybeUninit::<libc::stat64>::zeroed().assume_init() };
         st.st_dev = 0;
-        st.st_nlink = if inode_data.is_dir() {
-            // 2 is for . and ..
-            // unwrap is safe because of the `is_dir` check.
-            2 + inode_data.get_directory().unwrap().len() as libc::nlink_t
+        st.st_nlink = if let Some(directory) = inode_data.get_directory() {
+            (2 + directory.len() as libc::nlink_t).into()
         } else {
             1
         };
