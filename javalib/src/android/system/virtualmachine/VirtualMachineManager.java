5f47e098bc413d7dd7b28b2d43e113bf9ac68538
Jiyong Park <jiyong@google.com>
2021-07-01 12:55:14 +0900

    Add APIs for creating and running a virtual machine
    Bug: 183496040
    Test: atest MicrodroidHostTestCase
    Test: TARGET_BUILD_APPS=MicrodroidTestApp.signed
    Push the build MicrodroidTestApp.apk.idsig to /data/local/tmp/virt
    Install the APK and run it.
    
    Change-Id: I1614f8c83605a32ef34b62216c13cb4e4f7fcf99

---
+++ javalib/src/android/system/virtualmachine/VirtualMachineManager.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.system.virtualmachine;
+
+import android.content.Context;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Manages {@link VirtualMachine} objects created for an application.
+ *
+ * @hide
+ */
+public class VirtualMachineManager {
+    private final Context mContext;
+
+    private VirtualMachineManager(Context context) {
+        mContext = context;
+    }
+
+    static Map<Context, VirtualMachineManager> sInstances = new HashMap<>();
+
+    /** Returns the per-context instance. */
+    public static VirtualMachineManager getInstance(Context context) {
+        synchronized (sInstances) {
+            if (!sInstances.containsKey(context)) {
+                sInstances.put(context, new VirtualMachineManager(context));
+            }
+            return sInstances.get(context);
+        }
+    }
+
+    /**
+     * Creates a new {@link VirtualMachine}. Every call to this creates a new (and different)
+     * virtual machine even if the name and the config are the same.
+     */
+    public VirtualMachine create(String name, VirtualMachineConfig config)
+            throws VirtualMachineException {
+        VirtualMachine vm = VirtualMachine.create(mContext, name, config);
+        return vm;
+    }
+
+    /** Returns an existing {@link VirtualMachine} with the given name. */
+    public VirtualMachine get(String name) throws VirtualMachineException {
+        VirtualMachine vm = VirtualMachine.load(mContext, name);
+        return vm;
+    }
+
+    private static final Object sNameLock = new Object();
+
+    /** Returns an existing {@link VirtualMachine} if it exists, or create a new one. */
+    public VirtualMachine getOrCreate(String name, VirtualMachineConfig config)
+            throws VirtualMachineException {
+        VirtualMachine vm;
+        synchronized (sNameLock) {
+            vm = get(name);
+            if (vm == null) {
+                return create(name, config);
+            }
+        }
+
+        if (vm.getConfig().equals(config)) {
+            return vm;
+        } else {
+            throw new VirtualMachineException();
+        }
+    }
+}
