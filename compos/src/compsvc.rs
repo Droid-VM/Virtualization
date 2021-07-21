eedc5de0cab252bfdf33c0d8658a8cf3eebe81f9
Alan Stokes <alanstokes@google.com>
2021-07-21 11:29:10 +0100

    Use CompOsKeyService as compsvc factory.
    Define a Signer trait to encapsulate what we need to do to sign a
    digest.
    
    Modify compsvc to hold a signer.
    
    Modify CompOsKeyService to be able to take in a keyblob and produce a
    signer, then return a compsvc instance holding that signer.
    
    This doesn't yet do anything with the signer. Eventually we will want
    to use it to generate signatures on output artifacts.
    
    Bug: 194267113
    Test: atest ComposHostTestCases (with testOdrefesh un-ignored)
    Change-Id: I72aead0280914987f7c8d1721c1e12ee0fad1af4

--- compos/src/compsvc.rs
+++ compos/src/compsvc.rs
@@ -28,11 +28,8 @@
 //!     - authfs (fd translation)
 //!     - actual task
 
+use crate::signer::Signer;
 use anyhow::Result;
-use log::error;
-use minijail::{self, Minijail};
-use std::path::PathBuf;
-
 use compos_aidl_interface::aidl::com::android::compos::ICompService::{
     BnCompService, ICompService,
 };
@@ -40,6 +37,10 @@ use compos_aidl_interface::aidl::com::android::compos::Metadata::Metadata;
 use compos_aidl_interface::binder::{
     BinderFeatures, Interface, Result as BinderResult, Status, StatusCode, Strong,
 };
+use log::error;
+use minijail::{self, Minijail};
+use std::path::PathBuf;
+use std::sync::Arc;
 
 const WORKER_BIN: &str = "/apex/com.android.compos/bin/compsvc_worker";
 
@@ -50,12 +51,22 @@ pub struct CompService {
     worker_bin: PathBuf,
     task_bin: String,
     debuggable: bool,
+    #[allow(dead_code)] // TODO: Make use of this
+    signer: Option<Arc<dyn Signer>>,
 }
 
 impl CompService {
-    pub fn new_binder(task_bin: String, debuggable: bool) -> Strong<dyn ICompService> {
-        let service =
-            CompService { worker_bin: PathBuf::from(WORKER_BIN.to_owned()), task_bin, debuggable };
+    pub fn new_binder(
+        task_bin: String,
+        debuggable: bool,
+        signer: Option<Arc<dyn Signer>>,
+    ) -> Strong<dyn ICompService> {
+        let service = CompService {
+            worker_bin: PathBuf::from(WORKER_BIN.to_owned()),
+            task_bin,
+            debuggable,
+            signer,
+        };
         BnCompService::new_binder(service, BinderFeatures::default())
     }
 
