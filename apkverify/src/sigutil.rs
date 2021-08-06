bf9e193358bfd052c55b4272c19f02d9820fbcf9
Jooyung Han <jooyung@google.com>
2021-08-06 14:07:49 +0900

    apkverify: add ziputil
    apkverify doesn't need all zip features. ziputil provides zip_section()
    to discover the layout of a zip file.
    
    Bug: 190343842
    Test: cargo test
    Change-Id: Ib6354828cebe819fd3f48265089815ccf29f18ea

--- apkverify/src/sigutil.rs
+++ apkverify/src/sigutil.rs
@@ -21,7 +21,8 @@ use byteorder::{LittleEndian, ReadBytesExt};
 use bytes::{Buf, Bytes};
 use std::io;
 use std::io::Read;
-use zip::spec::CentralDirectoryEnd as EndOfCentralDirectory;
+
+use crate::ziputil::zip_sections;
 
 const APK_SIG_BLOCK_MIN_SIZE: u32 = 32;
 const APK_SIG_BLOCK_MAGIC: u128 = 0x3234206b636f6c4220676953204b5041;
@@ -46,31 +47,20 @@ const CONTENT_DIGEST_VERITY_CHUNKED_SHA256: u32 = 3;
 const CONTENT_DIGEST_SHA256: u32 = 4;
 
 pub struct SignatureInfo {
-    pub signing_block_offset: u32,
     pub signature_block: Bytes,
-    pub eocd_offset: u32,
-    pub eocd: EndOfCentralDirectory,
 }
 
 /// Returns the APK Signature Scheme block contained in the provided file for the given ID
 /// and the additional information relevant for verifying the block against the file.
 pub fn find_signature<F: Read + io::Seek>(f: &mut F, block_id: u32) -> Result<SignatureInfo> {
-    let (eocd, eocd_offset) = EndOfCentralDirectory::find_and_parse(f)?;
-    if eocd.disk_number != eocd.disk_with_central_directory {
-        bail!("Support for multi-disk files is not implemented");
-    }
-    // TODO(jooyung): reject zip64 file
-    let (signing_block, signing_block_offset) =
-        find_signing_block(f, eocd.central_directory_offset)?;
+    let sections = zip_sections(f)?;
+
+    let (signing_block, _signing_block_offset) =
+        find_signing_block(f, sections.central_directory_offset)?;
 
     // TODO(jooyung): propagate NotFound error so that verification can fallback to V2
     let signature_scheme_block = find_signature_scheme_block(signing_block, block_id)?;
-    Ok(SignatureInfo {
-        signing_block_offset,
-        signature_block: signature_scheme_block,
-        eocd_offset: eocd_offset as u32,
-        eocd,
-    })
+    Ok(SignatureInfo { signature_block: signature_scheme_block })
 }
 
 fn find_signing_block<T: Read + io::Seek>(
---
+++ apkverify/src/ziputil.rs
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//! Utilities for zip handling
+
+use anyhow::{bail, Result};
+use byteorder::{LittleEndian, ReadBytesExt};
+use std::io;
+use std::io::Read;
+
+const EOCD_SIGNATURE: u32 = 0x06054b50;
+const EOCD_MIN_SIZE: u64 = 22;
+const EOCD_COMMENT_MAX_SIZE: u16 = ::std::u16::MAX;
+const EOCD_MAX_SIZE: u64 = EOCD_MIN_SIZE + EOCD_COMMENT_MAX_SIZE as u64;
+
+/// EOCD is the center piece to find the layout of a zip file
+struct EndOfCentralDirectory {
+    central_directory_size: u32,
+    central_directory_offset: u32,
+    zip_file_comment_length: u16,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct ZipSections {
+    pub central_directory_offset: u32,
+    pub central_directory_size: u32,
+    pub eocd_offset: u32,
+    pub eocd_size: u32,
+}
+
+impl EndOfCentralDirectory {
+    fn parse<T: Read>(reader: &mut T) -> Result<EndOfCentralDirectory> {
+        let disk_number = reader.read_u16::<LittleEndian>()?;
+        let disk_with_central_directory = reader.read_u16::<LittleEndian>()?;
+        let _number_of_files_on_this_disk = reader.read_u16::<LittleEndian>()?;
+        let _number_of_files = reader.read_u16::<LittleEndian>()?;
+        let central_directory_size = reader.read_u32::<LittleEndian>()?;
+        let central_directory_offset = reader.read_u32::<LittleEndian>()?;
+        let zip_file_comment_length = reader.read_u16::<LittleEndian>()?;
+        let mut zip_file_comment = vec![0; zip_file_comment_length as usize];
+        reader.read_exact(&mut zip_file_comment)?;
+
+        if disk_number == 0xffff {
+            bail!("Support for ZIP64 file is not implemented");
+        }
+        if disk_number != disk_with_central_directory {
+            bail!("Support for multi-disk files is not implemented");
+        }
+
+        Ok(EndOfCentralDirectory {
+            central_directory_size,
+            central_directory_offset,
+            zip_file_comment_length,
+        })
+    }
+}
+
+/// Discover the layout of a zip file.
+pub fn zip_sections<T: Read + io::Seek>(reader: &mut T) -> Result<ZipSections> {
+    let file_length = reader.seek(io::SeekFrom::End(0))?;
+    if file_length < EOCD_MIN_SIZE {
+        bail!("Invalid zip header");
+    }
+
+    let search_upper_bound = file_length.saturating_sub(EOCD_MAX_SIZE);
+    let mut pos = file_length - EOCD_MIN_SIZE;
+    while pos >= search_upper_bound {
+        reader.seek(io::SeekFrom::Start(pos as u64))?;
+        if reader.read_u32::<LittleEndian>()? == EOCD_SIGNATURE {
+            let eocd = EndOfCentralDirectory::parse(reader)?;
+            return Ok(ZipSections {
+                central_directory_offset: eocd.central_directory_offset,
+                central_directory_size: eocd.central_directory_size,
+                eocd_offset: pos as u32,
+                eocd_size: EOCD_MIN_SIZE as u32 + eocd.zip_file_comment_length as u32,
+            });
+        }
+        pos = match pos.checked_sub(1) {
+            Some(p) => p,
+            None => break,
+        };
+    }
+    bail!("Could not find central directory end")
+}
