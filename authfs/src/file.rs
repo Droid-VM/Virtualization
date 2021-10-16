63339e8e48f10833af2d2befae406d0bded47c33
Victor Hsieh <victorhsieh@google.com>
2021-10-15 17:52:51 -0700

    authfs: support remote output directory
    This change contains 3 major groups:
     - authfs/{aidl, fd_server}: new AIDL API and the service implementation
     - authfs/src: implement FUSE APIs for creating directory and file, by
                   interact with the new service API as a client
     - authfs/tests, tests/: test coverage
    
    A few notable changes that might help reviewing:
    
     - Now that both AuthFs and FdService struct is no longer immutable (in
       order to allow writable directory), their BTreeMap are now guarded by
       Arc<Mutex<_>>.
    
       * AuthFs::insert_new_inode and FdService::insert_new_fd are designed
         specifically to allow querying then mutating the map, which isn't
         trivial.
    
     - File and directory modes from the user program / VFS are currently
       ignored (just not to grow the change size).
    
     - Some shuffling of test paths to make it easy to clean up in tearDown.
    
    Bug: 203251769
    Test: AuthFsHostTest
    Change-Id: I50f3f1ba8a3ebd969cf0f25a8feab2ec8cb1a2dc

--- authfs/src/file.rs
+++ authfs/src/file.rs
@@ -1,5 +1,7 @@
+mod remote_dir;
 mod remote_file;
 
+pub use remote_dir::RemoteDirEditor;
 pub use remote_file::{RemoteFileEditor, RemoteFileReader, RemoteMerkleTreeReader};
 
 use binder::unstable_api::{new_spibinder, AIBinder};
--- tests/hostside/helper/java/android/virt/test/VirtualizationTestCaseBase.java
+++ tests/hostside/helper/java/android/virt/test/VirtualizationTestCaseBase.java
@@ -152,6 +152,12 @@ public abstract class VirtualizationTestCaseBase extends BaseHostJUnit4Test {
                 .runTimedCmd(timeout, "adb", "-s", MICRODROID_SERIAL, "shell", join(cmd));
     }
 
+    // Returns whether the command has completed successfully on Microdroid.
+    public static boolean hasSucceededOnMicrodroid(String... cmd) {
+        CommandResult result = runOnMicrodroidForResult(cmd);
+        return result.getStatus() == CommandStatus.SUCCESS;
+    }
+
     private static String join(String... strs) {
         return String.join(" ", Arrays.asList(strs));
     }
