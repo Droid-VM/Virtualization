f694d6e54cac11a7e0c9904af95b5fb5532a76f4
Alan Stokes <alanstokes@google.com>
2021-10-18 12:30:31 +0100

    Add a system server JAR
    Add a Java system server classpath fragment to wrap access to
    composd. Provide initially empty SystemService and JobService classes.
    
    Bug: 199147668
    Test: manual
    Change-Id: I5b2bb2f4c9b9086cca85983d0a659ea92409f6bf

--- apex/product_packages.mk
+++ apex/product_packages.mk
@@ -25,4 +25,10 @@ PRODUCT_PACKAGES += \
 
 # TODO(b/207336449): Figure out how to get these off /system
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-     system/lib64/libgfxstream_backend.so \
+    system/lib64/libgfxstream_backend.so \
+    system/framework/oat/%@service-compos.jar@classes.odex \
+    system/framework/oat/%@service-compos.jar@classes.vdex \
+
+PRODUCT_APEX_SYSTEM_SERVER_JARS += com.android.compos:service-compos
+
+PRODUCT_SYSTEM_PROPERTIES += ro.config.isolated_compilation_enabled=true
