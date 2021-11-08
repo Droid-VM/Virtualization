0f1db7088740e113ee2333fb1ce04b6c5235ad15
Jooyung Han <jooyung@google.com>
2021-11-08 17:50:22 +0900

    Add sign_virt_apex_test
    This test re-signs the virt apex contents and then check if they are
    signed with the new key. Note that this isn't an end-to-end test to
    install re-signed apex and see if it works, but only checks avbtool
    signable items are signed with the new key by comparing pubkey digest.
    This will help to notice when adding a new signed item without modifying
    sign_virt_apex though.
    
    Bug: 193504286
    Test: atest sign_virt_apex_test
    Change-Id: I16098c49f41e14f571dca272310b81459d9803a8

---
+++ apex/sign_virt_apex_test.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2021 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+shopt -s extglob
+
+TMP_ROOT=$(mktemp -d -t sign_virt_apex-XXXXXXXX)
+TEST_DIR=$(dirname $0)
+
+# To access host tools
+PATH=$TEST_DIR:$PATH
+DEBUGFS=$TEST_DIR/debugfs_static
+
+deapexer --debugfs_path $DEBUGFS extract $TEST_DIR/com.android.virt.apex $TMP_ROOT
+
+sign_virt_apex $TEST_DIR/test.com.android.virt.pem $TMP_ROOT
+
+mkdir -p $TMP_ROOT/extracted
+
+sign_virt_apex --verify $TEST_DIR/test.com.android.virt.pem $TMP_ROOT/extracted
