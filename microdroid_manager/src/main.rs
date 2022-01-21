9d43e4bb3b39d92f5885ec2ba5049c8b00cafc60
Andrew Scull <ascull@google.com>
2022-01-21 22:20:28 +0000

    Remove keystore and keymint from microdroid
    Keystore has been replaced by diced and can now be removed.
    
    Bug: 215747811
    Change-Id: Id390e39558d93f4922481bc214a0c01a3a6c93ef

--- microdroid_manager/src/main.rs
+++ microdroid_manager/src/main.rs
@@ -182,11 +182,6 @@ fn try_run_payload(service: &Strong<dyn IVirtualMachineService>) -> Result<i32> 
     }
     mount_extra_apks(&config)?;
 
-    let fake_secret = "This is a placeholder for a value that is derived from the images that are loaded in the VM.";
-    if let Err(err) = rustutils::system_properties::write("ro.vmsecret.keymint", fake_secret) {
-        warn!("failed to set ro.vmsecret.keymint: {}", err);
-    }
-
     // Wait until apex config is done. (e.g. linker configuration for apexes)
     // TODO(jooyung): wait until sys.boot_completed?
     wait_for_apex_config_done()?;
