21ccbf517839178bad46aaa5888faf901ff7f8fc
Andrew Walbran <qwandor@google.com>
2022-02-04 12:01:38 +0000

    Temporary hack: Boot protected VMs without pVM firmware.
    Change-Id: I5eacbacf3aca15f380a2b78b53cb65c69c9604c6

--- virtualizationservice/src/crosvm.rs
+++ virtualizationservice/src/crosvm.rs
@@ -265,7 +265,7 @@ fn run_vm(config: CrosvmConfig) -> Result<SharedChild, Error> {
         .arg(config.cid.to_string());
 
     if config.protected {
-        command.arg("--protected-vm");
+        command.arg("--protected-vm-without-firmware");
     }
 
     if let Some(memory_mib) = config.memory_mib {
