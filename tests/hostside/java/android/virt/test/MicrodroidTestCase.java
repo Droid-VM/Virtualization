bc85433a753f9b269a80283d5140e5dfddfe706f
Shikha Panwar <shikhapanwar@google.com>
2022-03-24 08:54:43 +0000

    Enable exporting of tombstones out of guest VMs
    Microdroid init will start the tombstone_transmit service which will
    connect to a server (thread of virtualization service).
    
    virtualizationservice listens for incoming connections from guests VMs.
    The client (tombstone_transmit in  guest) sends each tombstone file
    content through a separate stream.
    
    For each received connection, handle_tombstone would send a connect
    request to tombstoned(via libtombstoned_client_rust), which provide a
    file  to write the content in (actually managed by the client libary).
    Once write is complete notify_completion() would send completion
    notification to tombstoned (which then will close the file, rename it).
    
    Test: I have written the hostside test which creates a test file in
    /data/tombstones/ in VM & checks logcat for whether tombstones are sent
    and recived on respective side. Other than that, for real crash, I
    manually killed random proceses inside VM (created by microdroid demo
    app) and checked that `adb bugreport` contains the respective tombstone.
    
    Bug: 202153827
    Change-Id: Ifcaa5da968ef39fdd05612d3e0baca4fd1c5eaf1

--- tests/hostside/java/android/virt/test/MicrodroidTestCase.java
+++ tests/hostside/java/android/virt/test/MicrodroidTestCase.java
@@ -22,6 +22,7 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -347,6 +348,42 @@ public class MicrodroidTestCase extends VirtualizationTestCaseBase {
         // Adb connection to the microdroid means that boot succeeded.
         adbConnectToMicrodroid(getDevice(), cid);
         shutdownMicrodroid(getDevice(), cid);
+    }
+
+    @Test
+    public void testTombstonesAreBeingForwarded() throws Exception {
+        // Note this test relies on logcat values being printed by tombstone_transmit on
+        // and the reeceiver on host (virtualization_service)
+        final String configPath = "assets/vm_config.json"; // path inside the APK
+        final String cid =
+                startMicrodroid(
+                        getDevice(),
+                        getBuild(),
+                        APK_NAME,
+                        PACKAGE_NAME,
+                        configPath,
+                        /* debug */ true,
+                        minMemorySize(),
+                        Optional.of(NUM_VCPUS),
+                        Optional.of(CPU_AFFINITY));
+        adbConnectToMicrodroid(getDevice(), cid);
+        // Wait until logd-init starts. The service is one of the last services that are started in
+        // the microdroid boot procedure. Therefore, waiting for the service means that we wait for
+        // the boot to complete. TODO: we need a better marker eventually.
+        tryRunOnMicrodroid("watch -e \"getprop init.svc.logd-reinit | grep '^$'\"");
+        // We need root permission to write to /data/tombstones/
+        rootMicrodroid();
+        // Write a test tombstone file in /data/tombstones
+        runOnMicrodroid("echo Just a test tombstone in VM > /data/tombstones/transmit.txt ");
+        // check if the tombstone have been tranferred from VM
+        assertNotEquals(runOnMicrodroid("timeout 15s logcat | grep -m 1 "
+                            + "'tombstone_transmit.microdroid:.*data/tombstones/transmit.txt'"),
+                "");
+        // Confirm that tombstone is received (from host logcat)
+        assertNotEquals(runOnHost("adb", "-s", getDevice().getSerialNumber(),
+                            "logcat", "-d", "-e",
+                            "Received.*bytes from guest & wrote to tombstone file.*"),
+                "");
     }
 
     @Test
