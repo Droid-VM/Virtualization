86545ee31f9488a7b06c2ff4824d7415e69569fd
Seungjae Yoo <seungjaeyoo@google.com>
2022-06-07 02:21:56 +0000

    Better test coverage for apkverify library
    Bug: 233573377
    Test: atest libapkverify.integration_test
    
    Change-Id: Icdad3afa352fec2286d70a395f18a0973e6e2def

--- libs/apkverify/tests/apkverify_test.rs
+++ libs/apkverify/tests/apkverify_test.rs
@@ -23,8 +23,40 @@ fn test_verify_v3() {
 }
 
 #[test]
+fn test_verify_v3_dsa_sha256_1024() {
+    // TODO(b/190343842)
+    let res = verify("tests/data/v3-only-with-dsa-sha256-1024.apk");
+    assert!(res.is_err());
+    assert_contains(
+        &res.unwrap_err().to_string(),
+        "TODO(b/190343842) not implemented signature algorithm",
+    );
+}
+
+#[test]
 fn test_verify_v3_ecdsa_sha256_p256() {
     assert!(verify("tests/data/v3-only-with-ecdsa-sha256-p256.apk").is_ok());
+}
+
+#[test]
+fn test_verify_v3_ecdsa_sha512_p256() {
+    // TODO(b/190343842)
+    let res = verify("tests/data/v3-only-with-ecdsa-sha512-p256.apk");
+    assert!(res.is_err());
+    assert_contains(
+        &res.unwrap_err().to_string(),
+        "TODO(b/190343842) not implemented signature algorithm",
+    );
+}
+
+#[test]
+fn test_verify_v3_rsa_pkcs1_sha256_3072() {
+    assert!(verify("tests/data/v3-only-with-rsa-pkcs1-sha256-3072.apk").is_ok());
+}
+
+#[test]
+fn test_verify_v3_rsa_pkcs1_sha512_4096() {
+    assert!(verify("tests/data/v3-only-with-rsa-pkcs1-sha512-4096.apk").is_ok());
 }
 
 #[test]
@@ -50,4 +82,84 @@ fn test_verify_truncated_cd() {
         res.unwrap_err().root_cause().downcast_ref::<ZipError>().unwrap(),
         ZipError::InvalidArchive(_),
     ));
+}
+
+#[test]
+fn test_verify_v3_empty() {
+    let res = verify("tests/data/v3-only-empty.apk");
+    assert!(res.is_err());
+    assert_contains(&res.unwrap_err().to_string(), "APK too small for APK Signing Block");
+}
+
+#[test]
+fn test_verify_v3_wrong_apk_sig_block_magic() {
+    let res = verify("tests/data/v3-only-with-ecdsa-sha512-p384-wrong-apk-sig-block-magic.apk");
+    assert!(res.is_err());
+    assert_contains(&res.unwrap_err().to_string(), "No APK Signing Block");
+}
+
+#[test]
+fn test_verify_v3_apk_sig_block_size_mismatch() {
+    let res =
+        verify("tests/data/v3-only-with-rsa-pkcs1-sha512-4096-apk-sig-block-size-mismatch.apk");
+    assert!(res.is_err());
+    assert_contains(
+        &res.unwrap_err().to_string(),
+        "APK Signing Block sizes in header and footer do not match",
+    );
+}
+
+#[test]
+fn test_verify_v3_no_certs_in_sig() {
+    let res = verify("tests/data/v3-only-no-certs-in-sig.apk");
+    assert!(res.is_err());
+    assert_contains(&res.unwrap_err().to_string(), "No certificates listed");
+}
+
+#[test]
+fn test_verify_v3_no_supported_sig_algs() {
+    let res = verify("tests/data/v3-only-no-supported-sig-algs.apk");
+    assert!(res.is_err());
+    assert_contains(&res.unwrap_err().to_string(), "No supported signatures found");
+}
+
+#[test]
+fn test_verify_v3_signatures_and_digests_block_mismatch() {
+    let res = verify("tests/data/v3-only-signatures-and-digests-block-mismatch.apk");
+    assert!(res.is_err());
+    assert_contains(
+        &res.unwrap_err().to_string(),
+        "Signature algorithms don't match between digests and signatures records",
+    );
+}
+
+#[test]
+fn test_verify_v3_sig_does_not_verify() {
+    let res = verify("tests/data/v3-only-with-rsa-pkcs1-sha256-3072-sig-does-not-verify.apk");
+    assert!(res.is_err());
+    assert_contains(&res.unwrap_err().to_string(), "Signature is invalid");
+}
+
+#[test]
+fn test_verify_v3_unknown_additional_attr() {
+    // TODO
+    assert!(verify("tests/data/v3-only-unknown-additional-attr.apk").is_ok());
+}
+
+#[test]
+fn test_verify_v3_unknown_pair_in_apk_sig_block() {
+    // TODO
+    assert!(verify("tests/data/v3-only-unknown-pair-in-apk-sig-block.apk").is_ok());
+}
+
+#[test]
+fn test_verify_v3_ignorable_unsupported_sig_algs() {
+    // TODO
+    assert!(verify("tests/data/v3-only-with-ignorable-unsupported-sig-algs.apk").is_ok());
+}
+
+#[test]
+fn test_verify_v3_stamp() {
+    // TODO
+    assert!(verify("tests/data/v3-only-with-stamp.apk").is_ok());
 }
