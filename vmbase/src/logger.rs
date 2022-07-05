ac62d7c67d5e74dd76f271c794e4f7a4d024104c
David Brazdil <dbrazdil@google.com>
2022-07-05 12:22:20 +0100

    vmbase: Add basic logger
    Introduce a logger in vmbase that wraps around the println! into UART to
    be able to distinguish different levels of log messages.
    
    Test: atest vmbase_example.integration_test
    Change-Id: Id3b98ef4cdc4b23a11f099d75cc1c9902a9da785

---
+++ vmbase/src/logger.rs
@@ -0,0 +1,47 @@
+// Copyright 2022, The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Logger for vmbase.
+
+extern crate log;
+
+use super::{eprintln, println};
+use log::{Level, LevelFilter, Log, Metadata, Record, SetLoggerError};
+
+struct Logger;
+static LOGGER: Logger = Logger;
+
+impl Log for Logger {
+    fn enabled(&self, _metadata: &Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &Record) {
+        // Use eprintln! to reset the serial device before printing errors.
+        if record.level() <= Level::Error {
+            eprintln!("[{}] {}", record.level(), record.args());
+        } else {
+            println!("[{}] {}", record.level(), record.args());
+        }
+    }
+
+    fn flush(&self) {}
+}
+
+/// Initialize vmbase logger with a given max logging level.
+pub fn init(max_level: LevelFilter) -> Result<(), SetLoggerError> {
+    log::set_logger(&LOGGER)?;
+    log::set_max_level(max_level);
+    Ok(())
+}
