bc18b57e9fcce6a693e5f2c5f72b8a470e9bedbf
Alice Wang <aliceywang@google.com>
2022-07-25 14:44:13 +0000

    benchmarks:Measure virtio-blk read throughput
    Test: atest MicrodroidBenchmarks
    Change-Id: Ia46ea8d1b4425fa280171adcce7f82a76f1a329e

--- tests/helper/src/java/com/android/microdroid/test/MicrodroidDeviceTestBase.java
+++ tests/helper/src/java/com/android/microdroid/test/MicrodroidDeviceTestBase.java
@@ -132,7 +132,7 @@ public abstract class MicrodroidDeviceTestBase {
     protected abstract static class VmEventListener implements VirtualMachineCallback {
         private ExecutorService mExecutorService = Executors.newSingleThreadExecutor();
 
-        void runToFinish(String logTag, VirtualMachine vm)
+        public void runToFinish(String logTag, VirtualMachine vm)
                 throws VirtualMachineException, InterruptedException {
             vm.setCallback(mExecutorService, this);
             vm.run();
@@ -141,7 +141,7 @@ public abstract class MicrodroidDeviceTestBase {
             mExecutorService.awaitTermination(300, TimeUnit.SECONDS);
         }
 
-        void forceStop(VirtualMachine vm) {
+        protected void forceStop(VirtualMachine vm) {
             try {
                 vm.clearCallback();
                 vm.stop();
