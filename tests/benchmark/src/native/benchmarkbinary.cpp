d44d212d4b6125e939165b4f45932f53a3593e5e
Alice Wang <aliceywang@google.com>
2022-07-25 14:44:13 +0000

    benchmarks:Measure virtio-blk read rate
    Bug: 236123069
    Test: atest MicrodroidBenchmarks
    Change-Id: Ia46ea8d1b4425fa280171adcce7f82a76f1a329e

--- tests/benchmark/src/native/benchmarkbinary.cpp
+++ tests/benchmark/src/native/benchmarkbinary.cpp
@@ -13,11 +13,165 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <aidl/android/system/virtualmachineservice/IVirtualMachineService.h>
+#include <aidl/com/android/microdroid/testservice/BnReportService.h>
+#include <android-base/result.h>
+#include <android-base/unique_fd.h>
+#include <fcntl.h>
+#include <linux/vm_sockets.h>
+#include <stdio.h>
 #include <unistd.h>
 
-extern "C" int android_native_main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
-    // do nothing for now; just leave it alive. good night.
-    for (;;) {
-        sleep(1000);
+#include <binder_rpc_unstable.hpp>
+#include <chrono>
+#include <random>
+#include <string>
+#include <vector>
+
+#include "android-base/logging.h"
+
+constexpr uint64_t BLOCK_SIZE = 4096; // bytes
+constexpr uint64_t BYTES_PER_MB = 1024 * 1024;
+
+using aidl::android::system::virtualmachineservice::IVirtualMachineService;
+using android::base::ErrnoError;
+using android::base::Error;
+using android::base::Result;
+using android::base::unique_fd;
+
+namespace {
+Result<void> run_virtio_blk_benchmark_tests() {
+    class VirtioBlkReportService
+          : public aidl::com::android::microdroid::testservice::BnReportService {
+    public:
+        ndk::ScopedAStatus getSeqReadRate(double* out) override {
+            *out = _seq_read_rate;
+            return ndk::ScopedAStatus::ok();
+        }
+
+        ndk::ScopedAStatus getRandReadRate(double* out) override {
+            *out = _rand_read_rate;
+            return ndk::ScopedAStatus::ok();
+        }
+
+        Result<void> run_tests() {
+            const char* filename = "/apex/com.android.virt/etc/fs/microdroid_super.img";
+            const int n = 5; // Times to run the I/O tests.
+            unique_fd fd(open(filename, O_RDONLY));
+            if (fd.get() == -1) {
+                return ErrnoError() << "opening " << filename << " failed";
+            }
+            char buf[BLOCK_SIZE] = {};
+            uint64_t block_count = 0;
+            // Gets the number of blocks.
+            while (read(fd.get(), buf, BLOCK_SIZE) > 0) {
+                ++block_count;
+            }
+            const double mb = block_count * BLOCK_SIZE / BYTES_PER_MB;
+
+            // Test sequential read.
+            double elapsed_seconds = 0.;
+            for (int i = 0; i < n; ++i) {
+                if (auto res = read_file(fd, block_count, /*is_rand=*/false); res.ok()) {
+                    elapsed_seconds += res.value();
+                } else {
+                    return res.error();
+                }
+            }
+            _seq_read_rate = mb / elapsed_seconds * n;
+
+            // Test random read.
+            elapsed_seconds = 0.;
+            for (int i = 0; i < n; ++i) {
+                if (auto res = read_file(fd, block_count, /*is_rand=*/true); res.ok()) {
+                    elapsed_seconds += res.value();
+                } else {
+                    return res.error();
+                }
+            }
+            _rand_read_rate = mb / elapsed_seconds * n;
+            return {};
+        }
+
+    private:
+        /** Returns the elapsed seconds for reading the given file. */
+        Result<double> read_file(unique_fd& fd, uint64_t block_count, bool is_rand) {
+            std::vector<uint64_t> offsets;
+            if (is_rand) {
+                std::mt19937 rd{std::random_device{}()};
+                offsets.reserve(block_count);
+                for (uint64_t i = 0; i < block_count; i++) offsets.push_back(i * BLOCK_SIZE);
+                std::shuffle(offsets.begin(), offsets.end(), rd);
+            }
+            char buf[BLOCK_SIZE] = {};
+            if (lseek(fd.get(), 0, SEEK_SET) == -1) {
+                return ErrnoError() << "failed to lseek";
+            }
+            sync();
+
+            clock_t start = clock();
+            for (uint64_t i = 0; i < block_count; i++) {
+                if (is_rand) {
+                    if (lseek(fd.get(), offsets[i], SEEK_SET) == -1) {
+                        return ErrnoError() << "failed to lseek";
+                    }
+                }
+                auto bytes = read(fd.get(), buf, BLOCK_SIZE);
+                if (bytes == 0) {
+                    return Error() << "unexpected end of file";
+                } else if (bytes == -1) {
+                    return ErrnoError() << "failed to read";
+                }
+            }
+            return {((double)clock() - start) / CLOCKS_PER_SEC};
+        }
+
+        double _seq_read_rate;
+        double _rand_read_rate;
+    };
+    auto test_service = ndk::SharedRefBase::make<VirtioBlkReportService>();
+    if (auto res = test_service->run_tests(); !res.ok()) {
+        return res.error();
     }
+    auto callback = []([[maybe_unused]] void* param) {
+        // Tell microdroid_manager that we're ready.
+        // If we can't, abort in order to fail fast - the host won't proceed without
+        // receiving the onReady signal.
+        ndk::SpAIBinder binder(
+                RpcClient(VMADDR_CID_HOST, IVirtualMachineService::VM_BINDER_SERVICE_PORT));
+        auto virtualMachineService = IVirtualMachineService::fromBinder(binder);
+        if (virtualMachineService == nullptr) {
+            LOG(ERROR) << "failed to connect VirtualMachineService\n";
+            abort();
+        }
+        if (auto status = virtualMachineService->notifyPayloadReady(); !status.isOk()) {
+            LOG(ERROR) << "failed to notify payload ready to virtualizationservice: "
+                       << status.getDescription();
+            abort();
+        }
+    };
+
+    if (!RunRpcServerCallback(test_service->asBinder().get(), test_service->SERVICE_PORT, callback,
+                              nullptr)) {
+        return Error() << "RPC Server failed to run";
+    }
+    return {};
+}
+} // Anonymous namespace
+
+extern "C" int android_native_main([[maybe_unused]] int argc, char* argv[]) {
+    if (strcmp(argv[1], "no_io") == 0) {
+        // do nothing for now; just leave it alive. good night.
+        for (;;) {
+            sleep(1000);
+        }
+    } else if (strcmp(argv[1], "io") == 0) {
+        if (auto res = run_virtio_blk_benchmark_tests(); res.ok()) {
+            return 0;
+        } else {
+            LOG(ERROR) << "IO benchmark test failed: " << res.error() << "\n";
+            return 1;
+        }
+    }
+    return 0;
 }
