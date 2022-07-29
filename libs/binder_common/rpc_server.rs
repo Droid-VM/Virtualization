71930aedfd736468ccca78ae0b8dd8a89e017282
Andrew Walbran <qwandor@google.com>
2022-07-29 15:59:17 +0000

    Add safe wrapper for RunRpcServerWithFactory.
    Test: atest compos_key_tests MicrodroidHostTestCases MicrodroidTestApp
    Change-Id: Iaab166d29e0ec08161db50c5e1419283bf304499

--- libs/binder_common/rpc_server.rs
+++ libs/binder_common/rpc_server.rs
@@ -18,7 +18,7 @@
 
 use binder::unstable_api::AsNative;
 use binder::SpIBinder;
-use std::os::raw;
+use std::{os::raw, ptr::null_mut};
 
 /// Runs a binder RPC server, serving the supplied binder service implementation on the given vsock
 /// port.
@@ -80,5 +80,49 @@ where
         if let Some(on_ready) = self.0.take() {
             on_ready();
         }
+    }
+}
+
+type BoxedRpcServerFactory<'a> = Box<dyn FnMut(u32) -> Option<SpIBinder> + Send + Sync + 'a>;
+
+/// Runs a binder RPC server, using the given factory function to construct a binder service
+/// implementation for each connection.
+///
+/// The current thread is joined to the binder thread pool to handle incoming messages.
+///
+/// Returns true if the server has shutdown normally, false if it failed in some way.
+pub fn run_rpc_server_with_factory<'a>(
+    port: u32,
+    factory: impl FnMut(u32) -> Option<SpIBinder> + Send + Sync + 'a,
+) -> bool {
+    // Double box the factory because trait objects aren't FFI safe.
+    // NB: The type annotation is necessary to ensure that we have a `dyn` rather than an `impl`
+    // inside the `Box`.
+    let mut boxed_factory: BoxedRpcServerFactory = Box::new(factory);
+    let context = &mut boxed_factory as *mut BoxedRpcServerFactory as *mut raw::c_void;
+
+    // SAFETY: `factory_wrapper` is only ever called by `RunRpcServerWithFactory`, with context
+    // taking the pointer value above (so a properly aligned non-null pointer to an initialized
+    // `Box`), within the lifetime of `boxed_factory` (i.e. no more calls will be made after
+    // `RunRpcServerWithFactory` returns).
+    unsafe {
+        binder_rpc_unstable_bindgen::RunRpcServerWithFactory(Some(factory_wrapper), context, port)
+    }
+}
+
+unsafe extern "C" fn factory_wrapper(
+    cid: u32,
+    context: *mut raw::c_void,
+) -> *mut binder_rpc_unstable_bindgen::AIBinder {
+    // SAFETY: `context` was created from an `&mut BoxedRpcServerFactory` by
+    // `run_rpc_server_with_factory`, and we are still within the lifetime of the value it is
+    // pointing to.
+    let factory_ptr = context as *mut BoxedRpcServerFactory;
+    let factory: &mut BoxedRpcServerFactory = factory_ptr.as_mut().unwrap();
+
+    if let Some(mut service) = factory(cid) {
+        service.as_native_mut() as *mut binder_rpc_unstable_bindgen::AIBinder
+    } else {
+        null_mut()
     }
 }
