65167e5d4db3f3aa04757010a3d1193142cfc18c
Andrew Walbran <qwandor@google.com>
2022-08-02 16:16:28 +0000

    Add safe wrapper for RpcPreconnectedClient.
    Test: atest compos_key_tests MicrodroidHostTestCases MicrodroidTestApp
    Change-Id: I8dcc6c9b0465950bfaced03699fa3167dc3dc641

--- libs/binder_common/rpc_client.rs
+++ libs/binder_common/rpc_client.rs
@@ -17,7 +17,8 @@
 //! Helpers for implementing an RPC Binder client.
 
 use binder::unstable_api::{new_spibinder, AIBinder};
-use binder::{StatusCode, Strong};
+use binder::{SpIBinder, StatusCode, Strong};
+use std::os::{raw, unix::io::RawFd};
 
 /// Connects to a binder RPC server.
 pub fn connect_rpc_binder<T: binder::FromIBinder + ?Sized>(
@@ -33,5 +34,40 @@ pub fn connect_rpc_binder<T: binder::FromIBinder + ?Sized>(
         <T>::try_from(ibinder)
     } else {
         Err(StatusCode::BAD_VALUE)
+    }
+}
+
+type RequestFd<'a> = &'a mut dyn FnMut() -> Option<RawFd>;
+
+/// Connects to a Binder RPC server, using the given callback to get file descriptors already
+/// connected to it.
+pub fn connect_preconnected_rpc_binder(
+    mut request_fd: impl FnMut() -> Option<RawFd>,
+) -> Option<SpIBinder> {
+    // Double reference the factory because trait objects aren't FFI safe.
+    let mut request_fd_ref: RequestFd = &mut request_fd;
+    let param = &mut request_fd_ref as *mut RequestFd as *mut raw::c_void;
+
+    // SAFETY: AIBinder returned by RpcPreconnectedClient has correct reference count, and the
+    // ownership can be safely taken by new_spibinder. RpcPreconnectedClient does not take ownership
+    // of param, only passing it to request_fd_wrapper.
+    unsafe {
+        let binder =
+            binder_rpc_unstable_bindgen::RpcPreconnectedClient(Some(request_fd_wrapper), param)
+                as *mut AIBinder;
+        new_spibinder(binder)
+    }
+}
+
+unsafe extern "C" fn request_fd_wrapper(param: *mut raw::c_void) -> raw::c_int {
+    // SAFETY: This is only ever called by RpcPreconnectedClient, within the lifetime of the
+    // BinderFdFactory reference, with param being a properly aligned non-null pointer to an
+    // initialized instance.
+    let request_fd_ptr = param as *mut RequestFd;
+    let request_fd = request_fd_ptr.as_mut().unwrap();
+    if let Some(fd) = request_fd() {
+        fd
+    } else {
+        -1
     }
 }
