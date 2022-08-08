782521521a1a40f3d09f2e2bda87add9b05ea1fb
Alan Stokes <alanstokes@google.com>
2022-08-08 14:44:48 +0100

    Move VM callback to vmclient
    Instead of having clients directly register a callback with VS,
    implement a Rust level callback interface in vmclient. This saves an
    extra binder call on each notification, a bunch of boilerplate code,
    and allows us to provide a slightly better interface (e.g. we can use
    the Rust DeathReason enum, as elsewhere in vmclient, for instantly
    better logging).
    
    I also replaced all our usages of <some_interface>::binder::{...} with
    direct access to binder::{...}. That makes it clearer what depends on
    the interface itself and what is just generic binder code. I realise
    this should be a separate change, but I only realised that after doing
    bits of both.
    
    Test: composd_cmd test-compile, observe logs (on both success & failure)
    Test: atest -b (to make sure all our tests build)
    Test: Presubmits
    Change-Id: Iceda8d7b8f8008f9d7a2c51106c2794f09bb378e

--- compos/common/binder.rs
+++ compos/common/binder.rs
@@ -16,7 +16,7 @@
 
 //! Helper for converting Error types to what Binder expects
 
-use android_system_virtualizationservice::binder::{ExceptionCode, Result as BinderResult};
+use binder::{ExceptionCode, Result as BinderResult};
 use binder_common::new_binder_exception;
 use log::warn;
 use std::fmt::Debug;
