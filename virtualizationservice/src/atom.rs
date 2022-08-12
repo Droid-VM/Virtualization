632b63c885359c654282a962a482b3a4d61c286a
Seungjae Yoo <seungjaeyoo@google.com>
2022-08-12 04:44:51 +0000

    Implement write_vm_booted_stats for VmBooted atom
    Bug: 236254440
    Test: N/A
    Change-Id: I512a2cd0ddf004e8c67778ad81878132cd9a495d

--- virtualizationservice/src/atom.rs
+++ virtualizationservice/src/atom.rs
@@ -23,7 +23,7 @@ use android_system_virtualizationservice::binder::{Status, Strong};
 use anyhow::{anyhow, Result};
 use log::{trace, warn};
 use microdroid_payload_config::VmPayloadConfig;
-use statslog_virtualization_rust::vm_creation_requested;
+use statslog_virtualization_rust::{vm_booted, vm_creation_requested};
 use zip::ZipArchive;
 
 fn get_vm_payload_config(config: &VirtualMachineAppConfig) -> Result<VmPayloadConfig> {
@@ -105,6 +105,21 @@ pub fn write_vm_creation_stats(
     };
 
     match vm_creation_requested.stats_write() {
+        Err(e) => {
+            warn!("statslog_rust failed with error: {}", e);
+        }
+        Ok(_) => trace!("statslog_rust succeeded for virtualization service"),
+    }
+}
+
+/// Write the stats of VM boot to statsd
+pub fn write_vm_booted_stats() {
+    let empty_string = String::new();
+    let vm_booted = vm_booted::VmBooted {
+        // TODO(seungjaeyoo) Implement sending proper data about vm_name
+        vm_name: &empty_string,
+    };
+    match vm_booted.stats_write() {
         Err(e) => {
             warn!("statslog_rust failed with error: {}", e);
         }
