f6c8f8d01218c6887b4df9d9a955a7dfdc58b097
Alan Stokes <alanstokes@google.com>
2022-08-22 13:55:05 +0100

    Migrate VirtualMachine API to @SystemApi
    Mark all the public members of the existing internal platform API as
    @SystemApi. Then make various changes, mostly to make API lint happy:
    - Replace enums with integer constants. Map the AIDL constants to the
      API constants. (Some of this was already done in
      https://r.android.com/2203781.)
    - Make connectToVsockServer synchronous rather than using Future.
    - Make VirtualMachine AutoCloseable (close() == stop()).
    - Add VirtualMachine getters to match the builder's setters.
    - Lots of nullability declarations.
    - Make VirtualMachineCallback an abstract class instead of an
      interface.
    
    Update system-current.txt to match.
    
    Modify the demo app and the test APK and dependencies to build against
    our stub library, instead of the implementation, and switch to the
    system_current SDK rather than platform APIs. (We still use platform
    APIs for the native payload code though.)
    
    Bug: 243512107
    Bug: 243512115
    Test: atest MicrodroidTests MicrodroidHostTestCases
    Test: Install & run demo app
    CTS-Coverage-Bug: 244561836
    Change-Id: Ibfffbb221f3ac37e03d5cfdbc9208428eeadbb9f

--- javalib/src/android/system/virtualmachine/VirtualMachineCallback.java
+++ javalib/src/android/system/virtualmachine/VirtualMachineCallback.java
@@ -19,6 +19,7 @@ package android.system.virtualmachine;
 import android.annotation.IntDef;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
+import android.annotation.SystemApi;
 import android.os.ParcelFileDescriptor;
 
 import java.lang.annotation.Retention;
@@ -30,125 +31,120 @@ import java.lang.annotation.RetentionPolicy;
  *
  * @hide
  */
-public interface VirtualMachineCallback {
+@SystemApi
+public abstract class VirtualMachineCallback {
+
     /** @hide */
     @Retention(RetentionPolicy.SOURCE)
     @IntDef({
-        ERROR_UNKNOWN,
-        ERROR_PAYLOAD_VERIFICATION_FAILED,
-        ERROR_PAYLOAD_CHANGED,
-        ERROR_PAYLOAD_INVALID_CONFIG
+            ERROR_UNKNOWN,
+            ERROR_PAYLOAD_VERIFICATION_FAILED,
+            ERROR_PAYLOAD_CHANGED,
+            ERROR_PAYLOAD_INVALID_CONFIG
     })
-    @interface ErrorCode {}
+    public @interface ErrorCode {}
 
     /** Error code for all other errors not listed below. */
-    int ERROR_UNKNOWN = 0;
-
+    public static final int ERROR_UNKNOWN = 0;
     /**
      * Error code indicating that the payload can't be verified due to various reasons (e.g invalid
      * merkle tree, invalid formats, etc).
      */
-    int ERROR_PAYLOAD_VERIFICATION_FAILED = 1;
-
+    public static final int ERROR_PAYLOAD_VERIFICATION_FAILED = 1;
     /** Error code indicating that the payload is verified, but has changed since the last boot. */
-    int ERROR_PAYLOAD_CHANGED = 2;
-
+    public static final int ERROR_PAYLOAD_CHANGED = 2;
     /** Error code indicating that the payload config is invalid. */
-    int ERROR_PAYLOAD_INVALID_CONFIG = 3;
+    public static final int ERROR_PAYLOAD_INVALID_CONFIG = 3;
+
 
     /** @hide */
     @Retention(RetentionPolicy.SOURCE)
     @IntDef({
-        DEATH_REASON_VIRTUALIZATIONSERVICE_DIED,
-        DEATH_REASON_INFRASTRUCTURE_ERROR,
-        DEATH_REASON_KILLED,
-        DEATH_REASON_UNKNOWN,
-        DEATH_REASON_SHUTDOWN,
-        DEATH_REASON_ERROR,
-        DEATH_REASON_REBOOT,
-        DEATH_REASON_CRASH,
-        DEATH_REASON_HANGUP,
+            DEATH_REASON_VIRTUALIZATION_SERVICE_DIED,
+            DEATH_REASON_INFRASTRUCTURE_ERROR,
+            DEATH_REASON_KILLED,
+            DEATH_REASON_UNKNOWN,
+            DEATH_REASON_SHUTDOWN,
+            DEATH_REASON_ERROR,
+            DEATH_REASON_REBOOT,
+            DEATH_REASON_CRASH,
+            DEATH_REASON_PVM_FIRMWARE_PUBLIC_KEY_MISMATCH,
+            DEATH_REASON_PVM_FIRMWARE_INSTANCE_IMAGE_CHANGED,
+            DEATH_REASON_BOOTLOADER_PUBLIC_KEY_MISMATCH,
+            DEATH_REASON_BOOTLOADER_INSTANCE_IMAGE_CHANGED,
+            DEATH_REASON_MICRODROID_FAILED_TO_CONNECT_TO_VIRTUALIZATION_SERVICE,
+            DEATH_REASON_MICRODROID_PAYLOAD_HAS_CHANGED,
+            DEATH_REASON_MICRODROID_PAYLOAD_VERIFICATION_FAILED,
+            DEATH_REASON_MICRODROID_INVALID_PAYLOAD_CONFIG,
+            DEATH_REASON_MICRODROID_UNKNOWN_RUNTIME_ERROR,
+            DEATH_REASON_HANGUP,
     })
-    @interface DeathReason {}
+    public @interface DeathReason {}
 
-    /**
-     * virtualizationservice itself died, taking the VM down with it. This is a negative number to
-     * avoid conflicting with the other death reasons which match the ones in the AIDL interface.
-     */
-    int DEATH_REASON_VIRTUALIZATIONSERVICE_DIED = -1;
-
+    /** The virtualization service itself died, taking the VM down with it. */
+    //  This is a negative number to avoid conflicting with the other death reasons which match
+    //  the ones in the AIDL interface.
+    public static final int DEATH_REASON_VIRTUALIZATION_SERVICE_DIED = -1;
     /** There was an error waiting for the VM. */
-    int DEATH_REASON_INFRASTRUCTURE_ERROR = 0;
-
+    public static final int DEATH_REASON_INFRASTRUCTURE_ERROR = 0;
     /** The VM was killed. */
-    int DEATH_REASON_KILLED = 1;
-
+    public static final int DEATH_REASON_KILLED = 1;
     /** The VM died for an unknown reason. */
-    int DEATH_REASON_UNKNOWN = 2;
-
+    public static final int DEATH_REASON_UNKNOWN = 2;
     /** The VM requested to shut down. */
-    int DEATH_REASON_SHUTDOWN = 3;
-
+    public static final int DEATH_REASON_SHUTDOWN = 3;
     /** crosvm had an error starting the VM. */
-    int DEATH_REASON_ERROR = 4;
-
+    public static final int DEATH_REASON_ERROR = 4;
     /** The VM requested to reboot, possibly as the result of a kernel panic. */
-    int DEATH_REASON_REBOOT = 5;
-
+    public static final int DEATH_REASON_REBOOT = 5;
     /** The VM or crosvm crashed. */
-    int DEATH_REASON_CRASH = 6;
-
+    public static final int DEATH_REASON_CRASH = 6;
     /** The pVM firmware failed to verify the VM because the public key doesn't match. */
-    int DEATH_REASON_PVM_FIRMWARE_PUBLIC_KEY_MISMATCH = 7;
-
+    public static final int DEATH_REASON_PVM_FIRMWARE_PUBLIC_KEY_MISMATCH = 7;
     /** The pVM firmware failed to verify the VM because the instance image changed. */
-    int DEATH_REASON_PVM_FIRMWARE_INSTANCE_IMAGE_CHANGED = 8;
-
+    public static final int DEATH_REASON_PVM_FIRMWARE_INSTANCE_IMAGE_CHANGED = 8;
     /** The bootloader failed to verify the VM because the public key doesn't match. */
-    int DEATH_REASON_BOOTLOADER_PUBLIC_KEY_MISMATCH = 9;
-
+    public static final int DEATH_REASON_BOOTLOADER_PUBLIC_KEY_MISMATCH = 9;
     /** The bootloader failed to verify the VM because the instance image changed. */
-    int DEATH_REASON_BOOTLOADER_INSTANCE_IMAGE_CHANGED = 10;
-
+    public static final int DEATH_REASON_BOOTLOADER_INSTANCE_IMAGE_CHANGED = 10;
     /** The microdroid failed to connect to VirtualizationService's RPC server. */
-    int DEATH_REASON_MICRODROID_FAILED_TO_CONNECT_TO_VIRTUALIZATION_SERVICE = 11;
-
+    public static final int DEATH_REASON_MICRODROID_FAILED_TO_CONNECT_TO_VIRTUALIZATION_SERVICE =
+            11;
     /** The payload for microdroid is changed. */
-    int DEATH_REASON_MICRODROID_PAYLOAD_HAS_CHANGED = 12;
-
+    public static final int DEATH_REASON_MICRODROID_PAYLOAD_HAS_CHANGED = 12;
     /** The microdroid failed to verify given payload APK. */
-    int DEATH_REASON_MICRODROID_PAYLOAD_VERIFICATION_FAILED = 13;
-
+    public static final int DEATH_REASON_MICRODROID_PAYLOAD_VERIFICATION_FAILED = 13;
     /** The VM config for microdroid is invalid (e.g. missing tasks). */
-    int DEATH_REASON_MICRODROID_INVALID_PAYLOAD_CONFIG = 14;
-
+    public static final int DEATH_REASON_MICRODROID_INVALID_PAYLOAD_CONFIG = 14;
     /** There was a runtime error while running microdroid manager. */
-    int DEATH_REASON_MICRODROID_UNKNOWN_RUNTIME_ERROR = 15;
-
+    public static final int DEATH_REASON_MICRODROID_UNKNOWN_RUNTIME_ERROR = 15;
     /** The VM killed due to hangup */
-    int DEATH_REASON_HANGUP = 16;
+    public static final int DEATH_REASON_HANGUP = 16;
 
     /**
      * Called when the payload starts in the VM. The stream, if non-null, provides access
      * to the stdin/stdout of the VM payload.
      */
-    void onPayloadStarted(@NonNull VirtualMachine vm, @Nullable ParcelFileDescriptor stream);
+    public abstract void onPayloadStarted(@NonNull VirtualMachine vm,
+            @Nullable ParcelFileDescriptor stream);
 
     /**
      * Called when the payload in the VM is ready to serve. See
      * {@link VirtualMachine#connectToVsockServer(int)} ()}.
      */
-    void onPayloadReady(@NonNull VirtualMachine vm);
+    public abstract void onPayloadReady(@NonNull VirtualMachine vm);
 
     /** Called when the payload has finished in the VM. */
-    void onPayloadFinished(@NonNull VirtualMachine vm, int exitCode);
+    public abstract void onPayloadFinished(@NonNull VirtualMachine vm, int exitCode);
 
     /** Called when an error occurs in the VM. */
-    void onError(@NonNull VirtualMachine vm, @ErrorCode int errorCode, @NonNull String message);
+    public abstract void onError(@NonNull VirtualMachine vm, @ErrorCode int errorCode,
+            @NonNull String message);
 
     /** Called when the VM has ended. */
-    void onDied(@NonNull VirtualMachine vm, @DeathReason int reason);
+    public abstract void onDied(@NonNull VirtualMachine vm, @DeathReason int reason);
 
     /** Called when kernel panic occurs and as a result ramdump is generated from the VM. */
-    void onRamdump(@NonNull VirtualMachine vm, @NonNull ParcelFileDescriptor ramdump);
+    public abstract void onRamdump(@NonNull VirtualMachine vm,
+            @NonNull ParcelFileDescriptor ramdump);
 }
