e4d36ae0d8c41e7b7095cce708f10a01f901e513
Shikha Panwar <shikhapanwar@google.com>
2022-08-30 14:45:49 +0000

    Ignore Bootloader related Microdroid tests
    Ignore the following 3 tests:
    1. testBootFailsWhenBootloaderAndVbMetaAreSignedWithDifferentKeys
    2. testBootSucceedsWhenNonProtectedVmStartsWithImagesSignedWithDifferentKey
    3. testBootSucceedsWhenBootloaderAndVbmetaHaveSameSigningKeys
    
    We are removing the bootloader from the VM boot flow and [1][3] are
    irrelevant. [2] require changes in runMicrodroidWithResignedImages()
    because the microdroid disks are constructed somewhat differently in
    non-protected case.
    
    Bug: 240235424
    Test: atest MicrodroidTestCase
    Change-Id: Ie8cf8ccb2f109b06214cce457b205b886c7dc3a6

--- tests/hostside/java/com/android/microdroid/test/MicrodroidTestCase.java
+++ tests/hostside/java/com/android/microdroid/test/MicrodroidTestCase.java
@@ -56,6 +56,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
@@ -303,6 +304,8 @@ public class MicrodroidTestCase extends MicrodroidHostTestCaseBase {
             boolean daemonize,
             String consolePath)
             throws Exception {
+        // TODO(b/244291943): Fix the partitions for non-protected VM
+        assumeTrue(isProtected);
         CommandRunner android = new CommandRunner(getDevice());
 
         File virtApexDir = FileUtil.createTempDir("virt_apex");
@@ -351,7 +354,7 @@ public class MicrodroidTestCase extends MicrodroidHostTestCaseBase {
         //   - its idsig
 
         // Load etc/microdroid.json
-        File microdroidConfigFile = new File(virtApexEtcDir, "microdroid.json");
+        File microdroidConfigFile = new File(virtApexEtcDir, "microdroid_legacy.json");
         JSONObject config = new JSONObject(FileUtil.readStringFromFile(microdroidConfigFile));
 
         // Replace paths so that the config uses re-signed images from TEST_ROOT
@@ -435,6 +438,7 @@ public class MicrodroidTestCase extends MicrodroidHostTestCaseBase {
     }
 
     @Test
+    @Ignore("b/244291943")
     @CddTest(requirements = {"9.17/C-2-2", "9.17/C-2-6"})
     public void testBootSucceedsWhenNonProtectedVmStartsWithImagesSignedWithDifferentKey()
             throws Exception {
@@ -452,6 +456,7 @@ public class MicrodroidTestCase extends MicrodroidHostTestCaseBase {
     }
 
     @Test
+    @Ignore("b/244291943")
     @CddTest(requirements = {"9.17/C-2-2", "9.17/C-2-6"})
     public void testBootFailsWhenBootloaderAndVbMetaAreSignedWithDifferentKeys() throws Exception {
         // Sign everything with key1 except vbmeta
@@ -465,7 +470,7 @@ public class MicrodroidTestCase extends MicrodroidHostTestCaseBase {
         String cid =
                 runMicrodroidWithResignedImages(
                         key, keyOverrides, isProtected, daemonize, consolePath);
-        // Wail for a while so that bootloader prints errors to console
+        // Wait for a while so that bootloader prints errors to console
         assertThatEventually(
                 10000,
                 () -> getDevice().pullFileContents(consolePath),
@@ -474,6 +479,7 @@ public class MicrodroidTestCase extends MicrodroidHostTestCaseBase {
     }
 
     @Test
+    @Ignore("b/244291943")
     @CddTest(requirements = {"9.17/C-2-2", "9.17/C-2-6"})
     public void testBootSucceedsWhenBootloaderAndVbmetaHaveSameSigningKeys() throws Exception {
         // Sign everything with key1 except bootloader and vbmeta
@@ -795,7 +801,7 @@ public class MicrodroidTestCase extends MicrodroidHostTestCaseBase {
 
         // Pull etc/microdroid.json
         File virtApexDir = FileUtil.createTempDir("virt_apex");
-        File microdroidConfigFile = new File(virtApexDir, "microdroid.json");
+        File microdroidConfigFile = new File(virtApexDir, "microdroid_legacy.json");
         assertTrue(getDevice().pullFile(VIRT_APEX + "etc/microdroid.json", microdroidConfigFile));
         JSONObject config = new JSONObject(FileUtil.readStringFromFile(microdroidConfigFile));
 
