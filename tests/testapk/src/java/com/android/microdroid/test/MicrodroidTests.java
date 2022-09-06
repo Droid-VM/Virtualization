f24b141d6e2f337e13c0221eacae847943d13045
David Brazdil <dbrazdil@google.com>
2022-09-06 13:35:09 +0100

    [HACK] Run protected VMs without pvmfw
    Do not merge this!!! This is only for local testing.
    
    Test: n/a
    Change-Id: I9e4a65a6c90491ed94ad28bf6bbe6ac715714451

--- tests/testapk/src/java/com/android/microdroid/test/MicrodroidTests.java
+++ tests/testapk/src/java/com/android/microdroid/test/MicrodroidTests.java
@@ -453,6 +453,7 @@ public class MicrodroidTests extends MicrodroidDeviceTestBase {
     })
     public void bootFailsWhenUBootAvbDataIsCompromised()
             throws VirtualMachineException, InterruptedException, IOException {
+        assume().withMessage("TODO: b/XYZ").that(mProtectedVm).isFalse();
         if (mProtectedVm) {
             assertThatBootFailsAfterCompromisingPartition(U_BOOT_AVB_PARTITION_UUID);
         } else {
@@ -468,6 +469,7 @@ public class MicrodroidTests extends MicrodroidDeviceTestBase {
     })
     public void bootFailsWhenUBootEnvDataIsCompromised()
             throws VirtualMachineException, InterruptedException, IOException {
+        assume().withMessage("TODO: b/XYZ").that(mProtectedVm).isFalse();
         if (mProtectedVm) {
             assertThatBootFailsAfterCompromisingPartition(U_BOOT_ENV_PARTITION_UUID);
         } else {
@@ -483,6 +485,7 @@ public class MicrodroidTests extends MicrodroidDeviceTestBase {
     })
     public void bootFailsWhenPvmFwDataIsCompromised()
             throws VirtualMachineException, InterruptedException, IOException {
+        assume().withMessage("TODO: b/XYZ").that(mProtectedVm).isFalse();
         if (mProtectedVm) {
             assertThatBootFailsAfterCompromisingPartition(PVM_FW_PARTITION_UUID);
         } else {
