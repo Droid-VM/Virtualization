3b06764ef5695507fe08ef92baecaacd7a76fc76
guile <guile@google.com>
2022-09-06 08:53:59 +0800

    Migrate pkvm_perf_test.py dex2oat64 and crosvm memory footprint collection to AOSP
    Test: atest ComposBenchmarkApp
    Change-Id: Ib5138dc7336f07937d2f1121a0fe995bbca5f583

--- compos/benchmark/src/java/com/android/compos/benchmark/ComposBenchmark.java
+++ compos/benchmark/src/java/com/android/compos/benchmark/ComposBenchmark.java
@@ -26,6 +26,7 @@ import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
 import android.util.Log;
 
+import com.android.microdroid.test.common.MetricsProcessor;
 import com.android.microdroid.test.device.MicrodroidDeviceTestBase;
 
 import org.junit.Before;
@@ -40,10 +41,14 @@ import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 
 @RunWith(JUnit4.class)
 public class ComposBenchmark extends MicrodroidDeviceTestBase {
@@ -53,6 +58,8 @@ public class ComposBenchmark extends MicrodroidDeviceTestBase {
     private static final double NANOS_IN_SEC = 1_000_000_000.0;
     private static final String METRIC_PREFIX = "avf_perf/compos/";
 
+    private final MetricsProcessor mMetricsProcessor = new MetricsProcessor(METRIC_PREFIX);
+
     private Instrumentation mInstrumentation;
 
     @Before
@@ -60,28 +67,12 @@ public class ComposBenchmark extends MicrodroidDeviceTestBase {
         mInstrumentation = getInstrumentation();
     }
 
-    private void reportMetric(String name, String unit, double[] values) {
-        double sum = 0;
-        double squareSum = 0;
-        double min = Double.MAX_VALUE;
-        double max = Double.MIN_VALUE;
-
-        for (double val : values) {
-            sum += val;
-            squareSum += val * val;
-            min = val < min ? val : min;
-            max = val > max ? val : max;
-        }
-
-        double average = sum / values.length;
-        double variance = squareSum / values.length - average * average;
-        double stdev = Math.sqrt(variance);
-
+    private void reportMetric(String name, String unit, List<Double> values) {
+        Map<String, Double> stats = mMetricsProcessor.computeStats(values, name, unit);
         Bundle bundle = new Bundle();
-        bundle.putDouble(METRIC_PREFIX + name + "_average_" + unit, average);
-        bundle.putDouble(METRIC_PREFIX + name + "_min_" + unit, min);
-        bundle.putDouble(METRIC_PREFIX + name + "_max_" + unit, max);
-        bundle.putDouble(METRIC_PREFIX + name + "_stdev_" + unit, stdev);
+        for (Map.Entry<String, Double> entry : stats.entrySet()) {
+            bundle.putDouble(entry.getKey(), entry.getValue());
+        }
         mInstrumentation.sendStatus(0, bundle);
     }
 
@@ -121,27 +112,154 @@ public class ComposBenchmark extends MicrodroidDeviceTestBase {
         }
     }
 
+    private static class ProcessInfo {
+        public final String mName;
+        public final int mPid;
+
+        ProcessInfo(String name, int pid) {
+            mName = name;
+            mPid = pid;
+        }
+    }
+
+    private Map<String, Long> parseMemInfo(String file) {
+        Map<String, Long> stats = new HashMap<>();
+        file.lines().forEach(line -> {
+            if (line.endsWith(" kB")) line = line.substring(0, line.length() - 3);
+
+            String[] elems = line.split(":");
+            stats.put(elems[0].trim(), Long.parseLong(elems[1].trim()));
+        });
+        return stats;
+    }
+
+    private Map<String, Long> getProcSmapsRollup(int pid) throws Exception {
+        String path = "/proc/" + pid + "/smaps_rollup";
+        return  parseMemInfo(skipFirstLine(executeCommand("cat " + path + " || true")));
+    }
+
+    private String skipFirstLine(String str) {
+        int index = str.indexOf("\n");
+        return (index < 0) ? "" : str.substring(index + 1);
+    }
+
+    private List<ProcessInfo> getRunningProcessesList() throws Exception {
+        List<ProcessInfo> list = new ArrayList<ProcessInfo>();
+        skipFirstLine(executeCommand("ps -Ao PID,NAME")).lines().forEach(ps -> {
+            ps = ps.trim();
+            int space = ps.indexOf(" ");
+            list.add(new ProcessInfo(
+                    ps.substring(space + 1),
+                    Integer.parseInt(ps.substring(0, space))));
+        });
+
+        return list;
+    }
+
+    private void updateProcessMaxMemMetrics(String processName,
+            Map<String, Long> processMaxMemMetrics) throws Exception {
+
+        for (ProcessInfo proc : getRunningProcessesList()) {
+            if (!proc.mName.equalsIgnoreCase(processName)) {
+                continue;
+            }
+            for (Map.Entry<String, Long> stat : getProcSmapsRollup(proc.mPid).entrySet()) {
+                String name = stat.getKey().toLowerCase();
+
+                Log.i(TAG, "Get running process " + proc.mName + " metrics : " + name
+                        + '-' + stat.getValue().toString());
+
+                if (!processMaxMemMetrics.containsKey(name)) {
+                    processMaxMemMetrics.put(name, stat.getValue());
+                } else {
+                    if (stat.getValue() > processMaxMemMetrics.get(name)) {
+                        processMaxMemMetrics.put(name, stat.getValue());
+                    }
+                }
+            }
+        }
+    }
+
     @Test
-    public void testGuestCompileTime() throws InterruptedException, IOException {
+    public void testGuestCompileTime() throws Exception {
         assume().withMessage("Skip on CF; too slow").that(isCuttlefish()).isFalse();
 
         final String command = "/apex/com.android.compos/bin/composd_cmd test-compile";
 
-        double[] compileTime = new double[ROUND_COUNT];
+        final List<Double> compileTimeArray = new ArrayList<>(ROUND_COUNT);
+        final List<Map<String, Long>> processMaxMemMetricsArray = new ArrayList<>(ROUND_COUNT);
 
         for (int round = 0; round < ROUND_COUNT; ++round) {
+
+            AtomicBoolean isCompilationFinish = new AtomicBoolean(false);
+
+            Thread threadGetMetrics = new Thread() {
+                @Override
+                public void run() {
+
+                    Map<String, Long> processMaxMemMetrics = new HashMap<>();
+
+                    while (true) {
+                        Log.i(TAG, "Start to get metrics");
+                        try {
+                            updateProcessMaxMemMetrics("crosvm", processMaxMemMetrics);
+                            Thread.sleep(1000);
+                        } catch (Exception e) {
+                            Log.i(TAG, "Get exception : " + e);
+                        }
+
+                        if (isCompilationFinish.get()) {
+                            break;
+                        }
+                    }
+
+                    for (Map.Entry<String, Long> stat : processMaxMemMetrics.entrySet()) {
+                        Log.i(TAG, "Get process max metrics : " + stat.getKey().toLowerCase()
+                                + " - " + stat.getValue().toString());
+                    }
+
+                    processMaxMemMetricsArray.add(processMaxMemMetrics);
+                }
+            };
+
+            threadGetMetrics.start();
+
             Long compileStartTime = System.nanoTime();
             String output = executeCommand(command);
             Long compileEndTime = System.nanoTime();
-
             Pattern pattern = Pattern.compile("All Ok");
             Matcher matcher = pattern.matcher(output);
             assertTrue(matcher.find());
+            double elapsedSec = (compileEndTime - compileStartTime) / NANOS_IN_SEC;
+            Log.i(TAG, "Compile time in guest took " + elapsedSec + "s");
+            isCompilationFinish.set(true);
 
-            compileTime[round] = (compileEndTime - compileStartTime) / NANOS_IN_SEC;
+            Log.i(TAG, "Waits for thread finish.");
+            threadGetMetrics.join();
+            Log.i(TAG, "Thread is finish.");
+
+            compileTimeArray.add(elapsedSec);
         }
 
-        reportMetric("guest_compile_time", "s", compileTime);
+        reportMetric("guest_compile_time", "s", compileTimeArray);
+
+        List<String> allMetrics = new ArrayList<>();
+        for (Map<String, Long> processMaxMemMetrics : processMaxMemMetricsArray) {
+            for (Map.Entry<String, Long> stat : processMaxMemMetrics.entrySet()) {
+                if (!allMetrics.contains(stat.getKey())) {
+                    allMetrics.add(stat.getKey());
+                }
+            }
+        }
+        for (String metrics : allMetrics) {
+            List<Double> allValues = new ArrayList<>();
+            for (Map<String, Long> processMaxMemMetrics : processMaxMemMetricsArray) {
+                if (processMaxMemMetrics.containsKey(metrics)) {
+                    allValues.add(processMaxMemMetrics.get(metrics).doubleValue());
+                }
+            }
+            reportMetric("guest_compile_crosvm_" + metrics, "kb", allValues);
+        }
     }
 
     private Timestamp getLatestDex2oatSuccessTime()
@@ -176,9 +294,46 @@ public class ComposBenchmark extends MicrodroidDeviceTestBase {
 
         final String command = "/apex/com.android.art/bin/odrefresh --force-compile";
 
-        double[] compileTime = new double[ROUND_COUNT];
+        final List<Double> compileTimeArray = new ArrayList<>(ROUND_COUNT);
+        final List<Map<String, Long>> processMaxMemMetricsArray = new ArrayList<>(ROUND_COUNT);
 
         for (int round = 0; round < ROUND_COUNT; ++round) {
+
+            AtomicBoolean isCompilationFinish = new AtomicBoolean(false);
+
+            Thread threadGetMetrics = new Thread() {
+                @Override
+                public void run() {
+
+                    Map<String, Long> processMaxMemMetrics = new HashMap<>();
+
+                    while (true) {
+                        Log.i(TAG, "Start to get metrics");
+                        try {
+                            updateProcessMaxMemMetrics("dex2oat64", processMaxMemMetrics);
+                            Thread.sleep(1000);
+                        } catch (Exception e) {
+                            Log.i(TAG, "Get exception : " + e);
+                        }
+
+                        Log.i(TAG, "Finish to get metrics");
+
+                        if (isCompilationFinish.get()) {
+                            break;
+                        }
+                    }
+
+                    for (Map.Entry<String, Long> stat : processMaxMemMetrics.entrySet()) {
+                        Log.i(TAG, "Get process max metrics : " + stat.getKey().toLowerCase()
+                                + " - " + stat.getValue().toString());
+                    }
+
+                    processMaxMemMetricsArray.add(processMaxMemMetrics);
+                }
+            };
+
+            threadGetMetrics.start();
+
             Timestamp beforeCompileLatestTime = getLatestDex2oatSuccessTime();
             Long compileStartTime = System.nanoTime();
             String output = executeCommand(command);
@@ -189,10 +344,35 @@ public class ComposBenchmark extends MicrodroidDeviceTestBase {
             assertTrue(beforeCompileLatestTime == null
                     || beforeCompileLatestTime.before(afterCompileLatestTime));
 
-            compileTime[round] = (compileEndTime - compileStartTime) / NANOS_IN_SEC;
+            double elapsedSec = (compileEndTime - compileStartTime) / NANOS_IN_SEC;
+            Log.i(TAG, "Compile time in host took " + elapsedSec + "s");
+            isCompilationFinish.set(true);
+
+            Log.i(TAG, "Waits for thread finish.");
+            threadGetMetrics.join();
+            Log.i(TAG, "Thread is finish.");
+
+            compileTimeArray.add(elapsedSec);
         }
 
-        reportMetric("host_compile_time", "s", compileTime);
-    }
+        reportMetric("host_compile_time", "s", compileTimeArray);
 
+        List<String> allMetrics = new ArrayList<>();
+        for (Map<String, Long> processMaxMemMetrics : processMaxMemMetricsArray) {
+            for (Map.Entry<String, Long> stat : processMaxMemMetrics.entrySet()) {
+                if (!allMetrics.contains(stat.getKey())) {
+                    allMetrics.add(stat.getKey());
+                }
+            }
+        }
+        for (String metrics : allMetrics) {
+            List<Double> allValues = new ArrayList<>();
+            for (Map<String, Long> processMaxMemMetrics : processMaxMemMetricsArray) {
+                if (processMaxMemMetrics.containsKey(metrics)) {
+                    allValues.add(processMaxMemMetrics.get(metrics).doubleValue());
+                }
+            }
+            reportMetric("host_compile_dex2oat64_" + metrics, "kb", allValues);
+        }
+    }
 }
