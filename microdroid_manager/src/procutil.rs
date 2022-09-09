6a83b78c2b76a8f2a911c902b88ceb7a17556011
Seungjae Yoo <seungjaeyoo@google.com>
2022-09-09 20:07:00 +0900

    Implement collecting VmStatus in microdroid_manager
    Bug: N/A
    Test: N/A
    Change-Id: I55ebb75f93943a3095f27c74fcef577a1d054865

---
+++ microdroid_manager/src/procutil.rs
@@ -0,0 +1,51 @@
+// Copyright 2022, The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use anyhow::Result;
+use std::fs;
+
+pub struct CpuTime {
+    pub user: i64,
+    pub nice: i64,
+    pub sys: i64,
+    pub idle: i64,
+}
+
+pub struct MemInfo {
+    pub total: i64,
+    pub free: i64,
+    pub available: i64,
+    pub buffer: i64,
+    pub cached: i64,
+}
+
+pub fn get_cpu_time() -> Result<CpuTime> {
+    let proc_stat = fs::read_to_string("/proc/stat")?;
+    let data_list: Vec<_> = proc_stat.split(' ').collect();
+    let cpu_time = CpuTime {
+        user: data_list[2].parse()?,
+        nice: data_list[3].parse()?,
+        sys: data_list[4].parse()?,
+        idle: data_list[5].parse()?,
+    };
+    Ok(cpu_time)
+}
+
+pub fn get_mem_info() -> Result<MemInfo> {
+    // let proc_mem_info = fs::read_to_string("/proc/meminfo")?;
+    // let data_list: Vec<_> = proc_mem_info.split('\n').collect();
+
+    let mem_info = MemInfo { total: 11, free: 11, available: 11, buffer: 11, cached: 11 };
+    Ok(mem_info)
+}
