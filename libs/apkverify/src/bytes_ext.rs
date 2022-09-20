ad2c9471f3e4f9b87ca548355bacb4d20c84bcfa
Alice Wang <aliceywang@google.com>
2022-09-20 10:03:02 +0000

    [apkverify] Refactor find v3 signer for reuse
    This CL refactors the find v3 signer method for reuse and also
    simplifies the existing indirection with callback.
    There is no behavior change with this cl
    
    Bug: 247689066
    Test: libapkverify.integration_test
    Change-Id: I6ae2d89a72d588a05fe5c19bc2afd4d7c8c2e39b

--- libs/apkverify/src/bytes_ext.rs
+++ libs/apkverify/src/bytes_ext.rs
@@ -18,7 +18,7 @@
 
 use anyhow::{ensure, Result};
 use bytes::{Buf, Bytes};
-use std::ops::Deref;
+use std::ops::{Deref, DerefMut};
 
 #[derive(Clone, Debug)]
 pub struct LengthPrefixed<T> {
@@ -29,6 +29,18 @@ impl<T> Deref for LengthPrefixed<T> {
     type Target = T;
     fn deref(&self) -> &Self::Target {
         &self.inner
+    }
+}
+
+impl<T> DerefMut for LengthPrefixed<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.inner
+    }
+}
+
+impl<T> LengthPrefixed<T> {
+    pub fn into_inner(self) -> T {
+        self.inner
     }
 }
 
