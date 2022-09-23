09e4f9462262eeaf6aa6ec8394f3d2b8ff4d190c
Shikha Panwar <shikhapanwar@google.com>
2022-09-23 16:35:07 +0000

    Run protected VMs without pvmfw - transitory
    We do this using crosvm's protected-vm-without-firmware flag. This
    enables us to boot microdroid without bootloader (without requiring
    kernelboot support from the firmware - b/245081929).
    
    Note: This is transitory change & enables easy debugging of the new
    pvm_fw as well.
    
    This patch also adds @Ignore to the tests that rely on pvm_fw
    
    Bug: 244294566
    Bug: 248552675
    Test: atest MicrodroidTests
    Change-Id: I8c83546a7180757acd8585c9499ed25b5b10c87c

--- tests/hostside/java/com/android/microdroid/test/MicrodroidTestCase.java
+++ tests/hostside/java/com/android/microdroid/test/MicrodroidTestCase.java
@@ -424,6 +424,7 @@ public class MicrodroidTestCase extends MicrodroidHostTestCaseBase {
     }
 
     @Test
+    @Ignore("b/248552675")
     @CddTest(requirements = {"9.17/C-2-1", "9.17/C-2-2", "9.17/C-2-6"})
     public void testBootFailsWhenProtectedVmStartsWithImagesSignedWithDifferentKey()
             throws Exception {
