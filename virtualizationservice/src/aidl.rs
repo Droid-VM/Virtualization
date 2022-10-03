4dd34e8f557169204d38711aea1365d8b37c19ac
David Brazdil <dbrazdil@google.com>
2022-10-03 12:18:10 +0100

    Create new virtmgr binary from virtualizationservice
    To bootstrap separation of virtualizationservice into two binaries
    (global service and user instance), start by compiling a second binary
    out of the same source code, just using a diffrent main source file.
    
    The new binary has a command line interface that takes two file
    descriptors - a socketpair() fd to start RpcBinder server on, and
    a pipe() fd to use for signalling when the server has been started.
    
    The process registers a death notification to kill itself when the
    parent process has died.
    
    Bug: 245727626
    Test: builds, not used in tests yet
    Change-Id: I0c4c7892af91b6deb5ece60e0d1b49a2b4356d35

--- virtualizationservice/src/aidl.rs
+++ virtualizationservice/src/aidl.rs
@@ -14,6 +14,7 @@
 
 //! Implementation of the AIDL interface of the VirtualizationService.
 
+use crate::{get_calling_pid, get_calling_uid};
 use crate::atom::{write_vm_booted_stats, write_vm_creation_stats};
 use crate::composite::make_composite_image;
 use crate::crosvm::{CrosvmConfig, DiskFile, PayloadState, VmContext, VmInstance, VmState};
@@ -48,7 +49,7 @@ use anyhow::{anyhow, bail, Context, Result};
 use apkverify::{HashAlgorithm, V4Signature};
 use binder::{
     self, BinderFeatures, ExceptionCode, Interface, LazyServiceGuard, ParcelFileDescriptor,
-    Status, StatusCode, Strong, ThreadState,
+    Status, StatusCode, Strong,
 };
 use disk::QcowFile;
 use libc::VMADDR_CID_HOST;
@@ -73,8 +74,6 @@ use zip::ZipArchive;
 
 /// The unique ID of a VM used (together with a port number) for vsock communication.
 pub type Cid = u32;
-
-pub const BINDER_SERVICE_IDENTIFIER: &str = "android.system.virtualizationservice";
 
 /// Directory in which to write disk image files used while running VMs.
 pub const TEMPORARY_DIRECTORY: &str = "/data/misc/virtualizationservice";
@@ -511,8 +510,8 @@ impl VirtualizationService {
         let state = &mut *self.state.lock().unwrap();
         let console_fd = console_fd.map(clone_file).transpose()?;
         let log_fd = log_fd.map(clone_file).transpose()?;
-        let requester_uid = ThreadState::get_calling_uid();
-        let requester_debug_pid = ThreadState::get_calling_pid();
+        let requester_uid = get_calling_uid();
+        let requester_debug_pid = get_calling_pid();
 
         // Counter to generate unique IDs for temporary image files.
         let mut next_temporary_image_id = 0;
@@ -838,8 +837,8 @@ struct CompositeImageFilenames {
 
 /// Checks whether the caller has a specific permission
 fn check_permission(perm: &str) -> binder::Result<()> {
-    let calling_pid = ThreadState::get_calling_pid();
-    let calling_uid = ThreadState::get_calling_uid();
+    let calling_pid = get_calling_pid();
+    let calling_uid = get_calling_uid();
     // Root can do anything
     if calling_uid == 0 {
         return Ok(());
---
+++ virtualizationservice/src/virtmgr.rs
@@ -0,0 +1,140 @@
+// Copyright 2022, The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Android Virtualization Manager
+
+mod aidl;
+mod atom;
+mod composite;
+mod crosvm;
+mod payload;
+mod selinux;
+
+use crate::aidl::VirtualizationService;
+use android_system_virtualizationservice::aidl::android::system::virtualizationservice::IVirtualizationService::BnVirtualizationService;
+use binder::{BinderFeatures, ProcessState};
+use lazy_static::lazy_static;
+use log::{info, Level};
+use rpcbinder::{FileDescriptorTransportMode, RpcServer};
+use std::fs::File;
+use std::io::Read;
+use std::os::unix::io::{FromRawFd, OwnedFd, RawFd};
+use clap::Parser;
+use nix::unistd::{Pid, Uid};
+use std::os::unix::raw::{pid_t, uid_t};
+use std::sync::Mutex;
+
+const LOG_TAG: &str = "virtmgr";
+
+lazy_static! {
+    static ref PID_PARENT: Pid = Pid::parent();
+    static ref UID_CURRENT: Uid = Uid::current();
+    static ref OWNED_FDS: Mutex<Vec<RawFd>> =
+        Mutex::new(vec![libc::STDIN_FILENO, libc::STDOUT_FILENO, libc::STDERR_FILENO,]);
+}
+
+fn get_calling_pid() -> pid_t {
+    // The caller is the parent of this process.
+    PID_PARENT.as_raw()
+}
+
+fn get_calling_uid() -> uid_t {
+    // The caller and this process share the same UID.
+    UID_CURRENT.as_raw()
+}
+
+#[derive(Parser)]
+struct Args {
+    /// File descriptor inherited from the caller to run RpcBinder server on.
+    /// This should be one end of a socketpair() compatible with RpcBinder's
+    /// UDS bootstrap transport.
+    #[clap(long, parse(try_from_str = parse_fd_arg))]
+    rpc_server_fd: OwnedFd,
+    /// File descriptor inherited from the caller to signal RpcBinder server
+    /// readiness. This should be one end of pipe() and the caller should be
+    /// waiting for HUP on the other end.
+    #[clap(long, parse(try_from_str = parse_fd_arg))]
+    ready_fd: OwnedFd,
+    /// File descriptor inherited from the caller to signal that virtmgr should
+    /// terminate itself. This process will wait for the caller to drop the FD
+    /// and then cleanly shut itself down.
+    #[clap(long, parse(try_from_str = parse_fd_arg))]
+    shutdown_fd: OwnedFd,
+}
+
+fn parse_fd_arg(arg: &str) -> Result<OwnedFd, clap::Error> {
+    let raw_fd = arg.parse::<RawFd>().map_err(|e| {
+        clap::Error::with_description(
+            format!("Failed to parse '{arg}': {e}"),
+            clap::ErrorKind::InvalidValue,
+        )
+    })?;
+
+    // Basic check that the integer value does correspond to a file descriptor.
+    nix::fcntl::fcntl(raw_fd, nix::fcntl::F_GETFD).map_err(|e| {
+        clap::Error::with_description(
+            format!("Invalid file descriptor {raw_fd}: {e}"),
+            clap::ErrorKind::InvalidValue,
+        )
+    })?;
+
+    // Reject RawFds that already have a corresponding OwnedFd, including stdin/stdout/stderr.
+    let mut owned_fds = OWNED_FDS.lock().unwrap();
+    if owned_fds.contains(&raw_fd) {
+        return Err(clap::Error::with_description(
+            format!("File descriptor {raw_fd} already owned"),
+            clap::ErrorKind::InvalidValue,
+        ));
+    }
+    owned_fds.push(raw_fd);
+
+    // SAFETY - Initializing OwnedFd for a RawFd provided in cmdline arguments.
+    // We checked that the integer value corresponds to a valid FD and that this
+    // is the first argument to claim its ownership.
+    Ok(unsafe { OwnedFd::from_raw_fd(raw_fd) })
+}
+
+fn main() {
+    android_logger::init_once(
+        android_logger::Config::default()
+            .with_tag(LOG_TAG)
+            .with_min_level(Level::Info)
+            .with_log_id(android_logger::LogId::System),
+    );
+
+    let args = Args::parse();
+
+    // Start the thread pool for kernel Binder to work properly, especially link_to_death.
+    ProcessState::start_thread_pool();
+
+    let service = VirtualizationService::init();
+    let service =
+        BnVirtualizationService::new_binder(service, BinderFeatures::default()).as_binder();
+
+    let server = RpcServer::new_unix_domain_bootstrap(service, args.rpc_server_fd)
+        .expect("Failed to start RpcServer");
+    server.set_supported_file_descriptor_transport_modes(&[FileDescriptorTransportMode::Unix]);
+
+    info!("Started VirtualizationService RpcServer. Ready to accept connections");
+
+    // Signal readiness to the caller by closing our end of the pipe.
+    drop(args.ready_fd);
+
+    server.start();
+
+    // Wait for the client to close its end of the shutdown pipe.
+    let _ = File::from(args.shutdown_fd).read(&mut [0]);
+
+    info!("Shutting down VirtualizationService RpcServer");
+}
