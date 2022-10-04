8659e8582c6d718d4a474bd8c253e0cf917ef030
Alice Wang <aliceywang@google.com>
2022-10-04 15:24:10 +0000

    [API] Add new service to notify host from microdroid manager
    This CL adds a new service to notify the host from
    microdroid manager instead of the payload process.
    
    Bug: 243512047
    Test: MicrodroidTests microdroid_manager_test
    Change-Id: Ic634944f6dd5266c084f495787bf4568a4d18ed3

---
+++ microdroid_manager/src/vm_payload_service.rs
@@ -0,0 +1,56 @@
+// Copyright 2022, The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Implementation of the AIDL interface `IVirtualMachinePayload`.
+
+use android_system_virtualization_payload::aidl::android::system::virtualization::payload::IVirtualMachinePayload::{BnVirtualMachinePayload, IVirtualMachinePayload};
+use android_system_virtualmachineservice::aidl::android::system::virtualmachineservice::IVirtualMachineService::IVirtualMachineService;
+use anyhow::{Context, Result};
+use binder::{Interface, BinderFeatures, Strong, add_service};
+
+const VM_PAYLOAD_SERVICE_NAME: &str = "virtual_machine_payload_service";
+
+/// Implementation of `IVirtualMachinePayload`.
+struct VirtualMachinePayload {
+    virtual_machine_service: Strong<dyn IVirtualMachineService>,
+}
+
+impl IVirtualMachinePayload for VirtualMachinePayload {
+    fn notifyPayloadReady(&self) -> binder::Result<()> {
+        self.virtual_machine_service.notifyPayloadReady()
+    }
+}
+
+impl Interface for VirtualMachinePayload {}
+
+impl VirtualMachinePayload {
+    /// Creates a new `VirtualMachinePayload` instance from the `IVirtualMachineService` reference.
+    fn new(vm_service: Strong<dyn IVirtualMachineService>) -> Self {
+        Self { virtual_machine_service: vm_service }
+    }
+}
+
+/// Registers the `IVirtualMachinePayload` service.
+pub(crate) fn register_virtual_machine_payload_service(
+    vm_service: Strong<dyn IVirtualMachineService>,
+) -> Result<()> {
+    let vm_payload_binder = BnVirtualMachinePayload::new_binder(
+        VirtualMachinePayload::new(vm_service),
+        BinderFeatures::default(),
+    );
+    add_service(VM_PAYLOAD_SERVICE_NAME, vm_payload_binder.as_binder())
+        .with_context(|| format!("Failed to register service {}", VM_PAYLOAD_SERVICE_NAME))?;
+    log::info!("{} is running", VM_PAYLOAD_SERVICE_NAME);
+    Ok(())
+}
