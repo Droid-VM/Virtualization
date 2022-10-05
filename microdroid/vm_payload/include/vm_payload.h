22df840c514fbeb0d3f44922df13855f85d3747a
Andrew Scull <ascull@google.com>
2022-10-05 17:41:43 +0000

    Migrate DICE access to IVmPayloadService
    Take control of the microdroid interface to DICE so we can eventually
    expose only what we need to with a simple interface.
    
    Merge the DICE logic into microdroid_manager, removing a process,
    removing IDiceMaintenance, and replacing IDiceNode with
    IVmPayloadService for access to the DICE values. The current interface
    is just meant to be enough to migrate current payload but it will need
    to be refined.
    
    The new interface is exposed in the vm_payload library and clients are
    updated to use the new library functions.
    
    Bug: 243133253
    Test: atest MicrodroidTests
    Test: atest ComposHostTestCases
    Change-Id: I2d659576ca3ccdb8f8ffd9d22ff0d1b96e85a3b8

--- microdroid/vm_payload/include/vm_payload.h
+++ microdroid/vm_payload/include/vm_payload.h
@@ -24,6 +24,22 @@ extern "C" {
 /// Returns true if the notification succeeds else false.
 bool notify_payload_ready();
 
+/// Get the VM's attestation chain.
+/// Returns the size of data or 0 on failure.
+/// TODO: don't expose the contained privacy breaking identifiers to the payload
+/// TODO: keep the DICE chain as an internal detail for as long as possible
+size_t get_dice_attestation_chain(void *data, size_t size);
+
+/// Get the VM's attestation CDI.
+/// Returns the size of data or 0 on failure.
+/// TODO: don't expose the raw CDI, only derived values
+size_t get_dice_attestation_cdi(void *data, size_t size);
+
+/// Get the VM's sealing CDI.
+/// Returns the size of data or 0 on failure.
+/// TODO: don't expose the raw CDI, only derived values
+size_t get_dice_sealing_cdi(void *data, size_t size);
+
 #ifdef __cplusplus
 } // extern "C"
 #endif
