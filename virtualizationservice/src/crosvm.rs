7801060249f7745cff6dafbc5bb4244b0a8d414b
David Brazdil <dbrazdil@google.com>
2022-10-05 13:01:03 +0100

    Remove requesterSid from VirtualMachineDebugInfo
    The field is unused.
    
    Test: atest -p packages/modules/Virtualization:avf-presubmit
    Change-Id: I39fa19302b82ae44488366c2603ee5730ddbe737

--- virtualizationservice/src/crosvm.rs
+++ virtualizationservice/src/crosvm.rs
@@ -179,8 +179,6 @@ pub struct VmInstance {
     pub temporary_directory: PathBuf,
     /// The UID of the process which requested the VM.
     pub requester_uid: u32,
-    /// The SID of the process which requested the VM.
-    pub requester_sid: String,
     /// The PID of the process which requested the VM. Note that this process may no longer exist
     /// and the PID may have been reused for a different process, so this should not be trusted.
     pub requester_debug_pid: i32,
@@ -204,7 +202,6 @@ impl VmInstance {
         config: CrosvmConfig,
         temporary_directory: PathBuf,
         requester_uid: u32,
-        requester_sid: String,
         requester_debug_pid: i32,
     ) -> Result<VmInstance, Error> {
         validate_config(&config)?;
@@ -218,7 +215,6 @@ impl VmInstance {
             protected,
             temporary_directory,
             requester_uid,
-            requester_sid,
             requester_debug_pid,
             callbacks: Default::default(),
             stream: Mutex::new(None),
