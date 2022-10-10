64fbf2f2044fd216f635eec31a8ca6d0e2fd449e
David Brazdil <dbrazdil@google.com>
2022-10-10 15:06:02 +0100

    Create IVirtualizationServiceInternal
    Add new AIDL interface for communication between virtmgr and the global
    virtualizationservice. This will be used for internal functionality,
    synchronizing the instances of virtmgr, and never exposed to users.
    
    Move the tombstone server to IVirtualizationServiceInternal. We will
    eventually remove it completely, but for now there should only be one
    instance of it.
    
    Test: -
    Change-Id: I43beac45bb6ed5b60182c62e8bb08d8d702192d0

--- virtualizationservice/src/aidl.rs
+++ virtualizationservice/src/aidl.rs
@@ -31,6 +31,7 @@ use android_system_virtualizationservice::aidl::android::system::virtualizations
     IVirtualMachine::{BnVirtualMachine, IVirtualMachine},
     IVirtualMachineCallback::IVirtualMachineCallback,
     IVirtualizationService::IVirtualizationService,
+    IVirtualizationServiceInternal::IVirtualizationServiceInternal,
     Partition::Partition,
     PartitionType::PartitionType,
     VirtualMachineAppConfig::{Payload::Payload, VirtualMachineAppConfig},
@@ -101,6 +102,28 @@ const ANDROID_VM_INSTANCE_VERSION: u16 = 1;
 const CHUNK_RECV_MAX_LEN: usize = 1024;
 
 const MICRODROID_OS_NAME: &str = "microdroid";
+
+/// Foobar
+#[derive(Debug, Default)]
+pub struct VirtualizationServiceInternal {}
+
+impl VirtualizationServiceInternal {
+    pub fn init() -> VirtualizationServiceInternal {
+        let service = VirtualizationServiceInternal::default();
+
+        std::thread::spawn(|| {
+            if let Err(e) = handle_stream_connection_tombstoned() {
+                warn!("Error receiving tombstone from guest or writing them. Error: {:?}", e);
+            }
+        });
+
+        service
+    }
+}
+
+impl Interface for VirtualizationServiceInternal {}
+
+impl IVirtualizationServiceInternal for VirtualizationServiceInternal {}
 
 /// Implementation of `IVirtualizationService`, the entry point of the AIDL service.
 #[derive(Debug, Default)]
@@ -308,12 +331,6 @@ fn handle_tombstone(stream: &mut VsockStream) -> Result<()> {
 impl VirtualizationService {
     pub fn init() -> VirtualizationService {
         let service = VirtualizationService::default();
-
-        std::thread::spawn(|| {
-            if let Err(e) = handle_stream_connection_tombstoned() {
-                warn!("Error receiving tombstone from guest or writing them. Error: {:?}", e);
-            }
-        });
 
         // binder server for vm
         // reference to state (not the state itself) is copied
