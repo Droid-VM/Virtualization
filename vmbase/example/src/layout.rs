b78bdcdc167bdfe30ce187f0645fbab2ea4508ad
Pierre-Clément Tosi <ptosi@google.com>
2022-10-18 14:02:23 +0100

    vmbase: Export linker-defined MemoryRegions & VAs
    Export the memory regions defined by the linker as safe types.
    
    Bug: 238050226
    Test: atest vmbase_example.integration_test
    Change-Id: I4de9c00c4f3dac869e72d5011f47ce1e5b0f67b6

--- vmbase/example/src/layout.rs
+++ vmbase/example/src/layout.rs
@@ -14,119 +14,31 @@
 
 //! Memory layout.
 
-use aarch64_paging::paging::{MemoryRegion, VirtualAddress};
-use core::arch::asm;
-use core::ops::Range;
-use vmbase::linker;
-use vmbase::println;
-
-/// The first 1 GiB of memory are used for MMIO.
-pub const DEVICE_REGION: MemoryRegion = MemoryRegion::new(0, 0x40000000);
-
-/// Memory reserved for the DTB.
-pub fn dtb_range() -> Range<VirtualAddress> {
-    unsafe {
-        VirtualAddress(&linker::dtb_begin as *const u8 as usize)
-            ..VirtualAddress(&linker::dtb_end as *const u8 as usize)
-    }
-}
-
-/// Executable code.
-pub fn text_range() -> Range<VirtualAddress> {
-    unsafe {
-        VirtualAddress(&linker::text_begin as *const u8 as usize)
-            ..VirtualAddress(&linker::text_end as *const u8 as usize)
-    }
-}
-
-/// Read-only data.
-pub fn rodata_range() -> Range<VirtualAddress> {
-    unsafe {
-        VirtualAddress(&linker::rodata_begin as *const u8 as usize)
-            ..VirtualAddress(&linker::rodata_end as *const u8 as usize)
-    }
-}
-
-/// Initialised writable data.
-pub fn data_range() -> Range<VirtualAddress> {
-    unsafe {
-        VirtualAddress(&linker::data_begin as *const u8 as usize)
-            ..VirtualAddress(&linker::data_end as *const u8 as usize)
-    }
-}
-
-/// Zero-initialised writable data.
-pub fn bss_range() -> Range<VirtualAddress> {
-    unsafe {
-        VirtualAddress(&linker::bss_begin as *const u8 as usize)
-            ..VirtualAddress(&linker::bss_end as *const u8 as usize)
-    }
-}
-
-/// Writable data region for the stack.
-pub fn boot_stack_range() -> Range<VirtualAddress> {
-    unsafe {
-        VirtualAddress(&linker::boot_stack_begin as *const u8 as usize)
-            ..VirtualAddress(&linker::boot_stack_end as *const u8 as usize)
-    }
-}
-
-/// Writable data, including the stack.
-pub fn writable_region() -> MemoryRegion {
-    unsafe {
-        MemoryRegion::new(
-            &linker::data_begin as *const u8 as usize,
-            &linker::boot_stack_end as *const u8 as usize,
-        )
-    }
-}
-
-fn data_load_address() -> VirtualAddress {
-    unsafe { VirtualAddress(&linker::data_lma as *const u8 as usize) }
-}
-
-fn binary_end() -> VirtualAddress {
-    unsafe { VirtualAddress(&linker::bin_end as *const u8 as usize) }
-}
+use vmbase::{layout, println};
 
 pub fn print_addresses() {
-    let dtb = dtb_range();
+    let dtb = layout::dtb_range();
     println!("dtb:        {}..{} ({} bytes)", dtb.start, dtb.end, dtb.end - dtb.start);
-    let text = text_range();
+    let text = layout::text_range();
     println!("text:       {}..{} ({} bytes)", text.start, text.end, text.end - text.start);
-    let rodata = rodata_range();
+    let rodata = layout::rodata_range();
     println!("rodata:     {}..{} ({} bytes)", rodata.start, rodata.end, rodata.end - rodata.start);
-    println!("binary end: {}", binary_end());
-    let data = data_range();
+    println!("binary end: {}", layout::binary_end());
+    let data = layout::data_range();
     println!(
         "data:       {}..{} ({} bytes, loaded at {})",
         data.start,
         data.end,
         data.end - data.start,
-        data_load_address(),
+        layout::data_load_address(),
     );
-    let bss = bss_range();
+    let bss = layout::bss_range();
     println!("bss:        {}..{} ({} bytes)", bss.start, bss.end, bss.end - bss.start);
-    let boot_stack = boot_stack_range();
+    let boot_stack = layout::boot_stack_range();
     println!(
         "boot_stack: {}..{} ({} bytes)",
         boot_stack.start,
         boot_stack.end,
         boot_stack.end - boot_stack.start
     );
-}
-
-/// Bionic-compatible thread-local storage entry, at the given offset from TPIDR_EL0.
-pub fn bionic_tls(off: usize) -> u64 {
-    let mut base: usize;
-    unsafe {
-        asm!("mrs {base}, tpidr_el0", base = out(reg) base);
-        let ptr = (base + off) as *const u64;
-        *ptr
-    }
-}
-
-/// Value of __stack_chk_guard.
-pub fn stack_chk_guard() -> u64 {
-    unsafe { linker::__stack_chk_guard }
 }
