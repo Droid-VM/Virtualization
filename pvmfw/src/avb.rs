5ab0f2e801a5becd258c01873e1ff54e1634cae8
Pierre-Clément Tosi <ptosi@google.com>
2022-10-26 17:40:50 +0100

    pvmfw: Use pvmfw_embedded_key from Rust
    pvmfw will need the key to validate AVB-signed images so convert it to C
    code and compile and link it against Rust, where it is imported through
    extern symbols and abstracted through a safe function.
    
    Bug: 255575669
    Test: Manual run of pvmfw with the increased log level
    Change-Id: I840a32a39f113d6becd50bea42b8552e551fee5f

--- pvmfw/src/avb.rs
+++ pvmfw/src/avb.rs
@@ -12,32 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! pVM firmware.
+//! Image verification.
 
-#![no_main]
-#![no_std]
-
-mod avb;
-mod entry;
-mod exceptions;
-mod helpers;
-mod mmio_guard;
-mod smccc;
-
-use log::{debug, info, trace};
-
-fn main(fdt: &mut [u8], payload: &[u8]) {
-    info!("pVM firmware");
-    debug!(
-        "fdt_address={:#018x}, payload_start={:#018x}, payload_size={:#018x}",
-        fdt.as_ptr() as usize,
-        payload.as_ptr() as usize,
-        payload.len(),
-    );
-
-    let key = avb::PUBLIC_KEY;
-    debug!("AVB public key: Address: {:?}, size: {:#x}", key.as_ptr(), key.len());
-    trace!("{key:02X?}");
-
-    info!("Starting payload...");
-}
+pub static PUBLIC_KEY: &[u8] =
+    include_bytes!("../../../../../external/avb/test/data/testkey_rsa4096_pub.bin");
