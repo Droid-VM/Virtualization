09539a1e1c55f7e88039a3bcfc71f9fb77725e8a
David Brazdil <dbrazdil@google.com>
2022-11-11 15:10:32 +0000

    Run RpcBinder server for each VM on port==CID
    Change CID allocation to allocate CIDs in the 0-65535 range (same as
    ports), with the first 1024 reserved for platform use. Then run
    VirtualMachineService on the same port as the VM's CID.
    
    Change microdroid_manager to find the local CID and use it as port
    number.
    
    Bug: 245727626
    Test: atest -p packages/modules/Virtualization:avf-presubmit
    Change-Id: Ia4c8448f76a0aa015658d6220c11fa73a8da8e73

--- microdroid_manager/src/main.rs
+++ microdroid_manager/src/main.rs
@@ -25,9 +25,7 @@ use crate::dice::{DiceContext, DiceDriver};
 use crate::instance::{ApexData, ApkData, InstanceDisk, MicrodroidData, RootHash};
 use crate::vm_payload_service::register_vm_payload_service;
 use android_system_virtualizationcommon::aidl::android::system::virtualizationcommon::ErrorCode::ErrorCode;
-use android_system_virtualmachineservice::aidl::android::system::virtualmachineservice::IVirtualMachineService::{
-        IVirtualMachineService, VM_BINDER_SERVICE_PORT,
-};
+use android_system_virtualmachineservice::aidl::android::system::virtualmachineservice::IVirtualMachineService::IVirtualMachineService;
 use android_system_virtualization_payload::aidl::android::system::virtualization::payload::IVmPayloadService::VM_APK_CONTENTS_PATH;
 use anyhow::{anyhow, bail, ensure, Context, Error, Result};
 use apkverify::{get_public_key_der, verify, V4Signature};
@@ -148,8 +146,8 @@ fn write_death_reason_to_serial(err: &Error) -> Result<()> {
 }
 
 fn get_vms_rpc_binder() -> Result<Strong<dyn IVirtualMachineService>> {
-    get_vsock_rpc_interface(VMADDR_CID_HOST, VM_BINDER_SERVICE_PORT as u32)
-        .context("Cannot connect to RPC service")
+    let port = vsock::get_local_cid().context("Finding local CID")?;
+    get_vsock_rpc_interface(VMADDR_CID_HOST, port).context("Connecting to IVirtualMachineService")
 }
 
 fn main() -> Result<()> {
