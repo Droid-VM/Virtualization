60f1e6946b058aa7aa4346504943f534ebb9f9d2
Alice Wang <aliceywang@google.com>
2022-12-09 13:37:00 +0000

    [avb] Merge libvbmeta_rust into libavb_nostd and libavb_rs
    This cl splits libvbmeta_rust into std and no_std parts and merges
    the no_std part to libavb_nostd.
    The no_std part can be used later for kernel verification inside
    pvmfw.
    
    Bug: 260563104
    Test: atest libavb_rs.test libapexutil_rust.test avmdtool_tests \
         && m pvmfw_img
    Change-Id: I6dc04f29f27aaa395c83332af6cfa2d3180302ac

--- libs/avb/src/lib.rs
+++ libs/avb/src/lib.rs
@@ -14,8 +14,14 @@
 
 //! This module regroups the rust API for libavb.
 
-#![no_std]
+#![cfg_attr(all(not(feature = "std"), not(test)), no_std)]
+
+extern crate alloc;
 
 mod avb_ops;
+mod descriptor;
+mod vbmeta;
 
 pub use avb_ops::{verify_image, AvbImageVerifyError};
+pub use descriptor::Descriptor;
+pub use vbmeta::{VbMetaImage, VbMetaImageParseError, VbMetaImageVerificationError};
