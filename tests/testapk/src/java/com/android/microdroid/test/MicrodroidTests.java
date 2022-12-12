4075fb7a322def0a66a0b95a69deafac6847f004
Shikha Panwar <shikhapanwar@google.com>
2022-12-12 14:44:03 +0000

    Resizing encryptedstore is incompatible
    Encrypted storage size should be same for 2 VirtualMachineConfig to be
    compatible. We don't support resizing of backing file.
    
    Bug: 259383481
    Bug: 254454175
    Test: MicrodroidTests#vmConfigUnitTests
    Change-Id: Ibb84fafb641e8400fcd0fa6945303b67516f2e1f

--- tests/testapk/src/java/com/android/microdroid/test/MicrodroidTests.java
+++ tests/testapk/src/java/com/android/microdroid/test/MicrodroidTests.java
@@ -300,6 +300,9 @@ public class MicrodroidTests extends MicrodroidDeviceTestBase {
 
         VirtualMachineConfig compatible = maximalBuilder.setNumCpus(1).setMemoryMib(99).build();
         assertThat(compatible.isCompatibleWith(maximal)).isTrue();
+
+        VirtualMachineConfig incompatible = maximalBuilder.setEncryptedStorageKib(1024).build();
+        assertThat(incompatible.isCompatibleWith(maximal)).isFalse();
     }
 
     @Test
