11e322954cb3141d5af9d561d7ea9ef9f04e34c9
David Brazdil <dbrazdil@google.com>
2023-01-07 11:50:21 +0000

    Measure Microdroid boot time in non-debug mode
    Our current benchmarks measure boot time in DEBUG_MODE_FULL and try to
    parse the console output to identify time spent in individual boot
    stages. That is helpful data but it means that the reported overall boot
    time is not representative of production environment.
    
    Rename the existing test to testMicrodroidBootTimeDebug and add
    testMicrodroidBootTime that runs in DEBUG_MODE_NONE.
    
    Test: atest MicrodroidBenchmarkApp
    Change-Id: Iab706340803957ef26c6ec2cede75485c2e04808

--- tests/benchmark/src/java/com/android/microdroid/benchmark/MicrodroidBenchmarks.java
+++ tests/benchmark/src/java/com/android/microdroid/benchmark/MicrodroidBenchmarks.java
@@ -140,6 +140,33 @@ public class MicrodroidBenchmarks extends MicrodroidDeviceTestBase {
 
         final int trialCount = 10;
 
+        List<Double> bootTimeMetrics = new ArrayList<>();
+        for (int i = 0; i < trialCount; i++) {
+            VirtualMachineConfig normalConfig =
+                    newVmConfigBuilder()
+                            .setPayloadBinaryPath("MicrodroidIdleNativeLib.so")
+                            .setDebugLevel(DEBUG_LEVEL_NONE)
+                            .setMemoryMib(256)
+                            .build();
+            forceCreateNewVirtualMachine("test_vm_boot_time", normalConfig);
+
+            BootResult result = tryBootVm(TAG, "test_vm_boot_time");
+            assertThat(result.payloadStarted).isTrue();
+
+            final double nanoToMilli = 1000000.0;
+            bootTimeMetrics.add(result.endToEndNanoTime / nanoToMilli);
+        }
+
+        reportMetrics(bootTimeMetrics, "boot_time", "ms");
+    }
+
+    @Test
+    public void testMicrodroidBootTimeDebug()
+            throws VirtualMachineException, InterruptedException, IOException {
+        assume().withMessage("Skip on CF; too slow").that(isCuttlefish()).isFalse();
+
+        final int trialCount = 10;
+
         List<Double> vmStartingTimeMetrics = new ArrayList<>();
         List<Double> bootTimeMetrics = new ArrayList<>();
         List<Double> bootloaderTimeMetrics = new ArrayList<>();
