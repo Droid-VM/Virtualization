640ee58b22d41c00c2dc394899a051a3b3897911
Shikha Panwar <shikhapanwar@google.com>
2023-01-10 05:25:05 +0000

    Ramdisk: Enable detaching bootconfig from initrd
    This is the reverse of appending bootconfigs to initrd.
    initrd_bootconfig detach-bootconfig takes initrd_with_bootconfig as
    input & outputs the containing ramdisk & bootconfigs.
    
    This is required because our signing infrastructure requires changing
    vbmeta related configs (when images are resigned using a different key).
    This can be achieved by detaching the configs, modifying them &
    re-attaching them.
    
    Bug: 245277660
    Test: Attach & then detach bootconfigs to initrd & the diff the
    artifacts.
    
    Change-Id: Ia8803496549d887dae84cf7c4f545213591c6a78

--- microdroid/initrd/src/main.rs
+++ microdroid/initrd/src/main.rs
@@ -12,29 +12,87 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Append bootconfig to initrd image
-use anyhow::Result;
+//! Attach/Detach bootconfigs to initrd image
+use anyhow::{bail, Result};
 use clap::Parser;
+use std::cmp::min;
 use std::fs::File;
-use std::io::{Read, Write};
+use std::io::{Read, Seek, SeekFrom, Write};
 use std::path::PathBuf;
 
 const FOOTER_ALIGNMENT: usize = 4;
 const ZEROS: [u8; 4] = [0u8; 4_usize];
+const BOOTCONFIG_MAGIC: &str = "#BOOTCONFIG\n";
+const BUFFSIZE: usize = 1024;
 
 #[derive(Parser, Debug)]
-struct Args {
-    /// Initrd (without bootconfig)
-    initrd: PathBuf,
-    /// Bootconfig
-    bootconfigs: Vec<PathBuf>,
-    /// Output
-    #[clap(long = "output")]
-    output: PathBuf,
+enum Opt {
+    // Append bootconfig to initrd image
+    AppendBootconfig {
+        /// Initrd (without bootconfig)
+        initrd: PathBuf,
+        /// Bootconfig
+        bootconfigs: Vec<PathBuf>,
+        /// Output
+        #[clap(long = "output")]
+        output: PathBuf,
+    },
+
+    // Detach the initrd & bootconfigs - this is required for cases when we replace bootconfig values in sign_virt apex
+    DetachBootconfig {
+        /// Initrd (with bootconfig)
+        initrd_with_bootconfig: PathBuf,
+        /// Initrd (without bootconfig)
+        initrd: PathBuf,
+        /// Bootconfig
+        bootconfig: PathBuf,
+    },
 }
 
 fn get_checksum(file_path: &PathBuf) -> Result<u32> {
     File::open(file_path)?.bytes().map(|x| Ok(x? as u32)).sum()
+}
+
+// Note attaching & then detaching bootconfig can lead to extra padding in bootconfigs
+fn detach_bootconfig(initrd_bc: PathBuf, initrd: PathBuf, bootconfig: PathBuf) -> Result<()> {
+    let mut initrd_bc = File::open(&initrd_bc)?;
+    let mut bootconfig = File::create(&bootconfig)?;
+    let mut initrd = File::create(&initrd)?;
+    let initrd_bc_size: usize = initrd_bc.metadata()?.len().try_into()?;
+    let mut buf = vec![0; BUFFSIZE];
+
+    initrd_bc.seek(SeekFrom::End(-(BOOTCONFIG_MAGIC.len() as i64)))?;
+    let mut magic_buf = [0; BOOTCONFIG_MAGIC.len()];
+    initrd_bc.read_exact(&mut magic_buf)?;
+    if magic_buf != BOOTCONFIG_MAGIC.as_bytes() {
+        bail!("BOOTCONFIG_MAGIC not found in initrd. Bootconfigs might not be attached correctly");
+    }
+
+    initrd_bc.seek(SeekFrom::End(-(BOOTCONFIG_MAGIC.len() as i64 + 8)))?;
+    initrd_bc.read_exact(&mut buf[..4])?;
+    let bc_size: usize =
+        u32::from_le_bytes(buf[..4].try_into().expect("slice with incorrect length")) as usize;
+
+    let initrd_size: usize = initrd_bc_size - bc_size - 8 - BOOTCONFIG_MAGIC.len();
+
+    initrd_bc.seek(SeekFrom::Start(0))?;
+    let mut copied: usize = 0;
+    while copied < initrd_size {
+        let n = min(initrd_size - copied, BUFFSIZE);
+        initrd_bc.read_exact(&mut buf[..n])?;
+        initrd.write_all(&buf[..n])?;
+        copied += n;
+    }
+
+    copied = 0;
+    while copied < bc_size {
+        let n = min(bc_size - copied, BUFFSIZE);
+        initrd_bc.read_exact(&mut buf[..n])?;
+        bootconfig.write_all(&buf[..n])?;
+        copied += n;
+    }
+
+    Ok(())
 }
 
 // Bootconfig is attached to the initrd in the following way:
@@ -59,14 +117,21 @@ fn attach_bootconfig(initrd: PathBuf, bootconfigs: Vec<PathBuf>, output: PathBuf
     output_file.write_all(&ZEROS[..padding_size])?;
     output_file.write_all(&((padding_size + bootconfig_size) as u32).to_le_bytes())?;
     output_file.write_all(&checksum.to_le_bytes())?;
-    output_file.write_all(b"#BOOTCONFIG\n")?;
+    output_file.write_all(BOOTCONFIG_MAGIC.as_bytes())?;
     output_file.flush()?;
     Ok(())
 }
 
 fn try_main() -> Result<()> {
-    let args = Args::parse();
-    attach_bootconfig(args.initrd, args.bootconfigs, args.output)?;
+    let args = Opt::parse();
+    match args {
+        Opt::AppendBootconfig { initrd, bootconfigs, output } => {
+            attach_bootconfig(initrd, bootconfigs, output)?
+        }
+        Opt::DetachBootconfig { initrd_with_bootconfig, initrd, bootconfig } => {
+            detach_bootconfig(initrd_with_bootconfig, initrd, bootconfig)?
+        }
+    };
     Ok(())
 }
 
