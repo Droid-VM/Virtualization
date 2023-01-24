25ff3116e4feb236400f0373dd3c272d7172fcca
Alan Stokes <alanstokes@google.com>
2023-01-24 12:18:23 +0000

    Improve assertion
    On failure it would be helpful to see the output of composd_cmd.
    
    Bug: 266470759
    Test: atest ComposBenchmarkApp
    Change-Id: Ic292cef54e1360ae8d14cccae01a7335a8e0cc88

--- compos/benchmark/src/java/com/android/compos/benchmark/ComposBenchmark.java
+++ compos/benchmark/src/java/com/android/compos/benchmark/ComposBenchmark.java
@@ -20,6 +20,7 @@ import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentat
 import static com.google.common.truth.TruthJUnit.assume;
 
 import static org.junit.Assert.assertTrue;
+import static com.google.common.truth.Truth.assertThat;
 
 import android.app.Instrumentation;
 import android.os.Bundle;
@@ -136,9 +137,7 @@ public class ComposBenchmark extends MicrodroidDeviceTestBase {
             Long compileStartTime = System.nanoTime();
             String output = executeCommand(command);
             Long compileEndTime = System.nanoTime();
-            Pattern pattern = Pattern.compile("All Ok");
-            Matcher matcher = pattern.matcher(output);
-            assertTrue(matcher.find());
+            assertThat(output).containsMatch("All Ok");
             double elapsedSec = (compileEndTime - compileStartTime) / NANOS_IN_SEC;
             Log.i(TAG, "Compile time in guest took " + elapsedSec + "s");
             getMetricsRunnable.stop();
