cf871f2b1e237057437c879ad70a359a72551b5c
Alice Wang <aliceywang@google.com>
2023-01-25 14:32:31 +0000

    [avb] Check that pointers are aligned and not null before usage
    This cl added the alignment check to the existing non-null check.
    
    Bug: 256148034
    Test: m pvmfw_img && atest libpvmfw_avb.integration_test
    Change-Id: I7846a52c726b5fbf4cca1df4aa35f70aea86ca49

--- pvmfw/avb/src/utils.rs
+++ pvmfw/avb/src/utils.rs
@@ -36,14 +36,15 @@ pub(crate) fn as_ref<'a, T>(ptr: *mut T) -> Result<&'a T> {
 }
 
 pub(crate) fn to_nonnull<T>(ptr: *mut T) -> Result<NonNull<T>> {
+    is_aligned_and_not_null(ptr)?;
     NonNull::new(ptr).ok_or(AvbIOError::NoSuchValue)
 }
 
-pub(crate) fn is_not_null<T>(ptr: *const T) -> Result<()> {
-    if ptr.is_null() {
-        Err(AvbIOError::NoSuchValue)
-    } else {
+pub(crate) fn is_aligned_and_not_null<T>(ptr: *const T) -> Result<()> {
+    if !ptr.is_null() && ptr.is_aligned() {
         Ok(())
+    } else {
+        Err(AvbIOError::NoSuchValue)
     }
 }
 
