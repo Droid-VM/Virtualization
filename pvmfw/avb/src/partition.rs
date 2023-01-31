b55f835799809ecb986a4655d93703d15de7805e
Alice Wang <aliceywang@google.com>
2023-01-31 12:59:00 +0000

    [refactoring] Use #[default] attribute to create enum default
    Test: atest libpvmfw_avb.integration_test \
    libapkverify.integration_test
    
    Change-Id: Ib618692c6fa0f93db20fc0f934aa3887b7f9fcaa

--- pvmfw/avb/src/partition.rs
+++ pvmfw/avb/src/partition.rs
@@ -20,16 +20,11 @@ use core::ffi::{c_char, CStr};
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub(crate) enum PartitionName {
+    /// The default `PartitionName` is needed to build the default `HashDescriptor`.
+    #[default]
     Kernel,
     InitrdNormal,
     InitrdDebug,
-}
-
-/// This is needed to build the default `HashDescriptor`.
-impl Default for PartitionName {
-    fn default() -> Self {
-        Self::Kernel
-    }
 }
 
 impl PartitionName {
