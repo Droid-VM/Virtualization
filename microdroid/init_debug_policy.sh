70bc1c87541c7cb0dffaf3c5e8739edaa62230aa
Jaewan Kim <jaewan@google.com>
2023-02-13 15:34:56 +0900

    Enable adb and adb root with debug level or debug policy
    adb and adb root are enabled when debug level is full (--debug=full)
    or debug policy allows so.
    
    Debug policy is handled in init.rc instead of changing bootconfig
    behavior. It allows the identity check to detect debug level changes
    by bootconfig in the initrd regardless of debug policy.
    
    Bug: 268270904, Bug: 26826551
    Test: Manual test with custom debug policy
    Change-Id: Ie6b0a6ae61e387d28873db91f1dcf08941b2fe61

---
+++ microdroid/init_debug_policy.sh
@@ -0,0 +1,37 @@
+#!/system/bin/sh
+# Copyright 2023 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Applies debug policies when booting microdroid
+
+avf_debug_policy_root="/sys/firmware/devicetree/base/avf"
+
+# If VM is debuggable or debug policy says so, send logs to outside ot the VM via the serial console.
+# Otherwise logs are internally consumed at /dev/null
+if [[ "$(getprop ro.boot.microdroid.debuggable)" == "1"
+        || $(xxd -p "${avf_debug_policy_root}/guest/common/log") == "00000001" ]]; then
+    setprop ro.log.file_logger.path /dev/hvc2
+else
+    setprop ro.log.file_logger.path /dev/null
+fi
+
+if [[ "$(getprop ro.boot.adb.enabled)" == "1"
+        || $(xxd -p "${avf_debug_policy_root}/guest/microdroid/adb") == "00000001" ]]; then
+    setprop ro.boot.adb.enabled 1
+    # This will enable adb root and also bypass adb authorization
+    setprop ro.debuggable 1
+else
+    setprop ro.boot.adb.enabled 0
+    setprop ro.debuggable 0
+fi
