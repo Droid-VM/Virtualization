f174dcc3b312c72dc27c540af5443bb63619e2f1
Jiyong Park <jiyong@google.com>
2023-03-13 05:43:23 +0000

    pvmfw: sanitize incoming device tree
    WIP
    
    Bug: 249054080
    Test: WIP
    
    Change-Id: Ie55fa4ba70c807c729407c136c5b7d262473dae5

--- pvmfw/src/fdt.rs
+++ pvmfw/src/fdt.rs
@@ -14,10 +14,15 @@
 
 //! High-level FDT functions.
 
+use crate::RebootReason;
+use crate::helpers::GUEST_PAGE_SIZE;
 use core::ffi::CStr;
 use core::ops::Range;
 use libfdt::Fdt;
 use libfdt::FdtError;
+use log::error;
+
+const RAM_BASE_ADDR: u64 = 0x8000_0000;
 
 /// Extract from /config the address range containing the pre-loaded kernel.
 pub fn kernel_range(fdt: &libfdt::Fdt) -> libfdt::Result<Option<Range<usize>>> {
@@ -49,6 +54,151 @@ pub fn initrd_range(fdt: &libfdt::Fdt) -> libfdt::Result<Option<Range<usize>>> {
     }
 
     Ok(None)
+}
+
+/// Read and validate the size and base address of memory, and returns the size
+fn parse_memory_node(fdt: &libfdt::Fdt) -> Result<usize, RebootReason> {
+    let memory_range = fdt.memory()
+        // actually, these checks are unnecessary because we read /memory node in entry.rs
+        // where the exact same checks are done. we are repeating the same check just for
+        // extra safety (in case when the code structure changes in the future).
+        .map_err(|e| {
+            error!("Failed to get /memory from the DT: {e}");
+            RebootReason::InvalidFdt
+        })?
+        .ok_or_else(|| {
+            error!("Node /memory was found empty");
+            RebootReason::InvalidFdt
+        })?
+        .next()
+        .ok_or_else(|| {
+            error!("Failed to read memory range from the DT");
+            RebootReason::InvalidFdt
+        })?;
+
+    let base = memory_range.start;
+    if base as u64 != RAM_BASE_ADDR {
+        error!("Memory base address {:#x} is not {:#x}", base, RAM_BASE_ADDR);
+        return Err(RebootReason::InvalidFdt);
+    }
+
+    let size = memory_range.end - memory_range.start;
+    if size % GUEST_PAGE_SIZE != 0 {
+        error!("Memory size {:#x} is not a multiple of page size {:#x}", size, GUEST_PAGE_SIZE);
+        return Err(RebootReason::InvalidFdt);
+    }
+    // In the u-boot implementation, we checked if base + size > u64::MAX, but we don't need that
+    // because memory() function uses checked_add when constructing the Range object. If an
+    // overflow happened, we should have gotten None from the next() call above and would have
+    // bailed already.
+
+    Ok(size)
+}
+
+/// Read the number of CPUs
+fn parse_cpu_nodes(fdt: &libfdt::Fdt) -> Result<usize, RebootReason> {
+    Ok(fdt.compatible_nodes(CStr::from_bytes_with_nul(b"arm,arm-v8\0").unwrap())
+        .map_err(|e| {
+            error!("Failed to read compatible nodes \"arm,arm-v8\" from DT: {e}");
+            RebootReason::InvalidFdt
+        })?
+        .count())
+}
+
+struct PciInfo {
+    low_addr: u64,
+    low_size: u64,
+    high_addr: u64,
+    high_size: u64,
+    num_irq: usize,
+}
+
+/// Read and validate PCI node
+fn parse_pci_nodes(fdt: &libfdt::Fdt) -> Result<PciInfo, RebootReason> {
+    let node = fdt.compatible_nodes(CStr::from_bytes_with_nul(b"pci-host-cam-generic\0").unwrap())
+        .map_err(|e| {
+            error!("Failed to read compatible node \"pci-host-cam-generic\" from DT: {e}");
+            RebootReason::InvalidFdt
+        })?
+        .next()
+        .ok_or_else(|| {
+            error("Compatible node \"pci-host-cam-generic\" doesn't exist");
+            RebootReason::InvalidFdt // why should this be an error? a VM without any pci is
+                                     // possible?
+        })?;
+
+    let iter = node.ranges::<(u32, u64), u64, u64>()
+        .map_err(|e| {
+            error!("Failed to read ranges from PCI node: {e}");
+            RebootReason::InvalidFdt
+        })?
+        .ok_or_else(|| {
+            error!("PCI node missing ranges property");
+            RebootReason::InvalidFdt
+        })?;
+
+    let range_low = iter.next().ok_or_else(|| {
+        error!("Low range missing in PCI node");
+        RebootReason
+    })?;
+    validate_pci_range(&range_low)?;
+
+    let range_high = iter.next().ok_or_else(|| {
+        error!("High range missing in PCI node");
+        RebootReason
+    })?;
+    validate_pci_range(&range_high)?;
+
+
+}
+
+fn validate_pci_range(range: &AddressRange<(u32, u64), u64, u64>) -> Result<(), RebootReason> {
+    let range_type = range.addr.0.range_type();
+    let bus_addr = range.addr.1;
+    let cpu_addr = range.parent;
+    let size = range.size;
+    if range_type != PciRangeType::Memory64 {
+        error!("Invalid range type {:?} in PCI node", range_type);
+        return Err(InvalidFdt);
+    }
+    if bus_addr != cpu_addr {
+        error!("PCI bus address: {:#x} is different from CPU address: {:#x}", bus_addr, cpu_addr);
+        return Err(InvalidFdt);
+    }
+    if bus_addr.checked_add(size).is_none() {
+        error!("PCI address range size {:#x} too big", size);
+        return Err(InvalidFdt);
+    }
+    Ok(())
+}
+
+/// Iterator over N cells as a chunk
+struct CellChunkIterator<'a> {
+    cells: CellIterator<'a>,
+    num_cells: usize,
+}
+
+impl<'a> CellChunkIterator<'a> {
+    fn new(cells: CellIterator<'a>, num_cells: usize) -> Self {
+        Self { cells, num_cells }
+    }
+}
+
+impl<'a> Iterator for CellChunkIterator<'a> {
+    type Item = [u32];
+    fn next(&mut self) -> Option<Self::Item> {
+
+    }
+}
+
+fn count_and_validate_pci_irq_masks(pciNode: &libfdt::FdtNode) -> Result<usize, RebootReason> {
+    let name = CStr::from_bytes_with_nul(b"interrupt-map-mask\0").unwrap();
+    let count = pciNode.getprop_cells(&name).count();
+
+    pciNode.getprop_cells
+    let count = pciNode.getprop_cells(Cstr::from_bytes_with_nul(b"interrupt-map-mask\0").unwrap()).count();
+
+    let node = fdt.compatible_nodes(CStr::from_bytes_with_nul(b"pci-host-cam-generic\0").unwrap())
 }
 
 /// Modifies the input DT according to the fields of the configuration.
