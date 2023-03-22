048693ff7422b801b332aa6295ecd235626444a0
Jakob Vukalovic <jakobvukalovic@google.com>
2023-03-22 11:23:44 +0000

    pvmfw: Optimize MMIO guard map
    Optimize MMIO guard mapping device pages by invalidating (unmapping) the
    pages in order to produce translation faults. When a translation fault
    happens, re-enable the pages and only then MMIO guard map the pages.
    
    Bug: 245267332
    Test: atest MicrodroidTestApp
    
    Change-Id: I81128d7efec1249a9a7da988ec098b29936338ef

--- pvmfw/src/exceptions.rs
+++ pvmfw/src/exceptions.rs
@@ -14,21 +14,71 @@
 
 //! Exception handlers.
 
+use crate::memory::{MemoryTrackerError, MEMORY};
 use crate::{helpers::page_4kb_of, read_sysreg};
+use core::fmt;
 use vmbase::console;
 use vmbase::{console::emergency_write_str, eprintln, power::reboot};
 
 const ESR_32BIT_EXT_DABT: usize = 0x96000010;
 const UART_PAGE: usize = page_4kb_of(console::BASE_ADDRESS);
+const ESR_32BIT_TRANSL_FAULT_BASE: usize = 0x96000004;
+const ESR_32BIT_TRANSL_FAULT_ISS_MASK: usize = !0x43;
+
+#[derive(Debug)]
+enum HandleExceptionError {
+    PageTableUnavailable,
+    PageTableNotInitialized,
+    InternalError(MemoryTrackerError),
+    UnknownException,
+}
+
+impl fmt::Display for HandleExceptionError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::PageTableUnavailable => write!(f, "Page table is not available."),
+            Self::PageTableNotInitialized => write!(f, "Page table is not initialized."),
+            Self::InternalError(e) => write!(f, "Error while updating page table: {e}"),
+            Self::UnknownException => write!(f, "An unknown exception occurred, not handled."),
+        }
+    }
+}
+
+impl From<MemoryTrackerError> for HandleExceptionError {
+    fn from(other: MemoryTrackerError) -> Self {
+        Self::InternalError(other)
+    }
+}
+
+fn handle_exception(esr: usize, far: usize) -> Result<(), HandleExceptionError> {
+    let mut locked = MEMORY.try_lock().ok_or(HandleExceptionError::PageTableUnavailable)?;
+    let memory = locked.as_mut().ok_or(HandleExceptionError::PageTableNotInitialized)?;
+    // Handle all translation faults on both read and write, and MMIO guard map
+    // flagged invalid pages or blocks that caused the exception.
+    if esr & ESR_32BIT_TRANSL_FAULT_ISS_MASK == ESR_32BIT_TRANSL_FAULT_BASE {
+        memory.handle_mmio_fault(far).map_err(|e| e.into())
+    } else {
+        Err(HandleExceptionError::UnknownException)
+    }
+}
 
 #[no_mangle]
 extern "C" fn sync_exception_current(_elr: u64, _spsr: u64) {
     let esr = read_sysreg!("esr_el1");
     let far = read_sysreg!("far_el1");
+    let is_uart = page_4kb_of(far) == UART_PAGE;
+
     // Don't print to the UART if we're handling the exception it could raise.
-    if esr != ESR_32BIT_EXT_DABT || page_4kb_of(far) != UART_PAGE {
+    if esr != ESR_32BIT_EXT_DABT || !is_uart {
         emergency_write_str("sync_exception_current\n");
-        eprintln!("esr={esr:#08x}");
+    }
+    match handle_exception(esr, far) {
+        Ok(()) => return,
+        Err(e) if !is_uart => {
+            eprintln!("{e}");
+            eprintln!("esr={esr:#08x}, far={far:#08x}");
+        }
+        _ => (),
     }
     reboot();
 }
