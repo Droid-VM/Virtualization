5e265e332eb40b4157dc5db165f563847f3d77fb
Jaewan Kim <jaewan@google.com>
2023-03-24 14:37:39 +0900

    Ensure no AVF debug policy in locked device
    VM can't know whether the host is locked or unlocked, so it will
    mindlessly enable debug policies in its device tree.
    
    Add CddTest and documentation to ensure that debug policy doesn't exist
    in locked devices.
    
    Test: atest
    Bug: 275328279
    Change-Id: I77d6cec6416200ffe9e60914aed00bcf0d9edbe7

--- tests/hostside/java/com/android/microdroid/test/MicrodroidHostTests.java
+++ tests/hostside/java/com/android/microdroid/test/MicrodroidHostTests.java
@@ -927,6 +927,21 @@ public class MicrodroidHostTests extends MicrodroidHostTestCaseBase {
         checkHashAlgorithm(virtApexEtcDir);
     }
 
+    @Test
+    @CddTest
+    public void testNoAvfDebugPolicyInLockedDevice() throws Exception {
+        ITestDevice device = getDevice();
+
+        String lockProp = device.getProperty("ro.boot.flash.locked");
+        assertThat(lockProp).isNotNull();
+        assertThat(lockProp).matches("0|1");
+        assumeTrue("Unlocked devices may have AVF debug policy", lockProp.equals("1"));
+
+        // Test that AVF debug policy doesn't exist.
+        boolean hasDebugPolicy = device.doesFileExist("/sys/firmware/devicetree/base/avf");
+        assertThat(hasDebugPolicy).isFalse();
+    }
+
     private String avbInfo(String image_path) throws Exception {
         File avbtool = findTestFile("avbtool");
         List<String> command =
