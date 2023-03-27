d7f65ced6d483871055e6c68b602692cad9b5c48
Shikha Panwar <shikhapanwar@google.com>
2023-03-27 18:46:48 +0000

    Encryptedstore: Enable 64-bit fs & extents
    mkfs skips these features when mounted on limited disk
    size otherwise.
    
    Test: atest #encryptedStorageAvailable with limited size
    Bug: 268648217
    Change-Id: I191cbfe9a0cb170a52eb18fe0f5a552a4a001c4a

--- encryptedstore/src/main.rs
+++ encryptedstore/src/main.rs
@@ -125,9 +125,10 @@ fn needs_formatting(data_device: &Path) -> Result<bool> {
 
 fn format_ext4(device: &Path) -> Result<()> {
     let mkfs_options = [
-        "-j",               // Create appropriate sized journal
-        "-O metadata_csum", // Metadata checksum for filesystem integrity
-        "-b 4096",          // block size in the filesystem
+        "-j", // Create appropriate sized journal
+        // metadata_csum: enabled for filesystem integrity
+        "-O metadata_csum, extents, 64bit",
+        "-b 4096", // block size in the filesystem
     ];
     let mut cmd = Command::new(MK2FS_BIN);
     let status = cmd
