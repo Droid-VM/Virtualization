2fd6288a84ad54c3c95eae1077de23daca96df28
Pierre-Clément Tosi <ptosi@google.com>
2023-04-11 16:14:07 +0100

    pvmfw: rand: Add driver for ARMv8.5 FEAT_RND
    On CPUs that support it, make use of the RNDRRS system register to
    obtain true entropy, instead of the SMCCC TRNG.
    
    Bug: 276303256
    Test: atest MicrodroidHostTests
    Change-Id: Iffe5d99ac3feaec685671c3edc18134d93201935

---
+++ pvmfw/src/rand/rndr.rs
@@ -0,0 +1,75 @@
+// Copyright 2023, The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use core::cmp;
+use core::num::NonZeroUsize;
+
+use crate::read_sysreg;
+
+use super::Error;
+use super::Result;
+
+pub struct Entropy;
+
+macro_rules! read_rnd_reg {
+    ($rndr:literal) => {{
+        let mut r: usize;
+        let mut failed: usize;
+        // Safe because it doesn't access main memory and we don't pass options(preserves_flags).
+        unsafe {
+            core::arch::asm!(
+                concat!("mrs {r}, ", $rndr),
+                // Reading RNDR(RS) sets PSTATE.NZCV to 0b0000 on success, 0b0100 otherwise.
+                "cset {failed}, eq",
+                r = out(reg) r,
+                failed = out(reg) failed,
+                options(nomem, nostack),
+            )
+        }
+        if failed == 0 { Some(r) } else { None }
+    }};
+}
+
+impl super::Entropy for Entropy {
+    fn init(&self) -> Result<()> {
+        const ID_REG_FIELD_MASK: usize = 0xF;
+        const ID_AA64ISAR0_EL1_RNDR_SHIFT: u32 = 60;
+        let id_aa64isar0_el1 = read_sysreg!("id_aa64isar0_el1");
+
+        if ((id_aa64isar0_el1 >> ID_AA64ISAR0_EL1_RNDR_SHIFT) & ID_REG_FIELD_MASK) == 0 {
+            Err(Error::RndrUnavailable)
+        } else {
+            Ok(())
+        }
+    }
+
+    fn fill_partial(&self, s: &mut [u8]) -> Result<Option<NonZeroUsize>> {
+        if s.is_empty() {
+            return Ok(None);
+        }
+
+        // RNDRRS = (0b11, 0b011, 0b0010, 0b0100, 0b001) is not known by the toolchain.
+        let size = if let Some(entropy) = read_rnd_reg!("s3_3_c2_c4_1") {
+            let entropy = entropy.to_ne_bytes();
+            let chunk_len = cmp::min(s.len(), entropy.len());
+            let chunk = &mut s[..chunk_len];
+            chunk.clone_from_slice(&entropy[..chunk_len]);
+            chunk_len
+        } else {
+            0
+        };
+
+        Ok(NonZeroUsize::new(size))
+    }
+}
