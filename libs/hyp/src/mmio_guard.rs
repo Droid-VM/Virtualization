a473d45d5544b671caa784b1dda24477eeeb62b8
Alice Wang <aliceywang@google.com>
2023-04-13 09:02:36 +0000

    [hypervisor] Add hypervisor backend trait to facilitate hyp support
    This cl adds a hypervisor backend trait to make it easier to support
    different hypervisors in the future for both Rialto and pvmfw
    (e.g. aosp/2479400).
    
    Test: m pvmfw_img
    Bug: 272226230
    Change-Id: I0a1b568f37b819c047ae3b09048a791cb40f3372

--- libs/hyp/src/mmio_guard.rs
+++ libs/hyp/src/mmio_guard.rs
@@ -14,7 +14,7 @@
 
 //! Safe MMIO_GUARD support.
 
-use crate::hypervisor::{mmio_guard_enroll, mmio_guard_info, mmio_guard_map, mmio_guard_unmap};
+use crate::hypervisor::get_hypervisor;
 use crate::util::{page_address, SIZE_4KB};
 use core::{fmt, result};
 
@@ -50,8 +50,9 @@ pub type Result<T> = result::Result<T, Error>;
 
 /// Initializes the hypervisor by enrolling a MMIO guard and checking the memory granule size.
 pub fn init() -> Result<()> {
-    mmio_guard_enroll().map_err(Error::EnrollFailed)?;
-    let mmio_granule = mmio_guard_info().map_err(Error::InfoFailed)? as usize;
+    let hyp = get_hypervisor();
+    hyp.mmio_guard_enroll().map_err(Error::EnrollFailed)?;
+    let mmio_granule = hyp.mmio_guard_info().map_err(Error::InfoFailed)? as usize;
     if mmio_granule != SIZE_4KB {
         return Err(Error::UnsupportedGranule(mmio_granule));
     }
@@ -60,10 +61,10 @@ pub fn init() -> Result<()> {
 
 /// Maps a memory address to the hypervisor MMIO guard.
 pub fn map(addr: usize) -> Result<()> {
-    mmio_guard_map(page_address(addr)).map_err(Error::MapFailed)
+    get_hypervisor().mmio_guard_map(page_address(addr)).map_err(Error::MapFailed)
 }
 
 /// Unmaps a memory address from the hypervisor MMIO guard.
 pub fn unmap(addr: usize) -> Result<()> {
-    mmio_guard_unmap(page_address(addr)).map_err(Error::UnmapFailed)
+    get_hypervisor().mmio_guard_unmap(page_address(addr)).map_err(Error::UnmapFailed)
 }
