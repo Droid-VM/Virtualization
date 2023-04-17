d257e200a2cf2c67888d97fa9542a3e29bae8dcd
Jakob Vukalovic <jakobvukalovic@google.com>
2023-04-17 19:10:57 +0100

    pvmfw: Manage dirty state in hardware if possible
    Manage dirty state in hardware on devices that support `FEAT_HAFDBS`.
    Check presence of feature and enable dirty state management in TCR_EL1.
    
    Bug: 269738062
    Test: atest MicrodroidTestApp
    
    Change-Id: If9044702a02fa2f29a747d9d80cc958773bdfe17

--- pvmfw/src/entry.rs
+++ pvmfw/src/entry.rs
@@ -25,6 +25,7 @@ use crate::rand;
 use core::arch::asm;
 use core::num::NonZeroUsize;
 use core::slice;
+use core::sync::atomic::{compiler_fence, Ordering};
 use hyp::get_hypervisor;
 use log::debug;
 use log::error;
@@ -207,6 +208,9 @@ fn main_wrapper(fdt: usize, payload: usize, payload_size: usize) -> Result<usize
 
     let (bcc_slice, debug_policy) = appended.get_entries();
 
+    // Activate dirty state management first, otherwise we may immediately get permission faults.
+    MemoryTracker::track_dirty_pages();
+    compiler_fence(Ordering::SeqCst);
     debug!("Activating dynamic page table...");
     // SAFETY - page_table duplicates the static mappings for everything that the Rust code is
     // aware of so activating it shouldn't have any visible effect.
@@ -236,7 +240,9 @@ fn main_wrapper(fdt: usize, payload: usize, payload_size: usize) -> Result<usize
         error!("Failed to unshare the UART: {e}");
         RebootReason::InternalError
     })?;
-    MEMORY.lock().take().unwrap();
+
+    // Drop MemoryTracker and deactivate page table.
+    core::mem::drop(MEMORY.lock().take());
 
     Ok(slices.kernel.as_ptr() as usize)
 }
