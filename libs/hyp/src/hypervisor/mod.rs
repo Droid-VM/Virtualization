d4b9a08a208f20bbd3927771ccdf99111cc0193e
Srivatsa Vaddagiri <quic_svaddagi@quicinc.com>
2023-04-18 23:49:01 -0700

    [hypervisor] Detect hypervisor
    Detect underlying hypervisor as a first step for being able to do
    hypervisor-specific operations.
    
    Test: m pvmfw_img
    Bug: 271493784
    Change-Id: I82cc31f3b852bd295eedd492a20e4ead5d128bf0
    Signed-off-by: Srivatsa Vaddagiri <quic_svaddagi@quicinc.com>

--- libs/hyp/src/hypervisor/mod.rs
+++ libs/hyp/src/hypervisor/mod.rs
@@ -14,14 +14,20 @@
 
 //! Wrappers around hypervisor back-ends.
 
+extern crate alloc;
+
 mod common;
 mod kvm;
 
+use crate::error::{Error, Result};
+use alloc::boxed::Box;
 pub use common::Hypervisor;
 pub use kvm::KvmError;
 use kvm::KvmHypervisor;
-
-static HYPERVISOR: HypervisorBackend = HypervisorBackend::Kvm;
+use log::debug;
+use once_cell::race::OnceBox;
+use psci::smccc::hvc64;
+use uuid::Uuid;
 
 enum HypervisorBackend {
     Kvm,
@@ -35,7 +41,61 @@ impl HypervisorBackend {
     }
 }
 
+impl TryFrom<Uuid> for HypervisorBackend {
+    type Error = Error;
+
+    fn try_from(uuid: Uuid) -> Result<HypervisorBackend> {
+        match uuid {
+            KvmHypervisor::UUID => {
+                debug!("Detected KVM Hypervisor");
+                Ok(HypervisorBackend::Kvm)
+            }
+            u => {
+                debug!("Unknown hypervisor UUID {}", u.urn());
+                Err(Error::UnsupportedHypervisorUuid(u))
+            }
+        }
+    }
+}
+
+const ARM_SMCCC_VENDOR_HYP_CALL_UID_FUNC_ID: u32 = 0x8600ff01;
+
+fn query_vendor_hyp_call_uid() -> Uuid {
+    let args = [0u64; 17];
+    let mut uid = [0u32; 4];
+    let res = hvc64(ARM_SMCCC_VENDOR_HYP_CALL_UID_FUNC_ID, args);
+
+    // Taking KVM's UUID as reference, "28b46fb6-2ec5-11e9-a9ca-4b564d003a74",
+    // Uuid::from_u128() expects the input u128 value to be in the same order
+    // i.e. 0x28b46fb6_2ec511e9_a9ca4b56_4d003a74. ARM's SMC calling convention
+    // (Document number ARM DEN 0028E) describes the UUID register mapping such
+    // that W0 contains bytes 0..3 of UUID, with byte 0 in lower order bits. In
+    // the KVM example, byte 0 (0x28) will be returned in the low 8-bits of W0,
+    // while byte 15 (0x74) will be returned in bits 31-24 of W3.
+    //
+    // Do some byte swapping to present the 128-bit value in the order expected
+    // by Uuid::from_u128() function.
+
+    uid[0] = (res[0] as u32).swap_bytes();
+    uid[1] = (res[1] as u32).swap_bytes();
+    uid[2] = (res[2] as u32).swap_bytes();
+    uid[3] = (res[3] as u32).swap_bytes();
+
+    Uuid::from_u128(
+        ((uid[0] as u128) << 96)
+            | ((uid[1] as u128) << 64)
+            | ((uid[2] as u128) << 32)
+            | (uid[3] as u128),
+    )
+}
+
+fn detect_hypervisor() -> HypervisorBackend {
+    query_vendor_hyp_call_uid().try_into().expect("Unknown hypervisor")
+}
+
 /// Gets the hypervisor singleton.
 pub fn get_hypervisor() -> &'static dyn Hypervisor {
-    HYPERVISOR.get_hypervisor()
+    static HYPERVISOR: OnceBox<HypervisorBackend> = OnceBox::new();
+
+    HYPERVISOR.get_or_init(|| Box::new(detect_hypervisor())).get_hypervisor()
 }
