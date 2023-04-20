cbee690052243acf6e103f31f8e2f18bc72c6415
Jakob Vukalovic <jakobvukalovic@google.com>
2023-04-20 10:04:52 +0100

    pvmfw: Optimize MMIO guard map
    Optimize MMIO guard mapping device pages by mapping the pages as invalid
    in order to produce translation faults. When a translation fault
    happens, re-enable the pages and only then MMIO guard map the pages.
    
    Bug: 245267332
    Test: atest MicrodroidTestApp
    
    Change-Id: I81128d7efec1249a9a7da988ec098b29936338ef

--- pvmfw/src/entry.rs
+++ pvmfw/src/entry.rs
@@ -226,14 +226,7 @@ fn main_wrapper(fdt: usize, payload: usize, payload_size: usize) -> Result<usize
     })?;
 
     // This wrapper allows main() to be blissfully ignorant of platform details.
-    crate::main(
-        slices.fdt,
-        slices.kernel,
-        slices.ramdisk,
-        bcc_slice,
-        debug_policy,
-        MEMORY.lock().as_mut().unwrap(),
-    )?;
+    crate::main(slices.fdt, slices.kernel, slices.ramdisk, bcc_slice, debug_policy)?;
 
     helpers::flushed_zeroize(bcc_slice);
     helpers::flush(slices.fdt.as_slice());
