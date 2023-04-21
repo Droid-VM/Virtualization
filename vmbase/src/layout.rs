0b3b2312770a6ad9a31c6d2e319fe1fc03dba0b9
Pierre-Clément Tosi <ptosi@google.com>
2023-04-21 16:10:56 +0100

    vmbase: Map .bss, .data, & stack separately
    Map the "scratch" (.data & .bss) and stack regions separately to allow
    them to be placed in non-contiguous regions.
    
    Note: No functional change intended.
    
    Test: atest vmbase_example.integration_test
    Test: atest rialto_test
    Test: atest MicrodroidTests
    Change-Id: I6c3f8e4957e39e1c966a219149b253360782ba8e

--- vmbase/src/layout.rs
+++ vmbase/src/layout.rs
@@ -68,9 +68,9 @@ pub fn boot_stack_range() -> Range<usize> {
     linker_region!(boot_stack_begin, boot_stack_end)
 }
 
-/// Writable data, including the stack.
-pub fn writable_region() -> Range<usize> {
-    linker_region!(data_begin, boot_stack_end)
+/// Allocated writable data.
+pub fn scratch_range() -> Range<usize> {
+    linker_region!(data_begin, bss_end)
 }
 
 /// Read-write data (original).
