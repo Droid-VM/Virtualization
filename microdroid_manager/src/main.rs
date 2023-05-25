3f2317bb42e5736d6d1905bfe296f4218a98e868
Alice Wang <aliceywang@google.com>
2023-05-25 09:37:38 +0000

    [binder] Create RpcServer with new_raw_socket in microdroid_manager
    This cl is part of the change to replace the init-dependent binder
    API ARpcServer_newInitUnixDomain with a more generic version
    ARpcServer_newRawSocket.
    
    Test: atest microdroid_manager_test
    Bug: 275729094
    Change-Id: I6e54166f919f0033bf5357cd0e1ece4e9d54f763

--- microdroid_manager/src/main.rs
+++ microdroid_manager/src/main.rs
@@ -59,6 +59,7 @@ use std::fs::{self, create_dir, OpenOptions, File};
 use std::io::{Read, Write};
 use std::os::unix::process::CommandExt;
 use std::os::unix::process::ExitStatusExt;
+use std::os::unix::io::RawFd;
 use std::path::Path;
 use std::process::{Child, Command, Stdio};
 use std::str;
@@ -191,21 +192,21 @@ fn main() -> Result<()> {
     })
 }
 
-fn set_cloexec_on_vm_payload_service_socket() -> Result<()> {
+fn prepare_vm_payload_service_socket() -> Result<RawFd> {
     let fd = android_get_control_socket(VM_PAYLOAD_SERVICE_SOCKET_NAME)?;
 
-    fcntl(fd, F_SETFD(FdFlag::FD_CLOEXEC))?;
+    if let Err(e) = fcntl(fd, F_SETFD(FdFlag::FD_CLOEXEC)) {
+        warn!("Failed to set cloexec on vm payload socket: {:?}", e);
+    }
 
-    Ok(())
+    Ok(fd)
 }
 
 fn try_main() -> Result<()> {
     let _ignored = kernlog::init();
     info!("started.");
 
-    if let Err(e) = set_cloexec_on_vm_payload_service_socket() {
-        warn!("Failed to set cloexec on vm payload socket: {:?}", e);
-    }
+    let vm_payload_service_fd = prepare_vm_payload_service_socket()?;
 
     load_crashkernel_if_supported().context("Failed to load crashkernel")?;
 
@@ -216,7 +217,7 @@ fn try_main() -> Result<()> {
         .context("cannot connect to VirtualMachineService")
         .map_err(|e| MicrodroidError::FailedToConnectToVirtualizationService(e.to_string()))?;
 
-    match try_run_payload(&service) {
+    match try_run_payload(&service, vm_payload_service_fd) {
         Ok(code) => {
             if code == 0 {
                 info!("task successfully finished");
@@ -330,7 +331,10 @@ fn get_debug_policy_bool(path: &'static str) -> Result<Option<bool>> {
     Ok(Some(u32::from_be_bytes(log) == 1))
 }
 
-fn try_run_payload(service: &Strong<dyn IVirtualMachineService>) -> Result<i32> {
+fn try_run_payload(
+    service: &Strong<dyn IVirtualMachineService>,
+    vm_payload_service_fd: RawFd,
+) -> Result<i32> {
     let metadata = load_metadata().context("Failed to load payload metadata")?;
     let dice = DiceDriver::new(Path::new("/dev/open-dice0")).context("Failed to load DICE")?;
 
@@ -447,7 +451,12 @@ fn try_run_payload(service: &Strong<dyn IVirtualMachineService>) -> Result<i32> 
     // Wait until zipfuse has mounted the APKs so we can access the payload
     zipfuse.wait_until_done()?;
 
-    register_vm_payload_service(allow_restricted_apis, service.clone(), dice_artifacts)?;
+    register_vm_payload_service(
+        allow_restricted_apis,
+        service.clone(),
+        dice_artifacts,
+        vm_payload_service_fd,
+    )?;
 
     // Wait for encryptedstore to finish mounting the storage (if enabled) before setting
     // microdroid_manager.init_done. Reason is init stops uneventd after that.
