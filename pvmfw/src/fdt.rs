1b3c8ceb6e633a28ea6f3adf5c7ce216c539f56a
Alice Wang <aliceywang@google.com>
2023-06-13 14:55:38 +0000

    [pvmfw] Ensure the /memory node in DT contains only one range
    As specified in crosvm.
    
    Bug: 284462758
    Test: atest MicrodroidHostTests
    Change-Id: I909ccfcbc304bbf0d6ea38bf88927976b8aab022

--- pvmfw/src/fdt.rs
+++ pvmfw/src/fdt.rs
@@ -106,8 +106,27 @@ fn patch_bootargs(fdt: &mut Fdt, bootargs: &CStr) -> libfdt::Result<()> {
     node.setprop(cstr!("bootargs"), bootargs.to_bytes_with_nul())
 }
 
-/// Check if memory range is ok
-fn validate_memory_range(range: &Range<usize>) -> Result<(), RebootReason> {
+/// Reads and validates the memory range in the DT.
+///
+/// Only one memory range is expected with the crosvm setup [1].
+///
+/// [1]: external/crosvm/aarch64/src/fdt.rs;l=97;drc=a14178eb6923378baa1cd48dc1ec14afd99d3890
+fn read_and_validate_memory_range(fdt: &Fdt) -> Result<Range<usize>, RebootReason> {
+    let mut memory = fdt.memory().map_err(|e| {
+        error!("Failed to read memory range from DT: {e}");
+        RebootReason::InvalidFdt
+    })?;
+    let Some(range) = memory.next() else {
+        error!("The /memory node in the DT contains no range.");
+        return Err(RebootReason::InvalidFdt);
+    };
+    if memory.next().is_some() {
+        error!(
+            "The /memory node in the DT contains more than one memory range,
+             while only one is expected."
+        );
+        return Err(RebootReason::InvalidFdt);
+    }
     let base = range.start;
     if base != MEM_START {
         error!("Memory base address {:#x} is not {:#x}", base, MEM_START);
@@ -124,7 +143,7 @@ fn validate_memory_range(range: &Range<usize>) -> Result<(), RebootReason> {
         error!("Memory size is 0");
         return Err(RebootReason::InvalidFdt);
     }
-    Ok(())
+    Ok(range)
 }
 
 fn patch_memory_range(fdt: &mut Fdt, memory_range: &Range<usize>) -> libfdt::Result<()> {
@@ -608,11 +627,7 @@ fn parse_device_tree(fdt: &libfdt::Fdt) -> Result<DeviceTreeInfo, RebootReason> 
         RebootReason::InvalidFdt
     })?;
 
-    let memory_range = fdt.first_memory_range().map_err(|e| {
-        error!("Failed to read memory range from DT: {e}");
-        RebootReason::InvalidFdt
-    })?;
-    validate_memory_range(&memory_range)?;
+    let memory_range = read_and_validate_memory_range(fdt)?;
 
     let bootargs = read_bootargs_from(fdt).map_err(|e| {
         error!("Failed to read bootargs from DT: {e}");
