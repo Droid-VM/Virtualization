449e3eb9206040779ded4986c9e022231c36f047
Alice Wang <aliceywang@google.com>
2023-07-04 08:33:27 +0000

    [rialto] Find virtio socket device through PCI bus
    Bug: 284462758
    Test: atest rialto_test
    Change-Id: I62836d42b6afba2beea37ef57e8745f03e6d0c3d

--- rialto/src/main.rs
+++ rialto/src/main.rs
@@ -30,14 +30,15 @@ use fdtpci::PciInfo;
 use hyp::{get_hypervisor, HypervisorCap, KvmError};
 use libfdt::FdtError;
 use log::{debug, error, info};
+use virtio_drivers::transport::{pci::bus::PciRoot, DeviceType, Transport};
 use vmbase::{
     configure_heap,
     fdt::SwiotlbInfo,
     layout::{self, crosvm},
     main,
-    memory::{MemoryTracker, PageTable, MEMORY, PAGE_SIZE, SIZE_64KB},
+    memory::{MemoryTracker, PageTable, MEMORY, PAGE_SIZE, SIZE_128KB},
     power::reboot,
-    virtio::pci,
+    virtio::pci::{self, PciTransportIterator, VirtIOSocket},
 };
 
 fn new_page_table() -> Result<PageTable> {
@@ -117,10 +118,21 @@ unsafe fn try_main(fdt_addr: usize) -> Result<()> {
 
     let pci_info = PciInfo::from_fdt(fdt)?;
     debug!("PCI: {pci_info:#x?}");
-    let pci_root = pci::initialize(pci_info, MEMORY.lock().as_mut().unwrap())
+    let mut pci_root = pci::initialize(pci_info, MEMORY.lock().as_mut().unwrap())
         .map_err(Error::PciInitializationFailed)?;
     debug!("PCI root: {pci_root:#x?}");
+    let socket_device = find_socket_device(&mut pci_root)?;
+    debug!("Found socket device: guest cid = {:?}", socket_device.guest_cid());
     Ok(())
+}
+
+fn find_socket_device(pci_root: &mut PciRoot) -> Result<VirtIOSocket> {
+    PciTransportIterator::new(pci_root)
+        .find(|t| DeviceType::Socket == t.device_type())
+        .map(VirtIOSocket::new)
+        .transpose()
+        .map_err(Error::VirtIOSocketCreationFailed)?
+        .ok_or(Error::MissingVirtIOSocketDevice)
 }
 
 fn memory_protection_granule() -> result::Result<usize, hyp::Error> {
@@ -169,4 +181,4 @@ pub fn main(fdt_addr: u64, _a1: u64, _a2: u64, _a3: u64) {
 }
 
 main!(main);
-configure_heap!(SIZE_64KB);
+configure_heap!(SIZE_128KB);
