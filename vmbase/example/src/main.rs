0cf11b0f072892290c5c1632da59a1ae3a1cb648
Pierre-Clément Tosi <ptosi@google.com>
2023-07-07 12:11:31 +0000

    Require standard safety comments in vmbase clients
    Enable unsafe_op_in_unsafe_fn and clippy::undocumented_unsafe_blocks in
    pvmfw, Rialto, and vmbase_example to improve the quality of our unsafe
    code and automatically detect missing SAFETY comments.
    
    Add SAFETY comments where required and/or format the existing ones.
    
    Remove the now-duplicate rustc frag from libpvmfw.
    
    Note that Clippy would want definitions like
    
        // SAFETY: ...
        let var =
            unsafe { unsafe_statement_that_does_not_fit_on_a_single_line() };
    
    to be written as
    
        let var =
            // SAFETY: ...
            unsafe { unsafe_statement_that_does_not_fit_on_a_single_line() };
    
    so break such unsafe statements down so that they fit on a single line.
    
    Bug: 290018030
    Test: m pvmfw_bin rialto_bin vmbase_example_bin
    Change-Id: I8014ea940d5061196a274f2c2c0bd958fe1e23d4

--- vmbase/example/src/main.rs
+++ vmbase/example/src/main.rs
@@ -16,6 +16,8 @@
 
 #![no_main]
 #![no_std]
+#![deny(unsafe_op_in_unsafe_fn)]
+#![deny(clippy::undocumented_unsafe_blocks)]
 
 mod exceptions;
 mod layout;
@@ -80,8 +82,9 @@ pub fn main(arg0: u64, arg1: u64, arg2: u64, arg3: u64) {
 
     info!("Checking FDT...");
     let fdt = dtb_range();
-    let fdt =
-        unsafe { core::slice::from_raw_parts_mut(fdt.start.0 as *mut u8, fdt.end.0 - fdt.start.0) };
+    let fdt_size = fdt.end.0 - fdt.start.0;
+    // SAFETY: The pointer to the FDT is assumed to point to a valid region.
+    let fdt = unsafe { core::slice::from_raw_parts_mut(fdt.start.0 as *mut u8, fdt_size) };
     let fdt = Fdt::from_mut_slice(fdt).unwrap();
     info!("FDT passed verification.");
     check_fdt(fdt);
@@ -98,6 +101,7 @@ pub fn main(arg0: u64, arg1: u64, arg2: u64, arg3: u64) {
     check_data();
     check_dice();
 
+    // SAFETY: The PciRoot is only created once, from here.
     let mut pci_root = unsafe { pci_info.make_pci_root() };
     check_pci(&mut pci_root);
 
@@ -123,7 +127,9 @@ fn check_stack_guard() {
 
 fn check_data() {
     info!("INITIALISED_DATA: {:?}", INITIALISED_DATA.as_ptr());
+    // SAFETY: Taking the address of a static mut variable should be safe.
     info!("ZEROED_DATA: {:?}", unsafe { ZEROED_DATA.as_ptr() });
+    // SAFETY: Taking the address of a static mut variable should be safe.
     info!("MUTABLE_DATA: {:?}", unsafe { MUTABLE_DATA.as_ptr() });
 
     assert_eq!(INITIALISED_DATA[0], 1);
@@ -131,7 +137,9 @@ fn check_data() {
     assert_eq!(INITIALISED_DATA[2], 3);
     assert_eq!(INITIALISED_DATA[3], 4);
 
+    // SAFETY: As we're single-threaded, no concurrent access to static mut variables is expected.
     let zeroed_data = unsafe { &mut ZEROED_DATA };
+    // SAFETY: As we're single-threaded, no concurrent access to static mut variables is expected.
     let mutable_data = unsafe { &mut MUTABLE_DATA };
 
     for element in zeroed_data.iter() {
