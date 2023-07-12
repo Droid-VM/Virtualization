LineageOS/android_packages_modules_Virtualization

commit 0a15fcf99caac27986cd18ccdaeedddb4d56d688

run-microdroid command: default --debug flag to "full"
The main purpose of this command is to provide developers an easy way to
start a Microdroid instance to play with, so defaulting to the "full"
debug mode will make developers life a tad easier.

Bug: 254912288
Test: adb shell /apex/com.android.virt/bin/vm run-microdroid
Change-Id: I9de7e25a6f7742abf58491b35f2f14a79aecdcd2

vm/src/main.rs
@@ -149,7 +149,7 @@ enum Opt {
         ramdump: Option<PathBuf>,
 
         /// Debug level of the VM. Supported values: "none" (default), "app_only", and "full".
-        #[clap(long, default_value = "none", value_parser = parse_debug_level)]
+        #[clap(long, default_value = "full", value_parser = parse_debug_level)]
         debug: DebugLevel,
 
         /// Run VM in protected mode.

Droid-VM/Virtualization

commit 6f11d9e2920b15a6ff9681a0bcca7782ee2c0062

docs: Reflect update that full debug is default for run-microdroid
In commit 0a15fcf99caa ("run-microdroid command: default --debug
flag to "full""), the default debug flag became full, but not all docs
were updated to reflect that. Update the docs.

Fixes: 0a15fcf99caa ("run-microdroid command: default --debug flag to "full"")
Change-Id: I1cc4389fd05a6b6c6a954d78c2adc871ad0a0d8e
Signed-off-by: Elliot Berman <quic_eberman@quicinc.com>

vm/src/main.rs
@@ -154,7 +154,7 @@ enum Opt {
         #[clap(long)]
         log: Option<PathBuf>,
 
-        /// Debug level of the VM. Supported values: "none" (default), and "full".
+        /// Debug level of the VM. Supported values: "none" and "full" (default).
         #[clap(long, default_value = "full", value_parser = parse_debug_level)]
         debug: DebugLevel,
 