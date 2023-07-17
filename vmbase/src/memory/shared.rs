faa515a644d40bd2b3b066818458518517d916a0
Alice Wang <aliceywang@google.com>
2023-07-17 14:30:57 +0000

    [vmbase] Adjust refill layout size in memory allocation
    This cl adjusts the refill layout size to the next power of two
    when allocating memory. This is necessary because the actual size
    of the memory allocated in the subsequent call `alloc_aligned` is
    the power of two.
    Without this adjustment, the `alloc_aligned` call will fail if
    given a layout whose size is not a power of two.
    
    Bug: 274441673
    Test: atest MicrodroidTests
    Change-Id: I37d7a2805aa6788d22c2cd5bfc8c701fad1f007d

--- vmbase/src/memory/shared.rs
+++ vmbase/src/memory/shared.rs
@@ -359,6 +359,10 @@ fn try_shared_alloc(layout: Layout) -> Option<NonNull<u8>> {
     if let Some(buffer) = shared_pool.alloc_aligned(layout) {
         Some(NonNull::new(buffer as _).unwrap())
     } else if let Some(shared_memory) = SHARED_MEMORY.lock().as_mut() {
+        // Adjusts the refill layout size to the next power of two as this is the actual size
+        // of the memory allocated in `alloc_aligned()`.
+        let layout =
+            Layout::from_size_align(layout.size().next_power_of_two(), layout.align()).unwrap();
         shared_memory.refill(&mut shared_pool, layout);
         shared_pool.alloc_aligned(layout).map(|buffer| NonNull::new(buffer as _).unwrap())
     } else {
