6106bcf236f32a164485a736a5f2564481ca6e21
Inseob Kim <inseob@google.com>
2023-07-28 19:20:56 +0900

    Bind devices to VFIO with VirtualizationService
    Bug: 287379025
    Bug: 278008182
    Test: manual
    Change-Id: Ia99f2be86c33b171297f76f7e30eacfc083aeaa0

--- virtualizationservice/src/aidl.rs
+++ virtualizationservice/src/aidl.rs
@@ -38,7 +38,7 @@ use libc::VMADDR_CID_HOST;
 use log::{error, info, warn};
 use rustutils::system_properties;
 use std::collections::HashMap;
-use std::fs::{canonicalize, create_dir, remove_dir_all, set_permissions, File, Permissions};
+use std::fs::{create_dir, read_link, remove_dir_all, set_permissions, File, OpenOptions, Permissions};
 use std::io::{Read, Write};
 use std::os::fd::FromRawFd;
 use std::os::unix::fs::PermissionsExt;
@@ -188,7 +188,14 @@ impl IVirtualizationServiceInternal for VirtualizationServiceInternal {
     fn bindDevicesToVfioDriver(&self, devices: &[String]) -> binder::Result<ParcelFileDescriptor> {
         check_use_custom_virtual_machine()?;
 
-        devices.iter().try_for_each(|x| bind_device(x))?;
+        if !*IS_VFIO_SUPPORTED {
+            return Err(Status::new_exception_str(
+                ExceptionCode::UNSUPPORTED_OPERATION,
+                Some("VFIO-platform not supported"),
+            ));
+        }
+
+        devices.iter().try_for_each(|x| bind_device(Path::new(x)))?;
 
         // TODO(b/278008182): create a file descriptor containing DTBO for devices.
         let (raw_read, raw_write) = pipe2(OFlag::O_CLOEXEC).map_err(|e| {
@@ -207,43 +214,115 @@ impl IVirtualizationServiceInternal for VirtualizationServiceInternal {
 }
 
 lazy_static! {
+    static ref DEV_VFIO: &'static Path = Path::new("/dev/vfio/vfio");
     static ref SYSFS_PLATFORM_DEVICES: &'static Path = Path::new("/sys/devices/platform/");
     static ref VFIO_PLATFORM_DRIVER: &'static Path =
         Path::new("/sys/bus/platform/drivers/vfio-platform");
+    static ref SYSFS_PLATFORM_DRIVERS_PROBE: &'static Path =
+        Path::new("/sys/bus/platform/drivers_probe");
+    static ref IS_VFIO_SUPPORTED: bool = is_vfio_supported();
 }
 
-fn bind_device(device: &str) -> binder::Result<()> {
-    // Check platform device exists
-    let dev_sysfs_path = canonicalize(device).map_err(|e| {
-        Status::new_exception_str(
-            ExceptionCode::SERVICE_SPECIFIC,
-            Some(format!("can't canonicalize: {e:?}")),
-        )
-    })?;
-    if !dev_sysfs_path.starts_with(*SYSFS_PLATFORM_DEVICES) {
+fn is_vfio_supported() -> bool {
+    (*DEV_VFIO).exists() && (*VFIO_PLATFORM_DRIVER).exists()
+}
+
+fn check_platform_device(path: &Path) -> binder::Result<()> {
+    if !path.exists() {
         return Err(Status::new_exception_str(
             ExceptionCode::ILLEGAL_ARGUMENT,
-            Some(format!("{device} is not a platform device")),
+            Some(format!("no such device {path:?}")),
         ));
     }
 
-    // Check platform device is bound to VFIO driver
-    let dev_driver_path = canonicalize(dev_sysfs_path.join("driver")).map_err(|e| {
+    if !path.starts_with(*SYSFS_PLATFORM_DEVICES) {
+        return Err(Status::new_exception_str(
+            ExceptionCode::ILLEGAL_ARGUMENT,
+            Some(format!("{path:?} is not a platform device")),
+        ));
+    }
+
+    Ok(())
+}
+
+fn get_device_iommu_group(path: &Path) -> Option<u64> {
+    let group_path = read_link(path.join("iommu_group")).ok()?;
+    let group = group_path.file_name()?;
+    group.to_str()?.parse().ok()
+}
+
+fn write_bytes_to(bytes: &[u8], path: &Path) -> binder::Result<()> {
+    let mut file = OpenOptions::new().write(true).open(path).map_err(|e| {
         Status::new_exception_str(
             ExceptionCode::SERVICE_SPECIFIC,
-            Some(format!("can't canonicalize: {e:?}")),
+            Some(format!("can't open {path:?}: {e:?}")),
         )
     })?;
-    if dev_driver_path != *VFIO_PLATFORM_DRIVER {
-        // TODO(b/278008182): unbind driver and bind to VFIO
-        return Err(Status::new_exception_str(
-            ExceptionCode::UNSUPPORTED_OPERATION,
-            Some("not implemented".to_owned()),
-        ));
+
+    file.write_all(bytes).map_err(|e| {
+        Status::new_exception_str(
+            ExceptionCode::SERVICE_SPECIFIC,
+            Some(format!("can't write to {path:?}: {e:?}")),
+        )
+    })
+}
+
+fn is_bound_to_vfio_driver(path: &Path) -> bool {
+    let Ok(driver_path) = read_link(path.join("driver")) else {
+        return false;
+    };
+    let Some(driver) = driver_path.file_name() else {
+        return false;
+    };
+    driver.to_str().unwrap_or("") == "vfio-platform"
+}
+
+fn bind_vfio_driver(path: &Path) -> binder::Result<()> {
+    if is_bound_to_vfio_driver(path) {
+        // already bound
+        return Ok(());
     }
 
-    // already bound to VFIO driver
-    Ok(())
+    // unbind
+    let Some(device) = path.file_name() else {
+        return Err(Status::new_exception_str(
+            ExceptionCode::ILLEGAL_ARGUMENT,
+            Some(format!("can't get device name from {path:?}"))
+        ));
+    };
+    let Some(device_str) = device.to_str() else {
+        return Err(Status::new_exception_str(
+            ExceptionCode::ILLEGAL_ARGUMENT,
+            Some(format!("invalid filename {device:?}"))
+        ));
+    };
+    write_bytes_to(device_str.as_bytes(), &path.join("driver/unbind"))?;
+
+    // bind to VFIO
+    write_bytes_to(b"vfio-platform", &path.join("driver_override"))?;
+    write_bytes_to(device_str.as_bytes(), *SYSFS_PLATFORM_DRIVERS_PROBE)?;
+
+    // final check
+    if is_bound_to_vfio_driver(path) && get_device_iommu_group(path).is_some() {
+        Ok(())
+    } else {
+        Err(Status::new_exception_str(
+            ExceptionCode::SERVICE_SPECIFIC,
+            Some(format!("could not bind {path:?} to VFIO platform driver")),
+        ))
+    }
+}
+
+fn bind_device(path: &Path) -> binder::Result<()> {
+    let dev_sysfs_path = path.canonicalize().map_err(|e| {
+        Status::new_exception_str(
+            ExceptionCode::ILLEGAL_ARGUMENT,
+            Some(format!("can't canonicalize {path:?}: {e:?}")),
+        )
+    })?;
+
+    check_platform_device(&dev_sysfs_path)?;
+    bind_vfio_driver(&dev_sysfs_path)
 }
 
 #[derive(Debug, Default)]
