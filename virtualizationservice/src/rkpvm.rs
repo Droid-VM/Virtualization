25bac5c6cad6eb7e7796ed1130ba07b09004d725
Alice Wang <aliceywang@google.com>
2023-09-05 08:25:59 +0000

    [attestation] Set up communication channel between host/service VM
    This cl sets up the communication channel over vsock between the
    host and the service VM. It will be used in both client VM
    attestation and RKP VM attestation for the host to send request
    to the service VM and for the service VM to send back the
    response.
    
    Bug: 271275206
    Test: Runs the ServiceVmClientApp in VM
    Test: atest MicrodroidHostTests
    Change-Id: Iba510289e931dad9fd082339638e990d15003b61

--- virtualizationservice/src/rkpvm.rs
+++ virtualizationservice/src/rkpvm.rs
@@ -17,18 +17,17 @@
 //! serves as a trusted platform to attest a client VM.
 
 use crate::service_vm;
-use anyhow::{anyhow, Result};
-use log::info;
-use std::time::Duration;
+use anyhow::{Context, Result};
+use service_vm_comm::{Request, Response};
 
 pub(crate) fn request_certificate(csr: &[u8]) -> Result<Vec<u8>> {
-    let vm = service_vm::start()?;
+    let mut vm = service_vm::start()?;
 
-    // TODO(b/274441673): The host can send the CSR to the RKP VM for attestation.
-    // Wait for VM to finish.
-    vm.wait_for_death_with_timeout(Duration::from_secs(10))
-        .ok_or_else(|| anyhow!("Timed out waiting for VM exit"))?;
+    // TODO(b/271275206): Send the correct request type with client VM's
+    // information to be attested.
+    let request = Request::Reverse(csr.to_vec());
+    let result = vm.process_request(request);
 
-    info!("service_vm: Finished getting the certificate");
-    Ok([b"Return: ", csr].concat())
+    vm.shutdown()?;
+    result.map(|Response::Reverse(cert)| cert).context("Failed to process request")
 }
