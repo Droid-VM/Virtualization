3ba2adede54f07261bef46466616db9d9fed9a32
Alice Wang <aliceywang@google.com>
2023-09-07 12:56:46 +0000

    [service-vm] Send shutdown request to exit rialto
    And rialto will be in a loop of processing request till receiving
    the shutdown request.
    
    Bug: 299432766
    Test: atest rialto_test
    Change-Id: Ifd65adde74a370a91fc462930bb513a3e1fe65b9

--- rialto/src/main.rs
+++ rialto/src/main.rs
@@ -33,7 +33,7 @@ use fdtpci::PciInfo;
 use hyp::{get_mem_sharer, get_mmio_guard};
 use libfdt::FdtError;
 use log::{debug, error, info};
-use service_vm_comm::VmType;
+use service_vm_comm::{Request, VmType};
 use virtio_drivers::{
     device::socket::{VsockAddr, VMADDR_CID_HOST},
     transport::{pci::bus::PciRoot, DeviceType, Transport},
@@ -140,9 +140,15 @@ unsafe fn try_main(fdt_addr: usize) -> Result<()> {
     debug!("Found socket device: guest cid = {:?}", socket_device.guest_cid());
 
     let mut vsock_stream = VsockStream::new(socket_device, host_addr())?;
-    let response = requests::process_request(vsock_stream.read_request()?)?;
-    vsock_stream.write_response(&response)?;
-    vsock_stream.flush()?;
+    loop {
+        let request = vsock_stream.read_request()?;
+        if let Request::Shutdown = request {
+            break;
+        }
+        let response = requests::process_request(request)?;
+        vsock_stream.write_response(&response)?;
+        vsock_stream.flush()?;
+    }
     vsock_stream.shutdown()?;
 
     Ok(())
