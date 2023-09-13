731dfce7d2742e2ce6dcb41b690eaf906c7603de
Alice Wang <aliceywang@google.com>
2023-09-13 12:39:16 +0000

    [rkp] Build MACed public key from boringssl EC_KEY
    The secret will be added in a subsequent after we get the DICE
    chain info of the RKP VM.
    
    Bug: 300068317
    Test: atest rialto_test
    Change-Id: Ia373930fc8f1c6d38349208ca62b9b71b98d126f

--- rialto/src/requests/ec_key.rs
+++ rialto/src/requests/ec_key.rs
@@ -16,22 +16,34 @@
 //! BoringSSL.
 
 use crate::error::RequestProcessingError;
+use alloc::vec;
 use alloc::vec::Vec;
+use bssl_ffi::BN_bn2bin;
+use bssl_ffi::BN_clear_free;
+use bssl_ffi::BN_new;
+use bssl_ffi::BN_num_bytes;
 use bssl_ffi::CBB_cleanup;
 use bssl_ffi::CBB_finish;
 use bssl_ffi::CBB_init;
 use bssl_ffi::EC_KEY_free;
 use bssl_ffi::EC_KEY_generate_key;
+use bssl_ffi::EC_KEY_get0_group;
+use bssl_ffi::EC_KEY_get0_public_key;
 use bssl_ffi::EC_KEY_marshal_private_key;
 use bssl_ffi::EC_KEY_new_by_curve_name;
+use bssl_ffi::EC_POINT_get_affine_coordinates;
 use bssl_ffi::NID_X9_62_prime256v1; // EC P-256 CURVE Nid
 use bssl_ffi::OPENSSL_free;
+use bssl_ffi::BIGNUM;
 use bssl_ffi::CBB;
+use bssl_ffi::EC_GROUP;
 use bssl_ffi::EC_KEY;
+use bssl_ffi::EC_POINT;
 use core::mem::MaybeUninit;
 use core::ptr;
 use core::result;
 use core::slice;
+use coset::{iana, CoseKey, CoseKeyBuilder};
 use zeroize::{Zeroize, ZeroizeOnDrop};
 
 type Result<T> = result::Result<T, RequestProcessingError>;
@@ -62,7 +74,56 @@ impl EcKey {
         }
     }
 
-    // TODO(b/300068317): Returns the CoseKey for the public key.
+    /// Returns the `CoseKey` for the public key.
+    pub fn cose_public_key(&self) -> Result<CoseKey> {
+        const ALGO: iana::Algorithm = iana::Algorithm::ES256;
+        const CURVE: iana::EllipticCurve = iana::EllipticCurve::P_256;
+
+        let (x, y) = self.public_key_coordinates()?;
+        let key = CoseKeyBuilder::new_ec2_pub_key(CURVE, x, y).algorithm(ALGO).build();
+        Ok(key)
+    }
+
+    /// Returns the x and y coordinates of the public key.
+    fn public_key_coordinates(&self) -> Result<(Vec<u8>, Vec<u8>)> {
+        let ec_group = self.ec_group()?;
+        let ec_point = self.public_key_ec_point()?;
+        let x = BigNum::new()?;
+        let y = BigNum::new()?;
+        let ctx = ptr::null_mut();
+        // SAFETY: All the parameters are checked non-null and initialized when needed.
+        // The last parameter `ctx` is generated when needed inside the function.
+        let ret = unsafe { EC_POINT_get_affine_coordinates(ec_group, ec_point, x.0, y.0, ctx) };
+        if ret == 1 {
+            Ok((x.bytes()?, y.bytes()?))
+        } else {
+            Err(RequestProcessingError::InternalError("EC_POINT_get_affine_coordinates"))
+        }
+    }
+
+    fn public_key_ec_point(&self) -> Result<*const EC_POINT> {
+        let ec_point =
+           // SAFETY: It is safe since the key pair has been generated and stored in the
+           // `EC_KEY` pointer.
+           unsafe { EC_KEY_get0_public_key(self.0) };
+        if ec_point.is_null() {
+            Err(RequestProcessingError::InternalError("EC_KEY_get0_public_key"))
+        } else {
+            Ok(ec_point)
+        }
+    }
+
+    fn ec_group(&self) -> Result<*const EC_GROUP> {
+        let group =
+           // SAFETY: It is safe since the key pair has been generated and stored in the
+           // `EC_KEY` pointer.
+           unsafe { EC_KEY_get0_group(self.0) };
+        if group.is_null() {
+            Err(RequestProcessingError::InternalError("EC_KEY_get0_group"))
+        } else {
+            Ok(group)
+        }
+    }
 
     /// Returns the DER-encoded ECPrivateKey structure described in RFC 5915 Section 3:
     ///
@@ -160,5 +221,43 @@ impl Drop for Cbb {
         // SAFETY: This is safe because the CBB pointer is initialized with `CBB_init()` at the
         // creation.
         unsafe { CBB_cleanup(self.as_mut()) }
+    }
+}
+
+struct BigNum(*mut BIGNUM);
+
+impl BigNum {
+    fn new() -> Result<Self> {
+        // SAFETY: The returned pointer is checked below.
+        let bn = unsafe { BN_new() };
+        if bn.is_null() {
+            return Err(RequestProcessingError::InternalError("BN_new"));
+        }
+        Ok(Self(bn))
+    }
+
+    fn num_bytes(&self) -> usize {
+        // SAFETY: The pointer has been created with `BN_new`.
+        let len = unsafe { BN_num_bytes(self.0 as *const BIGNUM) };
+        len.try_into().unwrap()
+    }
+
+    fn bytes(&self) -> Result<Vec<u8>> {
+        let len = self.num_bytes();
+        let mut res = vec![0u8; len];
+        // SAFETY: The pointer has been created with `BN_new`.
+        let read_len = unsafe { BN_bn2bin(self.0 as *const BIGNUM, res.as_mut_ptr()) };
+        if read_len == len {
+            Ok(res)
+        } else {
+            Err(RequestProcessingError::InternalError("BN_bn2bin"))
+        }
+    }
+}
+
+impl Drop for BigNum {
+    fn drop(&mut self) {
+        // SAFETY: The pointer has been created with `BN_new`.
+        unsafe { BN_clear_free(self.0) }
     }
 }
