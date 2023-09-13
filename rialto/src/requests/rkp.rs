baa90289f6de5b66fc9fba997d48c9d97c42abe3
Alice Wang <aliceywang@google.com>
2023-09-13 12:39:16 +0000

    [rkp] Build MACed public key from boringssl EC_KEY
    The secret will be added in a subsequent after we get the DICE
    chain info of the RKP VM.
    
    Bug: 300068317
    Test: atest rialto_test
    Change-Id: Ia373930fc8f1c6d38349208ca62b9b71b98d126f

--- rialto/src/requests/rkp.rs
+++ rialto/src/requests/rkp.rs
@@ -16,19 +16,22 @@
 //! service VM via the RKP (Remote Key Provisioning) server.
 
 use super::ec_key::EcKey;
+use super::pub_key::build_maced_public_key;
 use crate::error::Result;
 use alloc::vec::Vec;
 use service_vm_comm::{EcdsaP256KeyPair, GenerateCertificateRequestParams};
 
 pub(super) fn generate_ecdsa_p256_key_pair() -> Result<EcdsaP256KeyPair> {
+    // TODO(b/300590857): Derive the HMAC key from the DICE sealing CDI.
+    let hmac_key = [];
     let ec_key = EcKey::new_p256()?;
+    let maced_public_key = build_maced_public_key(ec_key.cose_public_key()?, &hmac_key)?;
 
     // TODO(b/279425980): Encrypt the private key in a key blob.
     // Remove the printing of the private key.
     log::debug!("Private key: {:?}", ec_key.private_key()?.as_slice());
 
-    // TODO(b/300068317): Build MACed public key.
-    let key_pair = EcdsaP256KeyPair { maced_public_key: Vec::new(), key_blob: Vec::new() };
+    let key_pair = EcdsaP256KeyPair { maced_public_key, key_blob: Vec::new() };
     Ok(key_pair)
 }
 
