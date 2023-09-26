785349e3db9c7d8cd0e00ae1690f74f375c9d389
Alice Wang <aliceywang@google.com>
2023-09-26 10:30:21 +0000

    [bssl] Add digest and hmac modules to libbssl
    Bug: 301068421
    Test: atest rialto_test
    Change-Id: I1d61902b80cc71c554b1009e6e1c91401164e7cf

---
+++ libs/bssl/src/digest.rs
@@ -0,0 +1,53 @@
+// Copyright 2023, The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Wrappers of the digest functions in BoringSSL digest.h.
+
+use bssl_ffi::{EVP_MD_size, EVP_sha256, EVP_sha512, EVP_MD};
+
+/// Message digester wrapping a `EVP_MD` pointer.
+///
+/// Since `EVD_MD`s are static, we don't need to free the memory after the usage.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Digester(*const EVP_MD);
+
+impl Digester {
+    /// Returns a Digetser implementing `SHA-256` algorithm.
+    pub fn sha256() -> Self {
+        // SAFETY: This function does not access any Rust variables and simply returns
+        // a pointer to the static variable in BoringSSL. The returned pointer
+        // should always be valid.
+        Self(unsafe { EVP_sha256() })
+    }
+
+    /// Returns a Digetser implementing `SHA-512` algorithm.
+    #[allow(dead_code)]
+    pub fn sha512() -> Self {
+        // SAFETY: This function does not access any Rust variables and simply returns
+        // a pointer to the static variable in BoringSSL. The returned pointer
+        // should always be valid.
+        Self(unsafe { EVP_sha512() })
+    }
+
+    /// Returns the digest size in bytes.
+    pub fn size(&self) -> usize {
+        // SAFETY: The inner pointer is fetched from EVP_* hash functions in BoringSSL digest.h
+        unsafe { EVP_MD_size(self.0) }
+    }
+
+    /// Returns the inner pointer to `EVD_MD`.
+    pub fn as_ptr(&self) -> *const EVP_MD {
+        self.0
+    }
+}
