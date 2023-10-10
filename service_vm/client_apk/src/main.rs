574d71454a7c5642627e8510e8d4ef402013acf7
Alice Wang <aliceywang@google.com>
2023-10-10 09:35:37 +0000

    [client-vm] Build client VM CSR and sign the CSR with two keys
    This cl builds the CSR that a client VM sends to the RKP VM for
    remote attestation and adjusted the API accordingly as discussed
    in the doc go/pvm-remote-attestation
    
    The CSR payload is signed with both the CDI_Leaf_Priv of the
    client VM's DICE chain and the attestation key. RKP VM should
    verify the signature later with the CDI_Leaf_Pub extracted
    from the same DICE chain in the CSR and the attestation public
    key.
    
    Bug: 303807447
    Test: run ServiceVmClientApp
    Test: atest microdroid_manager_test
    Change-Id: Ic2c09e7339d9981edda028e2694fa551c911a274

--- service_vm/client_apk/src/main.rs
+++ service_vm/client_apk/src/main.rs
@@ -16,8 +16,11 @@
 
 use anyhow::Result;
 use log::{error, info};
-use std::{ffi::c_void, panic};
-use vm_payload_bindgen::AVmPayload_requestAttestation;
+use std::{ffi::c_void, panic, ptr};
+use vm_payload_bindgen::{
+    AVmPayload_freeAttestationResult, AVmPayload_getCertificateChainFromResult,
+    AVmPayload_requestAttestation,
+};
 
 /// Entry point of the Service VM client.
 #[allow(non_snake_case)]
@@ -47,31 +50,26 @@ fn try_main() -> Result<()> {
         0x11, 0x7b,
     ];
     info!("Sending challenge: {:?}", challenge);
-    let certificate = request_attestation(challenge);
-    info!("Certificate: {:?}", certificate);
-    Ok(())
-}
-
-fn request_attestation(challenge: &[u8]) -> Vec<u8> {
-    // SAFETY: It is safe as we only request the size of the certificate in this call.
-    let certificate_size = unsafe {
-        AVmPayload_requestAttestation(
-            challenge.as_ptr() as *const c_void,
-            challenge.len(),
-            [].as_mut_ptr(),
-            0,
-        )
+    // SAFETY: It is safe as we only read the challenge within its bounds.
+    let res = unsafe {
+        AVmPayload_requestAttestation(challenge.as_ptr() as *const c_void, challenge.len())
     };
-    let mut certificate = vec![0u8; certificate_size];
-    // SAFETY: It is safe as we only write the data into the given buffer within the buffer
-    // size in this call.
+    let cert_chain_size =
+        // SAFETY: The result is returned by `AVmPayload_requestAttestation` and should be valid
+        // before getting freed.
+        unsafe { AVmPayload_getCertificateChainFromResult(res, ptr::null_mut(), 0) };
+    let mut cert_chain = vec![0u8; cert_chain_size];
+    // SAFETY: The result is returned by `AVmPayload_requestAttestation` and should be valid
+    // before getting freed.
     unsafe {
-        AVmPayload_requestAttestation(
-            challenge.as_ptr() as *const c_void,
-            challenge.len(),
-            certificate.as_mut_ptr() as *mut c_void,
-            certificate.len(),
+        AVmPayload_getCertificateChainFromResult(
+            res,
+            cert_chain.as_mut_ptr() as *mut c_void,
+            cert_chain.len(),
         );
-    };
-    certificate
+    }
+    info!("Attestation result certificateChain = {:?}", cert_chain);
+    // SAFETY: The result is returned by `AVmPayload_requestAttestation` and is only freed here.
+    unsafe { AVmPayload_freeAttestationResult(res) };
+    Ok(())
 }
