bd8544267f8fd24debb8296d237def660ce1139c
Shikha Panwar <shikhapanwar@google.com>
2023-10-10 11:54:23 +0000

    Importing assumeTrue for MicrodroidDeviceTestBase
    Upstream cherrypick aosp/2778688 missed this import, causing build failures.
    
    Change-Id: Ib14d668c6de9c7eca48e301e68680b0ffd058f33
    Test: Build
    Bug: 301014052
    Merged-In: Ic2818a8f513c778ad1f5217d3641398c8df1719d

--- tests/helper/src/java/com/android/microdroid/test/device/MicrodroidDeviceTestBase.java
+++ tests/helper/src/java/com/android/microdroid/test/device/MicrodroidDeviceTestBase.java
@@ -20,6 +20,8 @@ import static android.content.pm.PackageManager.FEATURE_VIRTUALIZATION_FRAMEWORK
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.TruthJUnit.assume;
 
+import static com.google.common.truth.TruthJUnit.assumeTrue;
+
 import android.app.Instrumentation;
 import android.app.UiAutomation;
 import android.content.Context;
