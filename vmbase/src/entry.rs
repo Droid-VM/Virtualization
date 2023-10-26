caaedce47a31d4587ae5660c6b4e8cd34982e9e7
Pierre-Clément Tosi <ptosi@google.com>
2023-10-26 11:34:55 +0100

    vmbase: Use logger when rand init fails
    As the logger is initialized at that point, use it to log (and format)
    the error message before panicking.
    
    Test: TH
    Change-Id: I7b87b1230d211249adf19c35244b49b303719813

--- vmbase/src/entry.rs
+++ vmbase/src/entry.rs
@@ -21,6 +21,7 @@ use crate::{
 };
 use core::mem::size_of;
 use hyp::{self, get_mmio_guard};
+use log::error;
 
 fn try_console_init() -> Result<(), hyp::Error> {
     console::init();
@@ -53,8 +54,18 @@ extern "C" fn rust_entry(x0: u64, x1: u64, x2: u64, x3: u64) -> ! {
     // We keep a null byte at the top of the stack guard to act as a string terminator.
     let random_guard = &mut stack_guard[..(SIZE_OF_STACK_GUARD - 1)];
 
-    rand::init().expect("Failed to initialize a source of entropy");
-    rand::fill_with_entropy(random_guard).expect("Failed to get stack canary entropy");
+    rand::init()
+        .map_err(|e| {
+            error!("Failed to initialize a source of entropy: {e}");
+        })
+        .unwrap();
+
+    rand::fill_with_entropy(random_guard)
+        .map_err(|e| {
+            error!("Failed to get stack canary entropy: {e}");
+        })
+        .unwrap();
+
     bionic::__get_tls().stack_guard = u64::from_ne_bytes(stack_guard);
 
     // Note: If rust_entry ever returned (which it shouldn't by being -> !), the compiler-injected
