37f801869223c74f9251900421fdc40d1e06f3cc
Shikha Panwar <shikhapanwar@google.com>
2023-11-27 16:59:30 +0000

    Add serialization primitive secretkeeper_comm
    SecretManagement is a cbor based protocol, with heavy usage of cbor
    serialization. This patch introduces traits ValueConversion &
    CborBytesConversion to assist this.
    
    RequestPacket/ResponsePacket & other types (Id, Secret that will be
    introduced in the following patch) will make use of this.
    
    Bug: 291228655
    Test: atest libsecretkeeper_comm_nostd
    Change-Id: I7332c2be57518fd291cb58c514acf543a389cf39

--- secretkeeper/comm/src/data_types/mod.rs
+++ secretkeeper/comm/src/data_types/mod.rs
@@ -20,8 +20,48 @@
 //!      platform/hardware/interfaces/security/\
 //!      secretkeeper/aidl/android/hardware/security/secretkeeper/SecretManagement.cddl
 
+pub mod cbor_ser;
 pub mod error;
 pub mod packet;
 pub mod request;
 pub mod request_response_impl;
 pub mod response;
+use crate::data_types::cbor_ser::{CborBytesConversion, ValueConversion};
+use crate::data_types::error::Error;
+use alloc::boxed::Box;
+use ciborium::Value;
+
+/// Size of the `id` bstr in SecretManagement.cddl
+pub const ID_SIZE: usize = 64;
+/// Size of the `secret` bstr in SecretManagement.cddl
+pub const SECRET_SIZE: usize = 32;
+
+/// Identifier of Secret. See `id` in SecretManagement.cddl
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Id(pub Box<[u8; ID_SIZE]>);
+
+impl ValueConversion for Id {
+    fn from_value(val: Value) -> Result<Self, Error> {
+        Ok(Self(val.into_bytes()?.try_into().map_err(|_| Error::ConversionError)?))
+    }
+    fn value(&self) -> Value {
+        Value::from(self.0.as_slice())
+    }
+}
+impl CborBytesConversion for Id {}
+
+/// Secret - corresponds to `secret` in SecretManagement.cddl
+// Note Secret has sensitive data. Do  not log!
+#[derive(Clone, Eq, PartialEq)]
+pub struct Secret(pub Box<[u8; SECRET_SIZE]>); // TODO: Implement ZeroOnDrop
+
+impl ValueConversion for Secret {
+    fn from_value(val: Value) -> Result<Self, Error> {
+        Ok(Self(val.into_bytes()?.try_into().map_err(|_| Error::ConversionError)?))
+    }
+
+    fn value(&self) -> Value {
+        Value::from(self.0.as_slice())
+    }
+}
+impl CborBytesConversion for Secret {}
