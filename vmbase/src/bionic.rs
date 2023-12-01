8046236c80dadc9392e005c57a4ef55099ea3355
Alice Wang <aliceywang@google.com>
2023-12-01 13:57:10 +0000

    [attestation] Validate DICE chain signatures and CSR signature
    With subject public keys in the DICE certificates.
    
    An implementation of realloc and vsnprintf have been included in
    this cl as they are required by BoringSSL.
    
    Bug: 314266221
    Bug: 310931749
    Test: atest rialto_test
    Change-Id: I82cc58441b4f4730e46e9de7a0efa03959d8d137

--- vmbase/src/bionic.rs
+++ vmbase/src/bionic.rs
@@ -18,6 +18,7 @@ use core::ffi::c_char;
 use core::ffi::c_int;
 use core::ffi::c_void;
 use core::ffi::CStr;
+use core::ptr;
 use core::slice;
 use core::str;
 
@@ -203,6 +204,36 @@ extern "C" fn perror(s: *const c_char) {
     } else {
         eprintln!("{error}");
     }
+}
+
+/// # Safety requirements
+///
+/// The following must be satisfied for this function to be safe:
+/// - `format` must be a valid pointer to a NULL-terminated string.
+/// - `s` must be a valid pointer to a buffer of size `n` for write.
+#[no_mangle]
+unsafe extern "C" fn vsnprintf(
+    s: *mut c_char,
+    n: usize,
+    format: *const c_char,
+    _ap: *mut c_void,
+) -> c_int {
+    // SAFETY: `format` is a valid NULL-terminated string.
+    let format = unsafe { CStr::from_ptr(format) };
+    if n == 0 {
+        return 0;
+    }
+    let format = format.to_bytes();
+    // We don't bother with printf formatting for now as this function is only
+    // used to print some debug information.
+    let length = format.len();
+    let length = length.min(n - 1);
+    // SAFETY: `s` is a valid buffer of size `n` and `length <= n - 1`.
+    unsafe {
+        ptr::copy_nonoverlapping(format.as_ptr(), s, length);
+        *s.add(length) = b'\0';
+    }
+    (length + 1).try_into().unwrap()
 }
 
 fn cstr_error(n: c_int) -> &'static CStr {
