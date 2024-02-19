ca6de9667bc7ea06c56fad32f71ddbae421a6a16
David Drysdale <drysdale@google.com>
2024-02-19 14:53:58 +0000

    [WIP] Notify virtualizationmaintenance of pkg/user gone
    DO NOT SUBMIT: not working yet
    
    Test: TBD
    Change-Id: I90acda427c99decfa0f1cfd2524302c2e2ca5096

--- java/service/src/com/android/system/virtualmachine/VirtualizationSystemService.java
+++ java/service/src/com/android/system/virtualmachine/VirtualizationSystemService.java
@@ -16,16 +16,119 @@
 
 package com.android.system.virtualmachine;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+import android.os.ServiceManager;
+import android.system.virtualizationmaintenance.IVirtualizationMaintenance;
+import android.util.Slog;
 import com.android.server.SystemService;
 
-/** TODO */
+
 public class VirtualizationSystemService extends SystemService {
+    private static final String TAG = "VirtualizationSystemService";
+
+    private static final String VSI_SERVICE_NAME = "android.system.virtualizationmaintenance";
+
+    private BroadcastReceiver mBroadcastReceiver;
 
     public VirtualizationSystemService(Context context) {
         super(context);
+        Slog.w(TAG, "@@@@ VSS created");
     }
 
     @Override
-    public void onStart() {}
+    public void onStart() {
+        Slog.w(TAG, "@@@@ VSS.onStart()");
+
+        mBroadcastReceiver =
+                new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context context, Intent intent) {
+                        int userId =
+                                intent.getIntExtra(
+                                        Intent.EXTRA_USER_HANDLE, /* @@@ UserHandle.USER_NULL */
+                                        -10000);
+                        Slog.w(
+                                TAG,
+                                "@@@@ onReceive(context="
+                                        + context
+                                        + ", intent="
+                                        + intent
+                                        + ") userId="
+                                        + userId);
+                        String packageName;
+                        switch (intent.getAction()) {
+                            case Intent.ACTION_PACKAGE_REMOVED:
+                                packageName = packageNameFromIntent(intent);
+                                Slog.w(
+                                        TAG,
+                                        "@@@@ package '"
+                                                + packageName
+                                                + "' removed for user "
+                                                + userId);
+                                break;
+                            case Intent.ACTION_PACKAGE_CHANGED:
+                                packageName = packageNameFromIntent(intent);
+                                Slog.w(
+                                        TAG,
+                                        "@@@@ package '"
+                                                + packageName
+                                                + "' changed for user "
+                                                + userId);
+                                break;
+                            case Intent.ACTION_USER_REMOVED:
+                                Slog.w(TAG, "@@@@ user " + userId + " removed");
+                                break;
+                        }
+
+                        Slog.w(TAG, "@@@@ talk to " + VSI_SERVICE_NAME);
+                        IVirtualizationMaintenance service = getMaintenanceService();
+                        Slog.w(TAG, "@@@@ talk to " + service);
+                    }
+                };
+        IntentFilter packageFilter = new IntentFilter();
+        // ?? "protected intent that can only be sent by the system"
+        packageFilter.addAction(Intent.ACTION_PACKAGE_REMOVED);
+        packageFilter.addAction(Intent.ACTION_PACKAGE_CHANGED);
+        packageFilter.addAction(Intent.ACTION_PACKAGE_FULLY_REMOVED);
+        packageFilter.addAction(Intent.ACTION_PACKAGE_FIRST_LAUNCH);
+
+        Context context = getContext();
+
+        context.registerReceiverForAllUsers(
+                mBroadcastReceiver,
+                packageFilter,
+                null /* broadcast permission */,
+                null /* handler */);
+
+        IntentFilter userFilter = new IntentFilter();
+        userFilter.addAction(
+                Intent.ACTION_USER_REMOVED); // Need android.Manifest.permission.MANAGE_USERS ?
+        userFilter.addAction(Intent.ACTION_USER_ADDED);
+        userFilter.addAction(Intent.ACTION_USER_UNLOCKED);
+
+        context.registerReceiverForAllUsers(
+                mBroadcastReceiver,
+                userFilter,
+                null /* broadcast permission */,
+                null /* handler */);
+        Slog.w(TAG, "@@@@ VSS.onStart() registered broadcast receivers");
+    }
+
+    private static String packageNameFromIntent(Intent intent) {
+        return intent.getDataString().substring("package:".length());
+    }
+
+    private static synchronized IVirtualizationMaintenance getMaintenanceService() {
+        final IBinder service = ServiceManager.waitForService(VSI_SERVICE_NAME);
+        if (service == null) {
+            Slog.e(TAG, "Unable to acquire IVirtualizationMaintenance");
+            return null;
+        }
+        Slog.e(TAG, "@@@@ got an IBinder object: " + service);
+        return IVirtualizationMaintenance.Stub.asInterface(service);
+    }
 }
