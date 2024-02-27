3227fb24b30762d74afe790b85bdad466668683e
David Dai <davidai@google.com>
2024-02-27 10:17:05 -0800

    Add setup stage for createVmRequiresPermission test
    Add set up stages to use the correct parameters
    for tests.
    
    Bug: 327205334
    Test: atest MicrodroidTestAppNoPerm
    
    Change-Id: Ia0259a9d36b5acdc07cb4bfe75cffcd14f84716f
    Signed-off-by: David Dai <davidai@google.com>

--- tests/testapk_no_perm/src/java/com/android/microdroid/test/MicrodroidTestAppNoPerm.java
+++ tests/testapk_no_perm/src/java/com/android/microdroid/test/MicrodroidTestAppNoPerm.java
@@ -24,16 +24,38 @@ import com.android.microdroid.test.device.MicrodroidDeviceTestBase;
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertThrows;
 
+import org.junit.Before;
+import org.junit.runners.Parameterized;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 /**
  * Test that the android.permission.MANAGE_VIRTUAL_MACHINE is enforced and that an app cannot launch
  * a VM without said permission.
  */
 @RunWith(JUnit4.class)
 public class MicrodroidTestAppNoPerm extends MicrodroidDeviceTestBase {
+
+    @Parameterized.Parameters(name = "protectedVm={0}")
+    public static Collection<Object[]> params() {
+        List<Object[]> ret = new ArrayList<>();
+        ret.add(new Object[] {true /* protectedVm */, null /* use microdroid kernel */});
+        ret.add(new Object[] {false /* protectedVm */, null /* use microdroid kernel */});
+        return ret;
+    }
+
+    @Parameterized.Parameter(0)
+    public boolean mProtectedVm;
+
+    @Before
+    public void setup() {
+        prepareTestSetup(mProtectedVm, null);
+    }
+
     @Test
     @CddTest(
             requirements = {
