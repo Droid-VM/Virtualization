29f479f8b9a73cae7fea7cff8796ef2082e5a992
Shikha Panwar <shikhapanwar@google.com>
2024-02-28 21:18:10 +0000

    MM: Check if Secretkeeper is supported from DT
    Virtmgr sets a prop in DT to indicate that Secretkeeper HAL is
    supported. Use that as the single source of information from host. This
    eliminates the vulnerability that arises when host gives different
    answers if asked multiple times.
    
    Test: atest MicrodroidTests#encryptedStorageIsPersistent
    Bug: 291213394
    Change-Id: I0bb71df64462c90dbf197b9630e7c57a94216388

--- microdroid_manager/src/vm_secret.rs
+++ microdroid_manager/src/vm_secret.rs
@@ -91,17 +91,16 @@ impl VmSecret {
         vm_service: &Strong<dyn IVirtualMachineService>,
     ) -> Result<Self> {
         ensure!(dice_artifacts.bcc().is_some(), "Dice chain missing");
-
-        let Some(sk_service) = is_sk_supported(vm_service)? else {
-            // Use V1 secrets if Secretkeeper is not supported.
+        if !crate::defer_rollback_protection() {
             return Ok(Self::V1 { dice_artifacts });
-        };
+        }
 
         let explicit_dice =
             OwnedDiceArtifactsWithExplicitKey::from_owned_artifacts(dice_artifacts)?;
         // For pVM, skp_secret are stored in Secretkeeper. For non-protected it is all 0s.
         let mut skp_secret = Zeroizing::new([0u8; SECRET_SIZE]);
         if super::is_strict_boot() {
+            let sk_service = get_secretkeeper_service(vm_service)?;
             let mut session =
                 SkSession::new(sk_service, &explicit_dice, Some(get_secretkeeper_identity()?))?;
             let id = super::get_instance_id()?.ok_or(anyhow!("Missing instance_id"))?;
@@ -268,22 +267,15 @@ fn anyhow_err<E: core::fmt::Debug>(err: E) -> anyhow::Error {
     anyhow!("{:?}", err)
 }
 
-/// Get the secretkeeper connection if supported. Host can be consulted whether the device supports
-/// secretkeeper but that should be used with caution for protected VM.
-pub fn is_sk_supported(
+fn get_secretkeeper_service(
     host: &Strong<dyn IVirtualMachineService>,
-) -> Result<Option<Strong<dyn ISecretkeeper>>> {
-    let sk = if cfg!(llpvm_changes) {
-        host.getSecretkeeper()
-            // TODO rename this error!
-            .map_err(|e| {
-                super::MicrodroidError::FailedToConnectToVirtualizationService(format!(
-                    "Failed to get Secretkeeper: {e:?}"
-                ))
-            })?
-    } else {
-        // LLPVM flag is disabled
-        None
-    };
-    Ok(sk)
+) -> Result<Strong<dyn ISecretkeeper>> {
+    Ok(host
+        .getSecretkeeper()
+        // TODO rename this error!
+        .map_err(|e| {
+            super::MicrodroidError::FailedToConnectToVirtualizationService(format!(
+                "Failed to get Secretkeeper: {e:?}"
+            ))
+        })?)
 }
