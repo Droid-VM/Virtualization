82021dbdeea8a36404bc8cc2a62a5687d43212a8
Jeongik Cha <jeongik@google.com>
2024-03-27 00:18:30 +0900

    Add InputDevice in VirtualMachineRawConfig
    Starts with single touch, evdev.
    
    Bug: 331191129
    Test: build
    Change-Id: I68639dec230f2979e145e261d8584813ee1645d7

--- virtualizationmanager/src/crosvm.rs
+++ virtualizationmanager/src/crosvm.rs
@@ -119,6 +119,7 @@ pub struct CrosvmConfig {
     pub dtbo: Option<File>,
     pub device_tree_overlay: Option<File>,
     pub display_config: Option<DisplayConfig>,
+    pub input_device_options: Vec<InputDeviceOption>,
 }
 
 #[derive(Debug)]
@@ -135,6 +136,14 @@ pub struct DisplayConfig {
 pub struct DiskFile {
     pub image: File,
     pub writable: bool,
+}
+
+/// virtio-input device configuration from `external/crosvm/src/crosvm/config.rs`
+#[derive(Debug)]
+#[allow(dead_code)]
+pub enum InputDeviceOption {
+    EvDev(File),
+    SingleTouch { file: File, width: Option<u32>, height: Option<u32>, name: Option<String> },
 }
 
 type VfioDevice = Strong<dyn IBoundDevice>;
@@ -945,6 +954,21 @@ fn run_vm(
         .arg(format!("--android-display-service={}", config.name));
     }
 
+    for input_device_option in config.input_device_options.iter() {
+        command.arg("--input");
+        command.arg(match input_device_option {
+            InputDeviceOption::EvDev(file) => {
+                format!("evdev[path={}]", add_preserved_fd(&mut preserved_fds, file))
+            }
+            InputDeviceOption::SingleTouch { file, width, height, name } => format!(
+                "single-touch[path={}{}{}{}]",
+                add_preserved_fd(&mut preserved_fds, file),
+                width.map_or("".into(), |w| format!(",width={}", w)),
+                height.map_or("".into(), |h| format!(",height={}", h)),
+                name.as_ref().map_or("".into(), |n| format!(",name={}", n))
+            ),
+        });
+    }
     append_platform_devices(&mut command, &mut preserved_fds, &config)?;
 
     debug!("Preserving FDs {:?}", preserved_fds);
