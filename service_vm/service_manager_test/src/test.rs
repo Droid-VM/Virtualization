e5c0439a2dbb4f77c0e18353fc77e35296b797d4
Alice Wang <aliceywang@google.com>
2024-03-28 16:13:23 +0000

    [DO NOT SUBMIT] some random try
    Test: atest FooServiceManagerTest
    Change-Id: I512146583335a55998443457af12f9635a0f6b7a

---
+++ service_vm/service_manager_test/src/test.rs
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//! Test service manager for VM AIDL services change.
+use com_android_virt_vm_attestation_testservice::aidl::com::android::virt::vm_attestation::testservice::IAttestationService::IAttestationService;
+use rdroidtest::rdroidtest;
+
+const FOO_SERVICE: &str = "com.android.virt.vm_attestation.testservice.IAttestationService/default";
+
+fn foo_service() -> binder::Strong<dyn IAttestationService> {
+    binder::ProcessState::set_thread_pool_max_thread_count(1);
+    binder::ProcessState::start_thread_pool();
+
+    binder::wait_for_interface(FOO_SERVICE).unwrap()
+}
+
+#[rdroidtest]
+fn getting_foo_service_succeeds() {
+    let service = foo_service();
+    // This call should panic in virtualizationservice.
+    service.requestAttestationForTesting().unwrap();
+}
+
+rdroidtest::test_main!();
