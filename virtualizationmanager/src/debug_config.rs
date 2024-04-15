7bd5a05bf0e1614184df6d6f3d27af7f8d0838c4
Pierre-Clément Tosi <ptosi@google.com>
2024-04-15 18:03:51 +0100

    vmconfig: Introduce get_debug_level()
    Add helper function to centralize deducing the debug level from a VM
    config (either app or raw).
    
    Test: m libvmconfig vm virtmgr
    Change-Id: Ia04202fe3b1659b0428fd772e83e075d329f4cca

--- virtualizationmanager/src/debug_config.rs
+++ virtualizationmanager/src/debug_config.rs
@@ -26,6 +26,7 @@ use std::ffi::{CString, NulError};
 use std::fs;
 use std::io::ErrorKind;
 use std::path::{Path, PathBuf};
+use vmconfig::get_debug_level;
 
 const CUSTOM_DEBUG_POLICY_OVERLAY_SYSPROP: &str =
     "hypervisor.virtualizationmanager.debug_policy.path";
@@ -157,10 +158,7 @@ pub struct DebugConfig {
 
 impl DebugConfig {
     pub fn new(config: &VirtualMachineConfig) -> Self {
-        let debug_level = match config {
-            VirtualMachineConfig::AppConfig(config) => config.debugLevel,
-            _ => DebugLevel::NONE,
-        };
+        let debug_level = get_debug_level(config);
 
         match system_properties::read(CUSTOM_DEBUG_POLICY_OVERLAY_SYSPROP).unwrap_or_default() {
             Some(path) if !path.is_empty() => {
