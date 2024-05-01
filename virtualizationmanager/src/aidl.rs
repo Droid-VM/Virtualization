7f3c5bb43749537ff1484d4304522ddb7dfc4205
Nikita Ioffe <ioffe@google.com>
2024-05-01 00:22:52 +0000

    Local patch to test android15-6.6 GKI as guest kernel for Microdroid
    DO NOT SUBMIT
    
    Test:
    Bug: 302467077
    Test: vm run-microdroid --gki android15-6.6
    Test: vm run-microdroid --gki android15-6.6 --protected
    Change-Id: I00b69d3fa2cbb4fcdbd13a6aab6b7ca0c367530d

--- virtualizationmanager/src/aidl.rs
+++ virtualizationmanager/src/aidl.rs
@@ -909,6 +909,12 @@ fn load_app_config(
         vm_config.devices = custom_config.devices.clone();
     }
 
+    if cfg!(target_arch = "aarch64") {
+        info!("[ioffe] enable earlycon");
+        append_kernel_param("earlycon=uart8250,mmio,0x3f8", &mut vm_config);
+        append_kernel_param("keep_bootcon", &mut vm_config);
+    }
+
     if config.memoryMib > 0 {
         vm_config.memoryMib = config.memoryMib;
     }
