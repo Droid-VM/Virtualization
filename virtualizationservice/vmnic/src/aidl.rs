0510a07fbb6f2efe6fec17153d25fe91e9d80590
Seungjae Yoo <seungjaeyoo@google.com>
2024-05-20 16:35:49 +0900

    Implement createTapInterface in vmnic
    Bug: 340376951
    Test: adb shell /apex/com.android.virt/bin/vm run-microdroid --network-supported
    Test: adb shell ifconfig
    
    Change-Id: Ia9d4f196ce6e37192f106786ba4f3f2c6ee5a4e4

--- virtualizationservice/vmnic/src/aidl.rs
+++ virtualizationservice/vmnic/src/aidl.rs
@@ -14,10 +14,76 @@
 
 //! Implementation of the AIDL interface of Vmnic.
 
-use anyhow::anyhow;
+use anyhow::{anyhow, Context};
 use android_system_virtualizationservice_internal::aidl::android::system::virtualizationservice_internal::IVmnic::IVmnic;
 use binder::{self, ExceptionCode, Interface, IntoBinderResult, ParcelFileDescriptor};
+use libc::{c_char, c_short, ifreq, socket, AF_INET, IFF_NO_PI, IFF_TAP, IFF_UP, IFNAMSIZ, SOCK_DGRAM};
 use log::info;
+use nix::{ioctl_write_int_bad, ioctl_write_ptr_bad};
+use nix::sys::ioctl::ioctl_num_type;
+use std::fs::File;
+use std::os::fd::{AsRawFd, RawFd};
+
+const TUNSETIFF: ioctl_num_type = 0x400454ca;
+const TUNSETPERSIST: ioctl_num_type = 0x400454cb;
+const SIOCGIFFLAGS: ioctl_num_type = 0x00008913;
+const SIOCSIFFLAGS: ioctl_num_type = 0x00008914;
+
+ioctl_write_ptr_bad!(ioctl_tunsetiff, TUNSETIFF, ifreq);
+ioctl_write_int_bad!(ioctl_tunsetpersist, TUNSETPERSIST);
+ioctl_write_ptr_bad!(ioctl_siocgifflags, SIOCGIFFLAGS, ifreq);
+ioctl_write_ptr_bad!(ioctl_siocsifflags, SIOCSIFFLAGS, ifreq);
+
+fn create_tap_interface(fd: RawFd, ifname: &str) -> binder::Result<()> {
+    // SAFETY: Zero-filling the variable.
+    let mut ifr: ifreq = unsafe { std::mem::zeroed() };
+    ifr.ifr_ifru.ifru_flags = (IFF_TAP | IFF_NO_PI) as c_short;
+    ifr.ifr_name[..ifname.len()].copy_from_slice(ifname.as_bytes());
+
+    // SAFETY: `ioctl` is copied into the kernel. It modifies the state in the kernel, not the
+    // state of this process in any way.
+    unsafe { ioctl_tunsetiff(fd, &ifr) }
+        .context("Failed to ioctl TUNSETIFF for creating TAP network interface")
+        .or_service_specific_exception(-1)?;
+
+    // SAFETY: `ioctl` is copied into the kernel. It modifies the state in the kernel, not the
+    // state of this process in any way.
+    unsafe { ioctl_tunsetpersist(fd, 1) }
+        .context("Failed to ioctl TUNSETPERSIST for creating TAP network interface")
+        .or_service_specific_exception(-1)?;
+
+    Ok(())
+}
+
+fn bring_up_interface(ifname: &str) -> binder::Result<()> {
+    // SAFETY: Below code checks whether file descriptor of the socket is non-negative value.
+    let fd = unsafe { socket(AF_INET, SOCK_DGRAM, 0) };
+    if fd < 0 {
+        return Err(anyhow!("Failed to create socket for creating TAP network interface"))
+            .or_service_specific_exception(-1);
+    }
+
+    // SAFETY: Zero-filling the variable.
+    let mut ifr: ifreq = unsafe { std::mem::zeroed() };
+    ifr.ifr_name[..ifname.len()].copy_from_slice(ifname.as_bytes());
+
+    // SAFETY: `ioctl` is copied into the kernel. It modifies the state in the kernel, not the
+    // state of this process in any way.
+    unsafe { ioctl_siocgifflags(fd, &ifr) }
+        .context("Failed to ioctl SIOCGIFFLAGS for creating TAP network interface")
+        .or_service_specific_exception(-1)?;
+
+    // SAFETY: After calling SIOCGIFFLAGS, ifr_ifru holds ifru_flags in its union field.
+    unsafe { ifr.ifr_ifru.ifru_flags |= IFF_UP as c_short };
+
+    // SAFETY: `ioctl` is copied into the kernel. It modifies the state in the kernel, not the
+    // state of this process in any way.
+    unsafe { ioctl_siocsifflags(fd, &ifr) }
+        .context("Failed to ioctl SIOCGIFFLAGS for creating TAP network interface")
+        .or_service_specific_exception(-1)?;
+
+    Ok(())
+}
 
 #[derive(Debug, Default)]
 pub struct Vmnic {}
@@ -33,9 +99,19 @@ impl Interface for Vmnic {}
 impl IVmnic for Vmnic {
     fn createTapInterface(&self, iface_name_suffix: &str) -> binder::Result<ParcelFileDescriptor> {
         let ifname = format!("avf_tap_{iface_name_suffix}");
-        info!("Creating TAP interface {}", ifname);
+        // The condition statement here is for checking buffer overflow, including null character.
+        if ifname.len() + 1 > IFNAMSIZ {
+            return Err(anyhow!(format!("TAP interface name {ifname} is too long")))
+                .or_binder_exception(ExceptionCode::ILLEGAL_ARGUMENT);
+        }
 
-        Err(anyhow!("Creating TAP network interface is not supported yet"))
-            .or_binder_exception(ExceptionCode::UNSUPPORTED_OPERATION)
+        let tunfd = File::open("/dev/tun")
+            .context("Failed to open /dev/tun")
+            .or_service_specific_exception(-1)?;
+        create_tap_interface(tunfd.as_raw_fd(), &ifname)?;
+        bring_up_interface(&ifname)?;
+        info!("Created TAP network interface: {}", ifname);
+
+        Ok(ParcelFileDescriptor::new(tunfd))
     }
 }
