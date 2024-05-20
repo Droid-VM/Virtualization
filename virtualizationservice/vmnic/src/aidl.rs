80432f0d679fc4b23af85e12337be50a26526e19
Seungjae Yoo <seungjaeyoo@google.com>
2024-05-20 16:35:49 +0900

    Implement createTapInterface in vmnic
    Bug: 340376951
    Test: adb shell /apex/com.android.virt/bin/vm run-microdroid --network-supported
    Test: adb shell ifconfig
    
    Change-Id: Ia9d4f196ce6e37192f106786ba4f3f2c6ee5a4e4

--- virtualizationservice/vmnic/src/aidl.rs
+++ virtualizationservice/vmnic/src/aidl.rs
@@ -14,10 +14,15 @@
 
 //! Implementation of the AIDL interface of Vmnic.
 
-use anyhow::anyhow;
+use anyhow::{anyhow, Context};
 use android_system_virtualizationservice_internal::aidl::android::system::virtualizationservice_internal::IVmnic::IVmnic;
-use binder::{self, ExceptionCode, Interface, IntoBinderResult, ParcelFileDescriptor};
+use base::sys::linux::ioctl_with_val;
+use binder::{self, Interface, IntoBinderResult, ParcelFileDescriptor};
 use log::info;
+use net_sys::TUNSETPERSIST;
+use net_util::sys::linux::Tap;
+use net_util::TapTCommon;
+use std::ffi::CString;
 
 #[derive(Debug, Default)]
 pub struct Vmnic {}
@@ -32,10 +37,32 @@ impl Interface for Vmnic {}
 
 impl IVmnic for Vmnic {
     fn createTapInterface(&self, iface_name_suffix: &str) -> binder::Result<ParcelFileDescriptor> {
-        let ifname = format!("avf_tap_{iface_name_suffix}");
-        info!("Creating TAP interface {}", ifname);
+        let ifname = CString::new(format!("avf_tap_{iface_name_suffix}"))
+            .context(format!(
+                "Failed to construct TAP interface name as CString: avf_tap_{iface_name_suffix}"
+            ))
+            .or_service_specific_exception(-1)?;
+        let tap = Tap::new_with_name(ifname.as_c_str().to_bytes_with_nul(), true, false)
+            .context(format!("Failed to create TAP interface: {ifname:#?}"))
+            .or_service_specific_exception(-1)?;
+        // SAFETY: Executing ioctl modifies the state of kernel, not this process. Execution failure
+        // is checked below.
+        let ret = unsafe { ioctl_with_val(&tap, TUNSETPERSIST(), 1) };
+        if ret < 0 {
+            return Err(anyhow!(format!(
+                "Failed to ioctl TUNSETPERSIST for the TAP interface: {ifname:#?}"
+            )))
+            .or_service_specific_exception(-1)?;
+        }
+        tap.enable()
+            .context(format!("Failed to enable TAP interface: {ifname:#?}"))
+            .or_service_specific_exception(-1)?;
+        info!("Created TAP interface: {:#?}", ifname);
 
-        Err(anyhow!("Creating TAP network interface is not supported yet"))
-            .or_binder_exception(ExceptionCode::UNSUPPORTED_OPERATION)
+        let tap_file = tap
+            .try_clone_file()
+            .context("Failed to get file of TAP interface")
+            .or_service_specific_exception(-1)?;
+        Ok(ParcelFileDescriptor::new(tap_file))
     }
 }
