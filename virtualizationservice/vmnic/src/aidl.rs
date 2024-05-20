3099a091b30515adab9eb5b21ed006715a696678
Seungjae Yoo <seungjaeyoo@google.com>
2024-05-20 16:35:49 +0900

    Implement createTapInterface in vmnic
    Bug: 340376951
    Test: adb shell /apex/com.android.virt/bin/vm run-microdroid --network-supported
    
    Change-Id: Ia9d4f196ce6e37192f106786ba4f3f2c6ee5a4e4

--- virtualizationservice/vmnic/src/aidl.rs
+++ virtualizationservice/vmnic/src/aidl.rs
@@ -14,10 +14,18 @@
 
 //! Implementation of the AIDL interface of Vmnic.
 
-use anyhow::anyhow;
+use anyhow::{anyhow, Context};
 use android_system_virtualizationservice_internal::aidl::android::system::virtualizationservice_internal::IVmnic::IVmnic;
 use binder::{self, ExceptionCode, Interface, IntoBinderResult, ParcelFileDescriptor};
+use libc::{ifreq, IFF_NO_PI, IFF_TAP, IFF_UP, IFNAMSIZ};
 use log::info;
+use nix::ioctl_write_ptr_bad;
+use std::fs::File;
+use std::os::fd::AsRawFd;
+
+const TUNSETIFF: i32 = 0x400454ca;
+
+ioctl_write_ptr_bad!(create_tap_ioctl, TUNSETIFF, ifreq);
 
 #[derive(Debug, Default)]
 pub struct Vmnic {}
@@ -35,7 +43,26 @@ impl IVmnic for Vmnic {
         let ifname = format!("avf_tap_{iface_name_suffix}");
         info!("Creating TAP interface {}", ifname);
 
-        Err(anyhow!("Creating TAP network interface is not supported yet"))
-            .or_binder_exception(ExceptionCode::UNSUPPORTED_OPERATION)
+        let tunfd = File::open("/dev/tun")
+            .context("Failed to open /dev/tun")
+            .or_service_specific_exception(-1)?;
+
+        // SAFETY: Zero-filling the variable.
+        let mut ifr: ifreq = unsafe { std::mem::zeroed() };
+
+        ifr.ifr_ifru.ifru_flags = (IFF_TAP | IFF_NO_PI | IFF_UP) as i16;
+        if ifname.len() + 1 > IFNAMSIZ {
+            return Err(anyhow!(format!("TAP interface name {ifname} is too long")))
+                .or_binder_exception(ExceptionCode::ILLEGAL_ARGUMENT);
+        }
+        ifr.ifr_name[..ifname.len()].copy_from_slice(ifname.as_bytes());
+
+        // SAFETY: `ioctl` is copied into the kernel. It modifies the state in the kernel, not the
+        // state of this process in any way.
+        unsafe { create_tap_ioctl(tunfd.as_raw_fd(), &ifr) }
+            .context("Failed to request ioctl for creating TAP network interface")
+            .or_service_specific_exception(-1)?;
+
+        Ok(ParcelFileDescriptor::new(tunfd))
     }
 }
