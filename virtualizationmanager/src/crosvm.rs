d0b52de62d59c44c3ae04f9a04bd928e32d18f74
Yi-Yo Chiang <yochiang@google.com>
2024-05-22 19:38:16 +0800

    Use ttyS0 as serial console in
    Ferrochrome doesn't support serial console on hvc0 yet.
    
    Bug: 335362012
    Test: Start vm and connect to the console in & out
    Change-Id: I2e74abfbec2a9b7fb9b3642cc529eca4be9f0fdc

--- virtualizationmanager/src/crosvm.rs
+++ virtualizationmanager/src/crosvm.rs
@@ -866,7 +866,8 @@ fn run_vm(
     {
         // bootconfig.normal will be used, but we need log.
         command.arg("--params").arg("printk.devkmsg=on");
-        command.arg("--params").arg("console=hvc0");
+        // Should ttyS0 vs. hvc0 be configurable?
+        command.arg("--params").arg("console=ttyS0");
     }
 
     if let Some(memory_mib) = config.memory_mib {
@@ -923,14 +924,11 @@ fn run_vm(
     // devices in the same PCI bus and serial devices comes before the block devices. Arm crosvm
     // doesn't have the issue.
     // /dev/ttyS0
-    command.arg(format!("--serial={},hardware=serial,num=1", &console_out_arg));
+    command.arg(format!("--serial={}{},hardware=serial,num=1", &console_out_arg, &console_in_arg));
     // /dev/ttyS1
     command.arg(format!("--serial=type=file,path={},hardware=serial,num=2", &failure_serial_path));
     // /dev/hvc0
-    command.arg(format!(
-        "--serial={}{},hardware=virtio-console,num=1",
-        &console_out_arg, &console_in_arg
-    ));
+    command.arg(format!("--serial={},hardware=virtio-console,num=1", &console_out_arg));
     // /dev/hvc1
     command.arg(format!("--serial={},hardware=virtio-console,num=2", &ramdump_arg));
     // /dev/hvc2
