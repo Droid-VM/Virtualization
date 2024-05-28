cbcd313bdeb2a978ffd4db1e952901f041bbcd52
Alan Stokes <alanstokes@google.com>
2024-05-28 09:50:22 +0100

    Create a Rust wrapper for vm_payload
    And use it in place of directly calling the bindgen-generated
    interface in our current clients.
    
    Bug: 340857915
    Test: Builds
    Change-Id: I51f1c1ab6a4dce09d9160731aacd83ebb9c0ce07

--- vm_payload/include/vm_payload.h
+++ vm_payload/include/vm_payload.h
@@ -129,7 +129,7 @@ const char* _Nonnull AVmPayload_getApkContentsPath(void);
  *
  * \return the path to the encrypted storage directory, or NULL if no encrypted
  * storage was requested in the VM configuration. If non-null the returned
- * string should not be deleted or freed by the application and remains valid
+ * string should not be deleted or freed by the m application and remains valid
  * for the lifetime of the VM.
  */
 const char* _Nullable AVmPayload_getEncryptedStoragePath(void);
