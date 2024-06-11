ebd28b6baa9c9ed39df3366db06283e18d5dc266
Seungjae Yoo <seungjaeyoo@google.com>
2024-06-11 10:14:07 +0900

    Skeleton for IVirtualizationTethering service
    Bug: 340376953
    Test: adb shell /apex/com.android.virt/bin/vm run-microdroid --network-supported
    Change-Id: Idab701f3c684e62629bfe02b7a6ad7904d311f14

--- virtualizationservice/src/aidl.rs
+++ virtualizationservice/src/aidl.rs
@@ -24,6 +24,7 @@ use android_system_virtualizationcommon::aidl::android::system::virtualizationco
 use android_system_virtualizationmaintenance::aidl::android::system::virtualizationmaintenance;
 use android_system_virtualizationservice::aidl::android::system::virtualizationservice;
 use android_system_virtualizationservice_internal as android_vs_internal;
+use android_system_virtualizationtethering::aidl::android::system::virtualizationtethering;
 use android_system_virtualmachineservice::aidl::android::system::virtualmachineservice;
 use android_vs_internal::aidl::android::system::virtualizationservice_internal;
 use anyhow::{anyhow, ensure, Context, Result};
@@ -71,6 +72,9 @@ use virtualizationservice_internal::{
     IVfioHandler::{BpVfioHandler, IVfioHandler},
     IVirtualizationServiceInternal::IVirtualizationServiceInternal,
     IVmnic::{BpVmnic, IVmnic},
+};
+use virtualizationtethering::IVirtualizationTethering::{
+    BpVirtualizationTethering, IVirtualizationTethering,
 };
 use virtualmachineservice::IVirtualMachineService::VM_TOMBSTONES_SERVICE_PORT;
 use vsock::{VsockListener, VsockStream};
@@ -163,6 +167,10 @@ lazy_static! {
     static ref NETWORK_SERVICE: Strong<dyn IVmnic> =
         wait_for_interface(<BpVmnic as IVmnic>::get_descriptor())
             .expect("Could not connect to Vmnic");
+    static ref TETHERING_SERVICE: Strong<dyn IVirtualizationTethering> = wait_for_interface(
+        <BpVirtualizationTethering as IVirtualizationTethering>::get_descriptor()
+    )
+    .expect("Could not connect to VirtualizationTethering");
 }
 
 fn is_valid_guest_cid(cid: Cid) -> bool {
@@ -519,7 +527,19 @@ impl IVirtualizationServiceInternal for VirtualizationServiceInternal {
             ))
             .with_log();
         }
-        NETWORK_SERVICE.createTapInterface(iface_name_suffix)
+        let tap_fd = NETWORK_SERVICE.createTapInterface(iface_name_suffix)?;
+
+        // TODO(340377643): Enabling tethering should be for bridge interface, not TAP interface.
+        let ret = TETHERING_SERVICE.enableVirtualizationTethering();
+        if let Err(e) = ret {
+            if e.exception_code() == ExceptionCode::UNSUPPORTED_OPERATION {
+                warn!("{}", e.get_description());
+            } else {
+                return Err(e);
+            }
+        }
+
+        Ok(tap_fd)
     }
 
     fn deleteTapInterface(&self, tap_fd: &ParcelFileDescriptor) -> binder::Result<()> {
