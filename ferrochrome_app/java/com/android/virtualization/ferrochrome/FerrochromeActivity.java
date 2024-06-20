303230063edebde3841e7d7cc1f8522085456c68
Jaewan Kim <jaewan@google.com>
2024-06-20 11:13:07 +0900

    VmLauncherApp: Define action
    Using intent action is the standard to launch a foreign app.
    
    Bug: 346676738
    Test: TBD; need to split
    Change-Id: I1a30a27c7e1aa12adaf14991af7d1d0fa5a6a0bc

--- ferrochrome_app/java/com/android/virtualization/ferrochrome/FerrochromeActivity.java
+++ ferrochrome_app/java/com/android/virtualization/ferrochrome/FerrochromeActivity.java
@@ -43,6 +43,7 @@ import java.util.zip.ZipInputStream;
 public class FerrochromeActivity extends Activity {
     ExecutorService executorService = Executors.newSingleThreadExecutor();
     private static final String TAG = "FerrochromeActivity";
+    private static final String ACTION_VM_LAUNCHER = "android.virtualization.VM_LAUNCHER";
     private static final String FERROCHROME_VERSION = "R127-15916.0.0";
     private static final String EXTERNAL_STORAGE_DIR =
             Environment.getExternalStorageDirectory().getPath() + File.separator;
@@ -58,9 +59,18 @@ public class FerrochromeActivity extends Activity {
         setContentView(R.layout.activity_ferrochrome);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
+        // Find VM Launcher
+        Intent intent = new Intent(ACTION_VM_LAUNCHER);
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> resolveInfo = pm.queryIntentActivities(intent);
+        if (resolveInfo == null || resolveInfo.size() != 1) {
+            updateStatus("Failed to resolve VM Launcher");
+            return;
+        }
+
         // Clean up the existing vm launcher process if there is
         ActivityManager am = getSystemService(ActivityManager.class);
-        am.killBackgroundProcesses(getVmLauncherAppPackageName());
+        am.killBackgroundProcesses(resolveInfo.activityInfo.packageName);
 
         executorService.execute(
                 () -> {
@@ -92,31 +102,8 @@ public class FerrochromeActivity extends Activity {
                     }
                     updateStatus("enable vmlauncher");
                     updateStatus("ready for ferrochrome");
-                    runOnUiThread(
-                            () ->
-                                    startActivity(
-                                            new Intent()
-                                                    .setClassName(
-                                                            getVmLauncherAppPackageName(),
-                                                            "com.android.virtualization.vmlauncher.MainActivity")));
+                    runOnUiThread(() -> startActivity(intent));
                 });
-    }
-
-    private String getVmLauncherAppPackageName() {
-        PackageManager pm = getPackageManager();
-        for (String packageName :
-                new String[] {
-                    "com.android.virtualization.vmlauncher",
-                    "com.google.android.virtualization.vmlauncher"
-                }) {
-            try {
-                pm.getPackageInfo(packageName, 0);
-                return packageName;
-            } catch (PackageManager.NameNotFoundException e) {
-                continue;
-            }
-        }
-        return null;
     }
 
     private void updateStatus(String line) {
