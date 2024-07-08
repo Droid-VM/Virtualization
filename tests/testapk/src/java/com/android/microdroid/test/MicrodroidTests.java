c3bddae5ff5fccf4219dd8661f25545b700b693b
Karuna Wadhera <kwadhera@google.com>
2024-07-08 17:22:04 +0000

    Revert^2 "Adjust tests to account for new DICE mode check"
    Bug: 318483637
    Test: atest rialto_test on eng/userdebug/user builds
    Change-Id: Idba4baa34c8be629737a72bbe098ce2db05374d3

--- tests/testapk/src/java/com/android/microdroid/test/MicrodroidTests.java
+++ tests/testapk/src/java/com/android/microdroid/test/MicrodroidTests.java
@@ -1321,9 +1321,7 @@ public class MicrodroidTests extends MicrodroidDeviceTestBase {
 
         grantPermission(VirtualMachine.USE_CUSTOM_VIRTUAL_MACHINE_PERMISSION);
         VirtualMachineConfig config =
-                newVmConfigBuilderWithPayloadConfig("assets/vm_config.json")
-                        .setDebugLevel(DEBUG_LEVEL_FULL)
-                        .build();
+                newVmConfigBuilderWithPayloadConfig("assets/vm_config.json").build();
         VirtualMachine vm = forceCreateNewVirtualMachine("bcc_vm_for_vsr", config);
         TestResults testResults =
                 runVmTestService(
