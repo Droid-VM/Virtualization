f5c8c4c4d4e5db3fb98bbe98f8ebc024f57fe43c
Jeongik Cha <jeongik@google.com>
2024-08-30 14:01:55 +0900

    Setup script for x86_64
    Bug: 362897977
    Test: run the script
    Change-Id: Iff1675d701f11785edcdc811c8927a3324c5c320

---
+++ android/LinuxInstaller/linux_image_builder/setup_x86_64.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+pushd $(dirname $0) > /dev/null
+
+echo Get Debian image and dependencies...
+wget https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-nocloud-amd64.raw -O debian.img
+wget https://github.com/tsl0922/ttyd/releases/download/1.7.7/ttyd.tyd.x86_64 -O ttyd
+
+echo Customize the image...
+virt-customize --commands-from-file commands -a debian.img
+
+asset_dir=../assets/linux
+mkdir -p ${asset_dir}
+
+echo Copy files...
+tar czvS -f ${asset_dir}/images.tar.gz *.img
+cp vm_config.json ${asset_dir}
+
+echo Calculating hash...
+hash=$(cat ${asset_dir}/images.tar.gz ${asset_dir}/vm_config.json | sha1sum | cut -d' ' -f 1)
+echo ${hash} > ${asset_dir}/version
+
+popd > /dev/null
