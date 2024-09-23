09581ec08adf63ba90df0955db8d79f28d7e0ca4
Elie Kheirallah <khei@google.com>
2024-09-23 20:40:42 +0000

    Add option to dump device tree blob in VM config
    As part of adding tests for backwards compatibility check, dumping the
    device tree will be essential in validating the state of the VM.
    
    Bug: 360388014
    Test: m
    Change-Id: I1f0835590d1e668c1737abe58e1cb7163aa6759f

--- android/virtmgr/src/crosvm.rs
+++ android/virtmgr/src/crosvm.rs
@@ -136,6 +136,7 @@ pub struct CrosvmConfig {
     pub audio_config: Option<AudioConfig>,
     pub no_balloon: bool,
     pub usb_config: UsbConfig,
+    pub dump_dt_fd: Option<File>,
 }
 
 #[derive(Debug)]
@@ -917,15 +918,6 @@ fn run_vm(
         command.arg("--no-usb");
     }
 
-    // TODO: DELETE ME ONCE PATH IS FIGURED OUT
-    command.arg("--dump-device-tree-blob").arg("/data/local/tmp/dt_dump.dtb");
-
-    cfg_if::cfg_if! {
-        if #[cfg(test)] {
-            command.arg("--dump-device-tree-blob").arg("/data/local/tmp/dt_dump.dtb");
-        }
-    }
-
     let mut memory_mib = config.memory_mib;
 
     if config.protected {
@@ -994,6 +986,11 @@ fn run_vm(
 
     // Keep track of what file descriptors should be mapped to the crosvm process.
     let mut preserved_fds = config.indirect_files.into_iter().map(|f| f.into()).collect();
+
+    if let Some(dump_dt_fd) = config.dump_dt_fd {
+        let dump_dt_fd = add_preserved_fd(&mut preserved_fds, dump_dt_fd);
+        command.arg("--dump-device-tree-blob").arg(dump_dt_fd);
+    }
 
     // Setup the serial devices.
     // 1. uart device: used as the output device by bootloaders and as early console by linux
