b27cac5dcb3347b189e79f5d8b8e10cf1445422e
Shikha Panwar <shikhapanwar@google.com>
2024-09-30 09:59:27 +0000

    XTS: Ensure Updatable VM is supported
    In order to support Updatable VMs, Secretkeeper is require. Get the
    relevant test to ensure that is the case.
    
    Test: #UpgradedPackageIsAcceptedWithSecretkeeper
    Bug: 360741531
    Change-Id: I09c2f6bc253e84b33ef37ea5796b7ac4241aaa03

--- tests/hostside/java/com/android/microdroid/test/MicrodroidHostTests.java
+++ tests/hostside/java/com/android/microdroid/test/MicrodroidHostTests.java
@@ -419,7 +419,7 @@ public class MicrodroidHostTests extends MicrodroidHostTestCaseBase {
     @CddTest
     @VsrTest(requirements = {"VSR-7.1-001.008"})
     public void UpgradedPackageIsAcceptedWithSecretkeeper() throws Exception {
-        assumeUpdatableVmSupported();
+        ensureUpdatableVmSupported();
         getDevice().uninstallPackage(PACKAGE_NAME);
         getDevice().installPackage(findTestFile(APK_NAME), /* reinstall= */ true);
         ensureMicrodroidBootsSuccessfully(INSTANCE_ID_FILE, INSTANCE_IMG);
@@ -1284,9 +1284,10 @@ public class MicrodroidHostTests extends MicrodroidHostTestCaseBase {
                         && device.doesFileExist("/sys/bus/platform/drivers/vfio-platform"));
     }
 
-    private void assumeUpdatableVmSupported() throws DeviceNotAvailableException {
-        assumeTrue(
-                "This test is only applicable if if Updatable VMs are supported",
+    private void ensureUpdatableVmSupported() throws DeviceNotAvailableException {
+        assertTrue(
+                "Missing Updatable VM support, have you declared Secretkeeper Secretkeeper"
+                        + " interface?",
                 isUpdatableVmSupported());
     }
 
