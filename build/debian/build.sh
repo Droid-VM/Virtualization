e2406bf0971c21af187d69e9a7abbddbb183e59f
Seungjae Yoo <seungjaeyoo@google.com>
2024-10-02 17:26:49 +0900

    Debian VM holds forwarder_guest
    Bug: 340126051
    Test: Run `forwarder_guest --help` in the VM
    Change-Id: I2cbe0c6772777e72b66dc4c79618969917e79819

--- build/debian/build.sh
+++ build/debian/build.sh
@@ -43,6 +43,7 @@ install_prerequisites() {
 		fai-server \
 		fai-setup-storage \
 		fdisk \
+		gcc-aarch64-linux-gnu \
 		make \
 		python3 \
 		python3-libcloud \
@@ -53,6 +54,12 @@ install_prerequisites() {
 		udev \
 		qemu-system-arm \
 		qemu-user-static
+
+    which cargo > /dev/null 2>&1 || {
+        apt install --no-install-recommends --assume-yes rustup
+        rustup default stable
+		rustup update
+    }
 
         sed -i s/losetup\ -f/losetup\ -P\ -f/g /usr/sbin/fai-diskimage
         sed -i 's/wget \$/wget -t 0 \$/g' /usr/share/debootstrap/functions
@@ -86,6 +93,14 @@ copy_android_config() {
 	mkdir -p ${dst}/files/usr/local/bin/ttyd
 	wget ${url} -O ${dst}/files/usr/local/bin/ttyd/AVF
 	chmod 777 ${dst}/files/usr/local/bin/ttyd/AVF
+
+	pushd forwarder_guest > /dev/null
+		RUSTFLAGS="-C linker=aarch64-linux-gnu-gcc" cargo build --target aarch64-unknown-linux-gnu
+		mkdir -p ${dst}/files/usr/local/bin/forwarder_guest
+		cp target/aarch64-unknown-linux-gnu/debug/forwarder_guest ${dst}/files/usr/local/bin/forwarder_guest/AVF
+		chmod 777 ${dst}/files/usr/local/bin/forwarder_guest/AVF
+		cargo clean
+	popd > /dev/null
 }
 
 run_fai() {
