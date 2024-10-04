0d42165e5f20e01039b93429f36b99c597fe27c8
Seungjae Yoo <seungjaeyoo@google.com>
2024-10-04 13:44:58 +0900

    Make port_listener buildable
    Bug: 340126051
    Test: cd build/debian/port_listener && sudo ./build.sh
    Change-Id: I01d54c6f5ef7c1b2f4706eedfbfea7d8c3064bd5

---
+++ build/debian/port_listener/build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+
+check_sudo() {
+	if [ "$EUID" -ne 0 ]; then
+		echo "Please run as root."
+		exit
+	fi
+}
+
+install_prerequisites() {
+    apt update
+    # TODO(b/340126051): Get rid of arch dependencies, to do cross compilation.
+    apt install --no-install-recommends --assume-yes \
+        bpftool \
+        clang \
+        g++-14-x86-64-linux-gnu \
+        libbpf-dev \
+        libgoogle-glog-dev
+}
+
+build_port_listener() {
+    cp $(dirname $0)/src/* $workdir
+    pushd $workdir
+        bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux.h
+        clang \
+            -O2 \
+            -Wall \
+            -target bpf \
+            -g \
+            -c listen_tracker.ebpf.c \
+            -o listen_tracker.ebpf.o
+        bpftool gen skeleton listen_tracker.ebpf.o > listen_tracker.skel.h
+        clang++ \
+            -O2 \
+            -Wall \
+            -std=c++20 \
+            -lbpf \
+            -lglog \
+            -o port_listener \
+            main.cc
+    popd
+}
+
+clean_up() {
+	rm -rf ${workdir}
+}
+trap clean_up EXIT
+workdir=$(mktemp -d)
+
+check_sudo
+install_prerequisites
+build_port_listener
