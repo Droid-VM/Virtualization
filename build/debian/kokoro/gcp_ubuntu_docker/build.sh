0b48549d6eb590563ba1f8b72eea5e51c5684eb6
maciek swiech <drmasquatch@google.com>
2024-10-15 22:34:41 +0000

    Add x86_64 image to kokoro build
    Change-Id: I6f18cc2dca8ae0136bb364407feb43f12621e30d

--- build/debian/kokoro/gcp_ubuntu_docker/build.sh
+++ build/debian/kokoro/gcp_ubuntu_docker/build.sh
@@ -6,7 +6,8 @@ cd "${KOKORO_ARTIFACTS_DIR}/git/avf/build/debian/"
 sudo losetup -D
 grep vmx /proc/cpuinfo || true
 sudo ./build.sh
-tar czvS -f ${KOKORO_ARTIFACTS_DIR}/images.tar.gz image.raw
+sudo ./build.sh -a x86_64 image-x86_64.raw
+tar czvS -f ${KOKORO_ARTIFACTS_DIR}/images.tar.gz image.raw image-x86_64.raw
 
 mkdir -p ${KOKORO_ARTIFACTS_DIR}/logs
 # TODO(b/372162211): Find exact location of log without breaking kokoro build.
