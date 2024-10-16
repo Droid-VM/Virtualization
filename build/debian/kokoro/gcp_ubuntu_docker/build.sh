350f5bb92ec2756146f1e42165666f8003458abc
maciek swiech <drmasquatch@google.com>
2024-10-16 21:01:39 +0000

    Add x86_64 image to kokoro build
    Bug: b:372169343
    Change-Id: I6f18cc2dca8ae0136bb364407feb43f12621e30d

--- build/debian/kokoro/gcp_ubuntu_docker/build.sh
+++ build/debian/kokoro/gcp_ubuntu_docker/build.sh
@@ -1,3 +1,19 @@
+<<<<<<< PATCH SET (1e439e Add x86_64 image to kokoro build)
+||||||| BASE
+#!/bin/bash
+
+set -e
+
+cd "${KOKORO_ARTIFACTS_DIR}/git/avf/build/debian/"
+sudo losetup -D
+grep vmx /proc/cpuinfo || true
+sudo ./build.sh
+tar czvS -f ${KOKORO_ARTIFACTS_DIR}/images.tar.gz image.raw
+
+mkdir -p ${KOKORO_ARTIFACTS_DIR}/logs
+# TODO(b/372162211): Find exact location of log without breaking kokoro build.
+cp -r /var/log/fai/*/last/* ${KOKORO_ARTIFACTS_DIR}/logs || true
+=======
 #!/bin/bash
 
 set -e
@@ -10,3 +26,4 @@ tar czvS -f ${KOKORO_ARTIFACTS_DIR}/images.tar.gz image.raw
 
 mkdir -p ${KOKORO_ARTIFACTS_DIR}/logs
 sudo cp -r /var/log/fai/* ${KOKORO_ARTIFACTS_DIR}/logs || true
+>>>>>>> BASE      (feb78a Merge "Remove dependency on once_cell where it isn't actuall)
