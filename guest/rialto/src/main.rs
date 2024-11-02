5218f3949802534ba6837be433b7eb3a2d821408
Pierre-Clément Tosi <ptosi@google.com>
2024-11-02 12:11:48 +0000

    vmbase: Default to largest stack size possible
    Remove the need for clients to decide how many pages the stack should
    use, which was originally added to pvmfw to reduce its boot times (as it
    must zero its stack) but was ported to the other clients, where it does
    not make sense. Instead, map all the available pages at the bottom of
    the writable_data region by default. Note that the linker script still
    reserves one page that remains unmapped, to catch stack overflows.
    
    For pvmfw, provide a macro to define its desired stack size.
    
    For other clients, use a temporary macro to satisfy the linker.
    
    As a result, vmbase can expose a layout::stack_range() without argument
    that matches the client. This will enable moving dynamic PT creation
    into the library, which is needed to enter clients with the dynamic PTs
    enabled.
    
    Test: m {pvmfw,rialto,vmbase_example_{bios,kernel}}_bin
    Bug: 377276983
    Change-Id: I8df6762954b3633b737d75f6221a75b8f9bea015

--- guest/rialto/src/main.rs
+++ guest/rialto/src/main.rs
@@ -50,9 +50,10 @@ use vmbase::{
     main,
     memory::{
         init_shared_pool, map_rodata, map_rodata_outside_main_memory, resize_available_memory,
-        switch_to_dynamic_page_tables, PageTable, PAGE_SIZE, SIZE_128KB,
+        switch_to_dynamic_page_tables, PageTable, SIZE_128KB,
     },
     power::reboot,
+    unlimited_stack_size,
     virtio::{
         pci::{self, PciTransportIterator, VirtIOSocket},
         HalImpl,
@@ -75,7 +76,7 @@ fn new_page_table() -> Result<PageTable> {
     let mut page_table = PageTable::default();
 
     page_table.map_data(&layout::scratch_range().into())?;
-    page_table.map_data(&layout::stack_range(40 * PAGE_SIZE).into())?;
+    page_table.map_data(&layout::stack_range().into())?;
     page_table.map_code(&layout::text_range().into())?;
     page_table.map_rodata(&layout::rodata_range().into())?;
     page_table.map_device(&layout::console_uart_page().into())?;
@@ -177,3 +178,4 @@ pub fn main(fdt_addr: u64, _a1: u64, _a2: u64, _a3: u64) {
 generate_image_header!();
 main!(main);
 configure_heap!(SIZE_128KB * 2);
+unlimited_stack_size!();
