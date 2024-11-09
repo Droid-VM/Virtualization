f242b8eecc77724270df8a9366cfa1dc77155093
Willis Kung <williskung@google.com>
2024-11-08 16:20:03 -0800

    Adding parameter for trusty_security_vm_launcher vCPU configuratiopn
    vCPU configuration:
    ONE_CPU (default): --cpu-topology 0
    MATCH_HOST:        --cpu-topology 1
    
    Bug: 361761876
    Test: /system_ext/bin/trusty_security_vm_launcher \
            --kernel /system_ext/etc/hw/desktop_lk_test.elf \
            --memory-size-mib 192
    Flag: EXEMPT desktop only changes
    
    Change-Id: I8d37494539cc284c3fb5238cc74bd7c28fe5509e

--- guest/trusty/security_vm/launcher/src/main.rs
+++ guest/trusty/security_vm/launcher/src/main.rs
@@ -15,8 +15,8 @@
 //! A client for trusty security VMs during early boot.
 
 use android_system_virtualizationservice::aidl::android::system::virtualizationservice::{
-    IVirtualizationService::IVirtualizationService, VirtualMachineConfig::VirtualMachineConfig,
-    VirtualMachineRawConfig::VirtualMachineRawConfig,
+    CpuTopology::CpuTopology, IVirtualizationService::IVirtualizationService,
+    VirtualMachineConfig::VirtualMachineConfig, VirtualMachineRawConfig::VirtualMachineRawConfig,
 };
 use android_system_virtualizationservice::binder::{ParcelFileDescriptor, Strong};
 use anyhow::{Context, Result};
@@ -42,6 +42,10 @@ struct Args {
     /// Memory size of the VM in MiB
     #[arg(long, default_value_t = 128)]
     memory_size_mib: i32,
+
+    /// CPU Topology exposed to the VM <one-cpu|match_host>
+    #[arg(long, default_value = "one-cpu")]
+    cpu_topology: String,
 }
 
 fn get_service() -> Result<Strong<dyn IVirtualizationService>> {
@@ -58,11 +62,18 @@ fn main() -> Result<()> {
     let kernel =
         File::open(&args.kernel).with_context(|| format!("Failed to open {:?}", &args.kernel))?;
 
+    let cpu_topology_config = match args.cpu_topology {
+        "one-cpu" => CpuTopology::ONE_CPU,
+        "match-host" => CpuTopology::MATCH_HOST,
+        _ => CpuTopology::ONE_CPU, // Default to ONE_CPU for other values
+    };
+
     let vm_config = VirtualMachineConfig::RawConfig(VirtualMachineRawConfig {
         name: args.name.to_owned(),
         kernel: Some(ParcelFileDescriptor::new(kernel)),
         protectedVm: args.protected,
         memoryMib: args.memory_size_mib,
+        cpuTopology: cpu_topology_config,
         platformVersion: "~1.0".to_owned(),
         // TODO: add instanceId
         ..Default::default()
