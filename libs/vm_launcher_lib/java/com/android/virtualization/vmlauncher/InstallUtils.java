557f30ff9bb137b2e4c11c9779f6cc1f34ad6763
Jeongik Cha <jeongik@google.com>
2024-11-15 17:23:16 +0900

    Implement backup in terminal
    1. Add an option to backup in recovery
    2. If on, preserve root_part as root_part_backup
    3. If backup exists, terminal app mounts it in /mnt/backup
    4. Provide remove-backup button as well
    
    Bug: 366386783
    Test: backup & check
    Change-Id: Ic7e2d38172615358253d92e26a63ec6b368d3b94

--- libs/vm_launcher_lib/java/com/android/virtualization/vmlauncher/InstallUtils.java
+++ libs/vm_launcher_lib/java/com/android/virtualization/vmlauncher/InstallUtils.java
@@ -28,6 +28,7 @@ import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 
 import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -41,6 +42,8 @@ public class InstallUtils {
 
     private static final String VM_CONFIG_FILENAME = "vm_config.json";
     private static final String COMPRESSED_PAYLOAD_FILENAME = "images.tar.gz";
+    public static final String ROOTFS_FILENAME = "root_part";
+    private static final String BACKUP_FILENAME = "root_part_backup";
     private static final String INSTALLATION_COMPLETED_FILENAME = "completed";
     private static final String PAYLOAD_DIR = "linux";
 
@@ -50,6 +53,13 @@ public class InstallUtils {
 
     public static boolean isImageInstalled(Context context) {
         return Files.exists(getInstallationCompletedPath(context));
+    }
+
+    public static void backupRootFs(Context context) throws IOException {
+        Files.copy(
+                getRootfsFile(context).toPath(),
+                getBackupFile(context).toPath(),
+                StandardCopyOption.REPLACE_EXISTING);
     }
 
     public static void unInstall(Context context) throws IOException {
@@ -87,6 +97,10 @@ public class InstallUtils {
 
     public static File getInternalStorageDir(Context context) {
         return new File(context.getFilesDir(), PAYLOAD_DIR);
+    }
+
+    public static File getBackupFile(Context context) {
+        return new File(context.getFilesDir(), BACKUP_FILENAME);
     }
 
     private static Path getInstallationCompletedPath(Context context) {
@@ -171,5 +185,19 @@ public class InstallUtils {
         } catch (IOException e) {
             return false;
         }
+    }
+
+    private static File getPartitionFile(Context context, String fileName)
+            throws FileNotFoundException {
+        File file = new File(getInternalStorageDir(context), fileName);
+        if (!file.exists()) {
+            Log.d(TAG, file.getAbsolutePath() + " - file not found");
+            throw new FileNotFoundException("File not found: " + fileName);
+        }
+        return file;
+    }
+
+    public static File getRootfsFile(Context context) throws FileNotFoundException {
+        return getPartitionFile(context, ROOTFS_FILENAME);
     }
 }
