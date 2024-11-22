b1695460dfdac2f2a0e4c92d19b8834b16730cb5
Frederick Mayle <fmayle@google.com>
2024-11-22 13:45:55 -0800

    move the PCI CAM and mem regions
    Change-Id: Ie4d09d4d7504dfc1022d0cf675af77c97750c257

--- android/virtmgr/src/crosvm.rs
+++ android/virtmgr/src/crosvm.rs
@@ -1022,6 +1022,11 @@ fn run_vm(
         command.arg("--params").arg("console=hvc0");
     }
 
+    #[cfg(target_arch = "aarch64")]
+    command
+        .arg("--pci")
+        .arg("mem=[start=0x70000000,size=0x2000000],cam=[start=0x72000000,size=0x1000000]");
+
     command.arg("--mem").arg(memory_mib.to_string());
 
     if let Some(cpus) = config.cpus {
