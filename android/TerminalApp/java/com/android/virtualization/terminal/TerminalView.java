f50bc2ad654c0d436415881a018abc3fe4259710
Jeongik Cha <jeongik@google.com>
2024-11-22 14:20:21 +0900

    add modifier keys in terminal app
    Except ctrl key, it just generates a matched keycode to the view
    ctrl key, it enables javascript variable `ctrl`, and handler inside
    xtermjs reads ctrl variable and the keycode, and if it is convertible
    to ctrl keycode, it does.
    
    Test: test added keys
    Bug: 376813452
    Change-Id: I22e5f80ca75ffe6426b7374855cdd015ad08f324

--- android/TerminalApp/java/com/android/virtualization/terminal/TerminalView.java
+++ android/TerminalApp/java/com/android/virtualization/terminal/TerminalView.java
@@ -20,6 +20,41 @@ import android.util.AttributeSet;
 import android.webkit.WebView;
 
 public class TerminalView extends WebView {
+    // keyCode 229 means composing text, so get the last character in e.target.value
+    // 64(@)-95(_) have mapped ctrl codes, and 97(A)-122(Z) is mapped ctrl+small letter
+    public static final String CTRL_KEY_HANDLER =
+            """
+            javascript: (function() {
+              window.term.attachCustomKeyEventHandler((e) => {
+                  console.log(e.type, e.keyCode, window.ctrl);
+                  if (window.ctrl) {
+                      console.log(e.type, e.keyCode);
+                      keyCode = e.keyCode;
+                      if (keyCode === 229) {
+                      keyCode = e.target.value.charAt(e.target.selectionStart - 1).charCodeAt();
+                          console.log(keyCode);
+                      }
+                      if (64 <= keyCode && keyCode <= 95) {
+                          input = String.fromCharCode(keyCode - 64);
+                      } else if (97 <= keyCode && keyCode <= 122) {
+                          input = String.fromCharCode(keyCode - 96);
+                      } else {
+                          return true;
+                      }
+                      if (e.type === 'keyup') {
+                          window.term.input(input);
+                          e.target.value = e.target.value.slice(0, -1);
+                          window.ctrl = false;
+                      }
+                      return false;
+                  } else {
+                      return true;
+                  }
+              });
+            })();
+            """;
+    public static final String ENABLE_CTRL_KEY = "javascript:(function(){window.ctrl=true;})();";
+
     public TerminalView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
