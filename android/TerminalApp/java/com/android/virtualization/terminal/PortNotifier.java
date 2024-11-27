e70eff9c795f734b8eac18738fea47e791230fb6
Pechetty Sravani (xWF) <pechetty@google.com>
2024-11-27 06:40:52 +0000

    Revert "Refactor PortNotifier"
    Revert submission 3367934
    
    Reason for revert: <DroidMonitor created revert due to b/381108280.Will be verified through ABTD for standard investigation.>
    
    Reverted changes: /q/submissionid:3367934
    
    Change-Id: Ife5911ce736d1f91c70e2eb1ee446709cfb42052

--- android/TerminalApp/java/com/android/virtualization/terminal/PortNotifier.java
+++ android/TerminalApp/java/com/android/virtualization/terminal/PortNotifier.java
@@ -19,13 +19,13 @@ package com.android.virtualization.terminal;
 import static com.android.virtualization.terminal.MainActivity.TAG;
 
 import android.app.Notification;
-import android.app.Notification.Action;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.graphics.drawable.Icon;
 import android.util.Log;
 
@@ -38,10 +38,6 @@ import java.util.Set;
  * enable or disable forwarding of the port in notification panel.
  */
 class PortNotifier {
-    private static final String ACTION_PORT_FORWARDING = "android.virtualization.PORT_FORWARDING";
-    private static final String KEY_PORT = "port";
-    private static final String KEY_ENABLED = "enabled";
-
     private final Context mContext;
     private final NotificationManager mNotificationManager;
     private final BroadcastReceiver mReceiver;
@@ -51,7 +47,8 @@ class PortNotifier {
         mNotificationManager = mContext.getSystemService(NotificationManager.class);
         mReceiver = new PortForwardingRequestReceiver();
 
-        IntentFilter intentFilter = new IntentFilter(ACTION_PORT_FORWARDING);
+        IntentFilter intentFilter =
+                new IntentFilter(PortForwardingRequestReceiver.ACTION_PORT_FORWARDING);
         mContext.registerReceiver(mReceiver, intentFilter, Context.RECEIVER_NOT_EXPORTED);
     }
 
@@ -60,11 +57,10 @@ class PortNotifier {
         union.addAll(newPorts);
         for (String portStr : union) {
             try {
-                int port = Integer.parseInt(portStr);
                 if (!oldPorts.contains(portStr)) {
-                    showNotificationFor(port);
+                    showPortForwardingNotification(Integer.parseInt(portStr));
                 } else if (!newPorts.contains(portStr)) {
-                    discardNotificationFor(port);
+                    discardPortForwardingNotification(Integer.parseInt(portStr));
                 }
             } catch (NumberFormatException e) {
                 Log.e(TAG, "Failed to parse port: " + portStr);
@@ -81,16 +77,16 @@ class PortNotifier {
         return mContext.getString(resId);
     }
 
-    private PendingIntent getPendingIntentFor(int port, boolean enabled) {
-        Intent intent = new Intent(ACTION_PORT_FORWARDING);
+    private PendingIntent getPortForwardingPendingIntent(int port, boolean enabled) {
+        Intent intent = new Intent(PortForwardingRequestReceiver.ACTION_PORT_FORWARDING);
         intent.setPackage(mContext.getPackageName());
         intent.setIdentifier(String.format(Locale.ROOT, "%d_%b", port, enabled));
-        intent.putExtra(KEY_PORT, port);
-        intent.putExtra(KEY_ENABLED, enabled);
+        intent.putExtra(PortForwardingRequestReceiver.KEY_PORT, port);
+        intent.putExtra(PortForwardingRequestReceiver.KEY_ENABLED, enabled);
         return PendingIntent.getBroadcast(mContext, 0, intent, PendingIntent.FLAG_IMMUTABLE);
     }
 
-    private void showNotificationFor(int port) {
+    private void showPortForwardingNotification(int port) {
         Intent tapIntent = new Intent(mContext, SettingsPortForwardingActivity.class);
         tapIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent tapPendingIntent =
@@ -103,29 +99,40 @@ class PortNotifier {
         String denyText = getString(R.string.settings_port_forwarding_notification_deny);
         Icon icon = Icon.createWithResource(mContext, R.drawable.ic_launcher_foreground);
 
-        Action acceptAction =
-                new Action.Builder(icon, acceptText, getPendingIntentFor(port, true /* enabled */))
-                        .build();
-        Action denyAction =
-                new Action.Builder(icon, denyText, getPendingIntentFor(port, false /* enabled */))
-                        .build();
         Notification notification =
                 new Notification.Builder(mContext, mContext.getPackageName())
                         .setSmallIcon(R.drawable.ic_launcher_foreground)
                         .setContentTitle(title)
                         .setContentText(content)
                         .setContentIntent(tapPendingIntent)
-                        .addAction(acceptAction)
-                        .addAction(denyAction)
+                        .addAction(
+                                new Notification.Action.Builder(
+                                                icon,
+                                                acceptText,
+                                                getPortForwardingPendingIntent(
+                                                        port, true /* enabled */))
+                                        .build())
+                        .addAction(
+                                new Notification.Action.Builder(
+                                                icon,
+                                                denyText,
+                                                getPortForwardingPendingIntent(
+                                                        port, false /* enabled */))
+                                        .build())
                         .build();
         mNotificationManager.notify(TAG, port, notification);
     }
 
-    private void discardNotificationFor(int port) {
+    private void discardPortForwardingNotification(int port) {
         mNotificationManager.cancel(TAG, port);
     }
 
     private final class PortForwardingRequestReceiver extends BroadcastReceiver {
+        private static final String ACTION_PORT_FORWARDING =
+                "android.virtualization.PORT_FORWARDING";
+        private static final String KEY_PORT = "port";
+        private static final String KEY_ENABLED = "enabled";
+
         @Override
         public void onReceive(Context context, Intent intent) {
             if (ACTION_PORT_FORWARDING.equals(intent.getAction())) {
@@ -134,17 +141,20 @@ class PortNotifier {
         }
 
         private void performActionPortForwarding(Context context, Intent intent) {
-            String prefKey = context.getString(R.string.preference_file_key);
             int port = intent.getIntExtra(KEY_PORT, 0);
-            String key = context.getString(R.string.preference_forwarding_port_is_enabled) + port;
             boolean enabled = intent.getBooleanExtra(KEY_ENABLED, false);
 
-            context.getSharedPreferences(prefKey, Context.MODE_PRIVATE)
-                    .edit()
-                    .putBoolean(key, enabled)
-                    .apply();
+            SharedPreferences sharedPref =
+                    context.getSharedPreferences(
+                            context.getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putBoolean(
+                    context.getString(R.string.preference_forwarding_port_is_enabled)
+                            + Integer.toString(port),
+                    enabled);
+            editor.apply();
 
-            discardNotificationFor(port);
+            mNotificationManager.cancel(TAG, port);
         }
     }
 }
