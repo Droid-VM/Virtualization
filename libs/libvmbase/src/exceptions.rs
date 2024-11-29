f241b3a8637cbc0be20e07eb1aecca1825d7fc30
Michał Mazurek <mazurekm@google.com>
2024-11-29 13:26:22 +0100

    vmbase: Move exceptions to aarch64 directories
    This commit move ARM specific exception handling separate directories. Also use statement in Rialto and PVMFW was limited to only used exception errors, to not leak platform specific detail form libvmbase library. This commit is part of bigger refactoring enabling adding new CPU architecture to libvmbase.
    
    Bug: 362733888
    Test: m {pvmfw, rialto, vmbase_example_{bios, kernel}_bin}
    Test: atest, rialto_test, vmbase_example.integration_test
    
    Change-Id: I17b79d880b9f2fbf9317344dfcb1ce56e935531e

--- libs/libvmbase/src/exceptions.rs
+++ libs/libvmbase/src/exceptions.rs
@@ -14,12 +14,7 @@
 
 //! Helper functions and structs for exception handlers.
 
-use crate::{
-    arch::aarch64::layout::UART_PAGE_ADDR,
-    eprintln,
-    memory::{page_4kb_of, MemoryTrackerError, MEMORY},
-    read_sysreg,
-};
+use crate::memory::{MemoryTrackerError, MEMORY};
 use aarch64_paging::paging::VirtualAddress;
 use core::fmt;
 use core::result;
@@ -51,90 +46,6 @@ impl fmt::Display for HandleExceptionError {
             Self::InternalError(e) => write!(f, "Error while updating page table: {e}"),
             Self::UnknownException => write!(f, "An unknown exception occurred, not handled."),
         }
-    }
-}
-
-/// Represents the possible types of exception syndrome register (ESR) values.
-#[derive(Debug, PartialEq, Copy, Clone)]
-pub enum Esr {
-    /// Data abort due to translation fault.
-    DataAbortTranslationFault,
-    /// Data abort due to permission fault.
-    DataAbortPermissionFault,
-    /// Data abort due to a synchronous external abort.
-    DataAbortSyncExternalAbort,
-    /// An unknown ESR value.
-    Unknown(usize),
-}
-
-impl Esr {
-    const EXT_DABT_32BIT: usize = 0x96000010;
-    const TRANSL_FAULT_BASE_32BIT: usize = 0x96000004;
-    const TRANSL_FAULT_ISS_MASK_32BIT: usize = !0x143;
-    const PERM_FAULT_BASE_32BIT: usize = 0x9600004C;
-    const PERM_FAULT_ISS_MASK_32BIT: usize = !0x103;
-}
-
-impl From<usize> for Esr {
-    fn from(esr: usize) -> Self {
-        if esr == Self::EXT_DABT_32BIT {
-            Self::DataAbortSyncExternalAbort
-        } else if esr & Self::TRANSL_FAULT_ISS_MASK_32BIT == Self::TRANSL_FAULT_BASE_32BIT {
-            Self::DataAbortTranslationFault
-        } else if esr & Self::PERM_FAULT_ISS_MASK_32BIT == Self::PERM_FAULT_BASE_32BIT {
-            Self::DataAbortPermissionFault
-        } else {
-            Self::Unknown(esr)
-        }
-    }
-}
-
-impl fmt::Display for Esr {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Self::DataAbortSyncExternalAbort => write!(f, "Synchronous external abort"),
-            Self::DataAbortTranslationFault => write!(f, "Translation fault"),
-            Self::DataAbortPermissionFault => write!(f, "Permission fault"),
-            Self::Unknown(v) => write!(f, "Unknown exception esr={v:#08x}"),
-        }
-    }
-}
-/// A struct representing an Armv8 exception.
-pub struct ArmException {
-    /// The value of the exception syndrome register.
-    pub esr: Esr,
-    /// The faulting virtual address read from the fault address register.
-    pub far: VirtualAddress,
-}
-
-impl fmt::Display for ArmException {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "ArmException: esr={}, far={}", self.esr, self.far)
-    }
-}
-
-impl ArmException {
-    /// Reads the values of the EL1 exception syndrome register (`esr_el1`)
-    /// and fault address register (`far_el1`) and returns a new instance of
-    /// `ArmException` with these values.
-    pub fn from_el1_regs() -> Self {
-        let esr: Esr = read_sysreg!("esr_el1").into();
-        let far = read_sysreg!("far_el1");
-        Self { esr, far: VirtualAddress(far) }
-    }
-
-    /// Prints the details of an obj and the exception, excluding UART exceptions.
-    pub fn print<T: fmt::Display>(&self, exception_name: &str, obj: T, elr: u64) {
-        // Don't print to the UART if we are handling an exception it could raise.
-        if !self.is_uart_exception() {
-            eprintln!("{exception_name}");
-            eprintln!("{obj}");
-            eprintln!("{}, elr={:#08x}", self, elr);
-        }
-    }
-
-    fn is_uart_exception(&self) -> bool {
-        self.esr == Esr::DataAbortSyncExternalAbort && page_4kb_of(self.far.0) == UART_PAGE_ADDR
     }
 }
 
