0524b7e0cd3dddf3ae3498d5b739f6903fcc5d5f
Nikolina Ilic <nikolinailic@google.com>
2024-12-03 21:09:10 +0000

    pvmfw: Introduce EFI SystemTable
    Define skeleton for the main structure - the EFI system table. Set all
    function pointers to NULL.
    
    Test: m pvmfw_with_uefi_support_bin
    Change-Id: I8fbe747b5343d52c63cd5a51e90ed2ca246b1f89

--- guest/pvmfw/src/entry.rs
+++ guest/pvmfw/src/entry.rs
@@ -16,9 +16,9 @@
 
 use crate::config;
 use crate::memory::MemorySlices;
-use crate::uefi::execute_efi_payload;
 use crate::uefi::linux::locate_linux_efi_entrypoint;
 use crate::uefi::EfiEntrypoint;
+use crate::uefi::{execute_efi_payload, init_efi};
 use core::arch::asm;
 use core::mem::size_of;
 use core::slice;
@@ -36,6 +36,8 @@ use vmbase::{
     power::reboot,
 };
 use zeroize::Zeroize;
+
+pub const FIRMWARE_REVISION: u32 = 0;
 
 #[derive(Debug, Clone)]
 pub enum RebootReason {
@@ -105,6 +107,7 @@ pub fn start(fdt_address: u64, payload_start: u64, payload_size: u64, _arg3: u64
                 }
             }
             NextStage::EfiBoot(ep) if cfg!(feature = "supports_uefi") => {
+                init_efi();
                 let status = execute_efi_payload(ep);
                 error!("EFI payload returned: {status:?}");
                 RebootReason::GuestBootFailed
