585ee358feb1c93af9ba9ea7ea325cbffa435f43
Shikha Panwar <shikhapanwar@google.com>
2024-12-06 18:38:06 +0000

    VM: Introduce writePayloadRpData/readPayloadRpData
    VM payload require an api to allow storing 32 bytes' data in
    Secretkeeper, with equivant confidentiality & integrity guarantees.
    
    Microdroid Manager implements this using the vm_secret module, which
    uses the payload's DICE chain to store/get secret from Secretkeeper.
    
    Additionally introduce a test that uses these api.
    Test: #rollbackProtectedDataOfPayload
    Bug: 378911776
    
    Change-Id: Id39f5c6c626531029bf33ef5d28dc237881e40e6

--- tests/testapk/src/native/testbinary.cpp
+++ tests/testapk/src/native/testbinary.cpp
@@ -344,6 +344,17 @@ Result<void> start_test_service() {
             return ScopedAStatus::ok();
         }
 
+        ScopedAStatus insecurelyReadPayloadRpData(std::array<uint8_t, 32>* out) override {
+            AVmPayload_readRollbackProtectedSecret(out->data());
+            return ScopedAStatus::ok();
+        }
+
+        ScopedAStatus insecurelyWritePayloadRpData(
+                const std::array<uint8_t, 32>& inputData) override {
+            AVmPayload_writeRollbackProtectedSecret(inputData.data());
+            return ScopedAStatus::ok();
+        }
+
         ScopedAStatus quit() override { exit(0); }
     };
     auto testService = ndk::SharedRefBase::make<TestService>();
