9b660d56ab9cab91ecc0c8aaac149ca08030afc6
Nikolina Ilic <nikolinailic@google.com>
2024-12-06 20:48:23 +0000

    pvmfw: Add support for EFI runtime services
    Enable support for EFI runtime services table.
    
    Test: m pvmfw_with_uefi_support_bin
    Change-Id: I4ada7ada8ebc78961051d16ebd5f42f69c3d3ab2

--- guest/pvmfw/src/uefi.rs
+++ guest/pvmfw/src/uefi.rs
@@ -16,6 +16,7 @@
 
 mod boot_services;
 pub mod linux;
+mod runtime_services;
 mod stdio;
 
 use crate::entry::FIRMWARE_REVISION;
@@ -24,6 +25,7 @@ use core::ptr::{null, null_mut};
 use spin::mutex::SpinMutex;
 use uefi_raw::protocol::console::SimpleTextOutputProtocol;
 use uefi_raw::table::boot::BootServices;
+use uefi_raw::table::runtime::RuntimeServices;
 use uefi_raw::table::system::SystemTable;
 use uefi_raw::table::{Header, Revision};
 use uefi_raw::Status;
@@ -39,6 +41,7 @@ static EFI_LOADER: SpinMutex<EfiLoader> = SpinMutex::new(EfiLoader::new());
 struct EfiLoader {
     pub system_table: SystemTable,
     boot_services: BootServices,
+    runtime_services: RuntimeServices,
     simple_text_output_protocol: SimpleTextOutputProtocol,
     firmware_vendor: [u16; 6],
 }
@@ -77,10 +80,17 @@ impl EfiLoader {
         };
 
         let boot_services = boot_services::init_boot_services();
+        let runtime_services = runtime_services::init_runtime_services();
         let simple_text_output_protocol = stdio::init_simple_text_output_protocol();
         let firmware_vendor = Self::FIRMWARE_VENDOR;
 
-        Self { system_table, boot_services, simple_text_output_protocol, firmware_vendor }
+        Self {
+            system_table,
+            boot_services,
+            runtime_services,
+            simple_text_output_protocol,
+            firmware_vendor,
+        }
     }
 
     fn get_system_table_ptr(&mut self) -> *mut SystemTable {
@@ -89,6 +99,7 @@ impl EfiLoader {
 
     fn patch_pointers(&mut self) {
         self.system_table.boot_services = &mut self.boot_services as *mut _;
+        self.system_table.runtime_services = &mut self.runtime_services as *mut _;
         self.system_table.firmware_vendor = &mut self.firmware_vendor as *mut _;
         self.system_table.stdout = &mut self.simple_text_output_protocol as *mut _;
         self.system_table.stderr = &mut self.simple_text_output_protocol as *mut _;
