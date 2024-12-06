c115a70fd6dcc9b936d4555b713481b3f08bd061
Nikolina Ilic <nikolinailic@google.com>
2024-12-06 20:48:38 +0000

    pvmfw: Support EFI loaded image protocol
    The EFI loaded image protocol is used to obtain information about the
    loaded image.
    
    Test: m pvmfw_with_uefi_support_bin
    Change-Id: I997dd735a71fbec6dc1125d5a4289f51791b0358

--- guest/pvmfw/src/entry.rs
+++ guest/pvmfw/src/entry.rs
@@ -107,7 +107,7 @@ pub fn start(fdt_address: u64, payload_start: u64, payload_size: u64, _arg3: u64
                 }
             }
             NextStage::EfiBoot(ep) if cfg!(feature = "supports_uefi") => {
-                init_efi();
+                init_efi(slices.kernel);
                 let status = execute_efi_payload(ep);
                 error!("EFI payload returned: {status:?}");
                 RebootReason::GuestBootFailed
