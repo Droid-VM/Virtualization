1fbe3df4c8dc079982134eebd944b6c05bd93033
Jeongik Cha <jeongik@google.com>
2024-12-13 16:06:04 +0900

    Increase timeout for ttyd, and make it interrupt-aware
    10s might be too tight, so increase timeout to 20s
    And raise a runtime exception only if the thread isn't interrupted
    
    Bug: 383047492
    Test: click close before ttyd is ready
    Change-Id: I593768053b52b4815cade9ba320c662c7bc83f93

--- android/TerminalApp/java/com/android/virtualization/terminal/MainActivity.java
+++ android/TerminalApp/java/com/android/virtualization/terminal/MainActivity.java
@@ -32,7 +32,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.ConditionVariable;
 import android.os.Environment;
-import android.os.SystemClock;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -77,7 +76,7 @@ public class MainActivity extends BaseActivity
     static final String KEY_DISK_SIZE = "disk_size";
     private static final String VM_ADDR = "192.168.0.2";
     private static final int TTYD_PORT = 7681;
-    private static final int TERMINAL_CONNECTION_TIMEOUT_MS = 10_000;
+    private static final int TERMINAL_CONNECTION_TIMEOUT_MS = 20_000;
     private static final int REQUEST_CODE_INSTALLER = 0x33;
     private static final int FONT_SIZE_DEFAULT = 13;
 
@@ -349,24 +348,20 @@ public class MainActivity extends BaseActivity
             // this can never happen.
         }
 
-        long startTime = SystemClock.elapsedRealtime();
-        while (true) {
-            int remainingTime =
-                    TERMINAL_CONNECTION_TIMEOUT_MS
-                            - (int) (SystemClock.elapsedRealtime() - startTime);
-            if (remainingTime <= 0) {
-                throw new RuntimeException("Connection to terminal timedout");
+        try {
+            // Note: this quits immediately if VM is unreachable.
+            if (addr.isReachable(TERMINAL_CONNECTION_TIMEOUT_MS)) {
+                return;
             }
-            try {
-                // Note: this quits immediately if VM is unreachable.
-                if (addr.isReachable(remainingTime)) {
-                    return;
-                }
-            } catch (IOException e) {
-                // give up on network error
-                throw new RuntimeException(e);
-            }
+        } catch (IOException e) {
+            // give up on network error
+            throw new RuntimeException(e);
         }
+        if (!Thread.interrupted()) {
+            Log.d(TAG, "the waiting thread is interrupted");
+            return;
+        }
+        throw new RuntimeException("Connection to terminal timeout");
     }
 
     @Override
