c96d26819d2dd0b7df09a53ddead891f9b056ccf
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2024-12-24 14:17:42 -0800

    Revert "Check |ro.vendor.api_level| for vendor API level on GRF/..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/4adf0173-ff42-4dbb-b7a3-4d45803beb0a). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I2d8e9b6b5f3b6566d1f9227a8585f931054111fb
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- tests/helper/src/java/com/android/microdroid/test/device/MicrodroidDeviceTestBase.java
+++ tests/helper/src/java/com/android/microdroid/test/device/MicrodroidDeviceTestBase.java
@@ -259,11 +259,11 @@ public abstract class MicrodroidDeviceTestBase {
     }
 
     /**
-     * @return The vendor API level that the device as a whole must conform to, this value should be
-     *     available on both GRF and non-GRF devices.
+     * @return The first vendor API level when the vendor images for an SoC that is qualified for
+     *     vendor freeze are first released with this property, or 0 if the property is not set.
      */
     protected static int getFirstVendorApiLevel() {
-        return SystemProperties.getInt("ro.vendor.api_level", -1);
+        return SystemProperties.getInt("ro.board.first_api_level", 0);
     }
 
     protected void assumeSupportedDevice() {
