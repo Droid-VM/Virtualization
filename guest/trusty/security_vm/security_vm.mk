b3d41ebb4adf2eb4660a7c4935af90d535cef291
Armelle Laine <armellel@google.com>
2025-01-23 00:40:04 +0000

    guest: trusty: enable pvmfw-verified pVM for security and test VMs
    - rules that use unsigned images for x86, signed images for arm64
    
    - system_ext artifacts enabled only on devices enabling
      `trusty_system_vm` via a soong config variable:
    
    - VM payload configuration:
      support a `placeholder_trusted_hal` soong bool variable allowing
      to enable placeholder hals on devices that don't support
      the Trusted HALs (Cuttlefish on x86_64 for example)
       (placeholder_trusted_hal ll not be enabled in production)
    
    - security_vm: update launcher build rules to also support arm64
      and define .rc prebuilts to be reused by any device
      (not just cuttlefish)
    
    - test_vm: enable pvmfw-verified pVM by adding image signing rules
    
    Bug: 391719387
    Bug: 391210895
    Test: qemu_trusty_arm64-trunk_staging-userdebug
    Change-Id: I12fb81d5291a4f64dd0693002f6097bc2abb7927
    
    guest: trusty: test_vm: enable pvmfw pVM
    
    - use signed image for arm64 test
    
    Bug: 391719387
    Test: atest TrustyTestVM_UnitTests
    Change-Id: I82dc911e482538e3f22dde6246a64e712bd0f1d0

--- guest/trusty/security_vm/security_vm.mk
+++ guest/trusty/security_vm/security_vm.mk
@@ -1,12 +1,10 @@
-#!/bin/sh
-
-# Copyright 2024 Google Inc. All rights reserved.
+# Copyright (C) 2025 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,4 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-/apex/com.android.virt/bin/vm run /data/local/tmp/trusty_test_vm/trusty_test_vm_config.json
+# TODO(b/379646659): remove the non-signed prebuild when pvmfw can be enabled
+PRODUCT_PACKAGES += \
+	trusty-security_vm-lk \
+	trusty-security_vm-lk.signed \
+	trusty_security_vm_launcher \
+	early_vms.xml \
