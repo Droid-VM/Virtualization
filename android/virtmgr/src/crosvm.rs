2e3add0725c5c79fb6175817c5e075936d74e376
Elie Kheirallah <khei@google.com>
2025-01-24 11:04:19 -0800

    Revert^2 "Enable SVE when available in AVF"
    This reverts commit 75621c84adc390ade2066dcdbc2915f2a1bf81ff.
    
    Reason for revert: Fixed issue which was SVE enabled on all architectures. It's now properly gated for Aarc64
    
    Bug: 376260018
    Test: ABTD, TH
    
    Change-Id: I857dacfedef5894766679b53b31f7791dc5c83cf

--- android/virtmgr/src/crosvm.rs
+++ android/virtmgr/src/crosvm.rs
@@ -1110,7 +1110,7 @@ fn run_vm(
     command.arg("--mem").arg(memory_mib.to_string());
 
     if let Some(cpus) = config.cpus {
-        command.arg("--cpus").arg(cpus.to_string());
+        command.arg("--cpus").arg(cpus.to_string() + ",sve=[auto=true]");
     }
 
     if config.host_cpu_topology {
@@ -1121,7 +1121,12 @@ fn run_vm(
                     command.arg("--virt-cpufreq");
                 }
             }
+            #[cfg(target_arch = "aarch64")]
+            command.arg("--cpus").arg("sve=[auto=true]");
         } else if let Some(cpus) = get_num_cpus() {
+            #[cfg(target_arch = "aarch64")]
+            command.arg("--cpus").arg(cpus.to_string() + ",sve=[auto=true]");
+            #[cfg(not(target_arch = "aarch64"))]
             command.arg("--cpus").arg(cpus.to_string());
         } else {
             bail!("Could not determine the number of CPUs in the system");
