ec236fd4e9b0104e8d514c2ecbfea3a2700a9e5a
Shraddha Basantwani <basantwani@google.com>
2025-01-31 00:08:20 -0800

    Add support for enctypted storage expansion
    Bug: 381067202
    Test: atest com.android.microdroid.test.MicrodroidTests#encryptedStorageExpansionIsPersistent
    Change-Id: I6f5737ee601e7c511bdd316b180bf50e3d102ab1

--- tests/vmshareapp/src/java/com/android/microdroid/test/sharevm/VmShareServiceImpl.java
+++ tests/vmshareapp/src/java/com/android/microdroid/test/sharevm/VmShareServiceImpl.java
@@ -291,10 +291,5 @@ public class VmShareServiceImpl extends Service {
         public boolean isNewInstance() {
             throw new UnsupportedOperationException("Not supported");
         }
-
-        @Override
-        public void checkLibIcuIsAccessible() {
-            throw new UnsupportedOperationException("Not supported");
-        }
     }
 }
