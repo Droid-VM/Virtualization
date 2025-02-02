cf4e106d81d0b6a217348c1fcd2b3d50d48a634d
Matt Gilbride <mattgilbride@google.com>
2025-02-02 13:44:43 +0000

    virtmgr: ExtraDeviceTreeProperties
    Creates an explicit type for the properties that are added to the guest
    DT. This change makes the properties that are added to the guest DT more
    explicit, as well as making it more obvious that *only* the secretkeeper
    public key is fetched from the host DT.
    
    On devices that support secretkeeper HAL v2, the dependency on the host
    VM reference DT is removed entirely.
    
    Note: no functional change intended.
    
    Bug: 358078056
    Test: TH
    Change-Id: Id2f1b6aeb2cda5a481f0cf4a16481af3ef9dbcad

--- android/virtmgr/src/dt_overlay.rs
+++ android/virtmgr/src/dt_overlay.rs
@@ -18,12 +18,97 @@ use anyhow::{anyhow, Result};
 use fsfdt::FsFdt;
 use libfdt::Fdt;
 use std::ffi::CStr;
-use std::path::Path;
+use std::fs::{write, File};
+use std::path::{Path, PathBuf};
 
-pub(crate) const AVF_NODE_NAME: &CStr = c"avf";
-pub(crate) const UNTRUSTED_NODE_NAME: &CStr = c"untrusted";
+const AVF_NODE_NAME: &CStr = c"avf";
+const UNTRUSTED_NODE_NAME: &CStr = c"untrusted";
+const VM_DT_OVERLAY_MAX_SIZE: usize = 2000;
 pub(crate) const VM_DT_OVERLAY_PATH: &str = "vm_dt_overlay.dtbo";
-pub(crate) const VM_DT_OVERLAY_MAX_SIZE: usize = 2000;
+
+pub(crate) struct TrustedDeviceTreeProperties {
+    pub(crate) secretkeeper_public_key: Option<Vec<u8>>,
+    pub(crate) vendor_hashtree_descriptor_root_digest: Option<Vec<u8>>,
+}
+
+pub(crate) struct UntrustedDeviceTreeProperties {
+    pub(crate) defer_rollback_protection: bool,
+    pub(crate) instance_id: Option<[u8; 64]>,
+}
+
+pub(crate) struct ExtraDeviceTreeProperties {
+    pub(crate) trusted: TrustedDeviceTreeProperties,
+    pub(crate) untrusted: UntrustedDeviceTreeProperties,
+}
+
+impl ExtraDeviceTreeProperties {
+    pub(crate) fn maybe_create_device_tree_overlay(
+        &self,
+        dt_output: &PathBuf,
+    ) -> Result<Option<File>> {
+        let mut avf_props = Vec::new();
+        if let Some(secretkeeper_public_key) = &self.trusted.secretkeeper_public_key {
+            avf_props.push((c"secretkeeper-public-key", secretkeeper_public_key));
+        }
+        if let Some(vendor_hashtree_descriptor_root_digest) =
+            &self.trusted.vendor_hashtree_descriptor_root_digest
+        {
+            avf_props.push((
+                c"vendor-hashtree-descriptor-root-digest",
+                vendor_hashtree_descriptor_root_digest,
+            ));
+        }
+
+        let mut untrusted_props: Vec<(&CStr, &[u8])> = Vec::new();
+        let instance_id_prop; // satisfy the borrow checker
+        if let Some(instance_id) = self.untrusted.instance_id {
+            instance_id_prop = instance_id;
+            untrusted_props.push((c"instance-id", &instance_id_prop));
+        }
+        if self.untrusted.defer_rollback_protection {
+            untrusted_props.push((c"defer-rollback-protection", &[]));
+        }
+
+        if avf_props.is_empty() && untrusted_props.is_empty() {
+            return Ok(None);
+        }
+
+        let mut buffer = [0_u8; VM_DT_OVERLAY_MAX_SIZE];
+        let fdt = Fdt::create_empty_tree(&mut buffer)
+            .map_err(|e| anyhow!("Failed to create empty Fdt: {e:?}"))?;
+        let mut fragment = fdt
+            .root_mut()
+            .add_subnode(c"fragment@0")
+            .map_err(|e| anyhow!("Failed to add fragment node: {e:?}"))?;
+        fragment
+            .setprop(c"target-path", b"/\0")
+            .map_err(|e| anyhow!("Failed to set target-path property: {e:?}"))?;
+        let overlay = fragment
+            .add_subnode(c"__overlay__")
+            .map_err(|e| anyhow!("Failed to add __overlay__ node: {e:?}"))?;
+        let mut avf = overlay
+            .add_subnode(AVF_NODE_NAME)
+            .map_err(|e| anyhow!("Failed to add avf node: {e:?}"))?;
+
+        for (key, val) in avf_props {
+            avf.setprop(key, val)?;
+        }
+
+        if !untrusted_props.is_empty() {
+            let mut untrusted = avf
+                .add_subnode(UNTRUSTED_NODE_NAME)
+                .map_err(|e| anyhow!("Failed to add untrusted node: {e:?}"))?;
+
+            for (key, val) in untrusted_props {
+                untrusted.setprop(key, val)?;
+            }
+        }
+
+        fdt.pack().map_err(|e| anyhow!("Failed to pack DT overlay, {e:?}"))?;
+        write(dt_output, fdt.as_slice())?;
+        Ok(Some(File::open(dt_output)?))
+    }
+}
 
 /// Create a Device tree overlay containing the provided proc style device tree & properties!
 /// # Arguments
@@ -48,6 +133,7 @@ pub(crate) const VM_DT_OVERLAY_MAX_SIZE: usize = 2000;
 ///     };
 /// };
 /// ```
+#[allow(dead_code)]
 pub(crate) fn create_device_tree_overlay<'a>(
     buffer: &'a mut [u8],
     dt_path: Option<&'a Path>,
