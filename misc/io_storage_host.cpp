761b861fb09a09495002151ad11575792c84df07
Shikha Panwar <shikhapanwar@google.com>
2025-02-03 17:58:12 +0000

    Benchmarks Android storage
    This builds a binary that can be run on different files. Change the
    filename if required.
    
    Test: NA
    Bug: 390648694
    DO_NOT_MERGE
    
    Change-Id: If90e0eeb7122974e4733b71a5584c41e0530454d

---
+++ misc/io_storage_host.cpp
@@ -0,0 +1,137 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <android-base/file.h>
+#include <android-base/result.h>
+#include <time.h>
+
+using android::base::ErrnoError;
+using android::base::Error;
+using android::base::Result;
+using android::base::unique_fd;
+using android::base::WriteStringToFd;
+
+constexpr int kBlockSizeBytes = 4096;
+constexpr int kNumBytesPerMB = 1024 * 1024;
+
+/**
+ * Measures the read rate for reading the given file.
+ * @return The read rate in MB/s.
+ */
+Result<double> measure_read_rate(const std::string& filename, bool is_rand) {
+    struct stat file_stats;
+    if (stat(filename.c_str(), &file_stats) == -1) {
+        return Error() << "failed to get file stats";
+    }
+    const int64_t file_size_bytes = file_stats.st_size;
+    const int64_t block_count = file_size_bytes / kBlockSizeBytes;
+    std::vector<uint64_t> offsets(block_count);
+    for (auto i = 0; i < block_count; ++i) {
+        offsets[i] = i * kBlockSizeBytes;
+    }
+    if (is_rand) {
+        return ErrnoError() << "Not supported";
+    }
+    char buf[kBlockSizeBytes];
+
+    struct timespec start;
+    if (clock_gettime(CLOCK_MONOTONIC, &start) == -1) {
+        return ErrnoError() << "failed to clock_gettime";
+    }
+    unique_fd fd(open(filename.c_str(), O_RDONLY | O_CLOEXEC));
+    if (fd.get() == -1) {
+        return ErrnoError() << "Read: opening " << filename << " failed";
+    }
+    for (auto i = 0; i < block_count; ++i) {
+        auto bytes = pread(fd, buf, kBlockSizeBytes, offsets[i]);
+        if (bytes == 0) {
+            return Error() << "unexpected end of file";
+        } else if (bytes == -1) {
+            return ErrnoError() << "failed to read";
+        }
+    }
+    struct timespec finish;
+    if (clock_gettime(CLOCK_MONOTONIC, &finish) == -1) {
+        return ErrnoError() << "failed to clock_gettime";
+    }
+    double elapsed_seconds = finish.tv_sec - start.tv_sec + (finish.tv_nsec - start.tv_nsec) / 1e9;
+    double file_size_mb = (double)file_size_bytes / kNumBytesPerMB;
+    return {file_size_mb / elapsed_seconds};
+}
+
+/**
+ * Measures the throughput of writing random data to the given file.
+ * @return The write rate in MB/s.
+ */
+Result<double> measure_write_rate(const std::string& filename, int64_t size_bytes) {
+    struct stat file_stats;
+    const int64_t block_count = size_bytes / kBlockSizeBytes;
+    char buf[kBlockSizeBytes];
+    int fd_rand = open("/dev/urandom", O_RDONLY);
+    read(fd_rand, buf, kBlockSizeBytes);
+
+    struct timespec start;
+    if (clock_gettime(CLOCK_MONOTONIC, &start) == -1) {
+        return ErrnoError() << "failed to clock_gettime";
+    }
+    // TODO(b/390648694): Ideally open with O_SYNC instead of syncfs().
+    unique_fd fd(open(filename.c_str(), O_CREAT | O_WRONLY, 00666));
+    if (fd.get() == -1) {
+        return ErrnoError() << "Write: opening " << filename << " failed";
+    }
+    if (stat(filename.c_str(), &file_stats) == -1) {
+        return Error() << "failed to get file stats";
+    }
+
+    for (auto i = 0; i < block_count; ++i) {
+        auto bytes = write(fd, buf, kBlockSizeBytes);
+        if (bytes == 0) {
+            return Error() << "unexpected end of file";
+        } else if (bytes == -1) {
+            return ErrnoError() << "failed to write";
+        }
+    }
+    syncfs(fd);
+    struct timespec finish;
+    if (clock_gettime(CLOCK_MONOTONIC, &finish) == -1) {
+        return ErrnoError() << "failed to clock_gettime";
+    }
+    double elapsed_seconds = finish.tv_sec - start.tv_sec + (finish.tv_nsec - start.tv_nsec) / 1e9;
+    double file_size_mb = (double)size_bytes / kNumBytesPerMB;
+    return {file_size_mb / elapsed_seconds};
+}
+
+int main() {
+    int n = 5;
+    printf("Testing...");
+    int64_t size = (1073741824 / 4) * 3;
+    double sum_r = 0, sum_w = 0;
+    std::vector<uint64_t> read_rates, write_rates;
+    for (auto i = 0; i < n; ++i) {
+        auto w_rate = measure_write_rate("/data/test/file", size);
+        auto r_rate = measure_read_rate("/data/test/file", false);
+        write_rates.push_back(w_rate.value());
+        read_rates.push_back(r_rate.value());
+        printf("Write rate {%f}\n", w_rate.value());
+        printf("Read rate {%f}\n", r_rate.value());
+        sum_w += w_rate.value();
+        sum_r += r_rate.value();
+    }
+    printf("Average Write rate {%f} MB/s\n", sum_w / n);
+    printf("Average Read rate {%f} MB/s\n", sum_r / n);
+
+    return 0;
+}
