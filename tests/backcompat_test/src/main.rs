d116037f23e277407b3084ad6161130226b2c1d6
Elie Kheirallah <khei@google.com>
2025-02-11 22:46:50 +0000

    Check API version levels for backcompat tests
    Check that vendor API level is set and that the board API level >=
    202504 when running backcompat tests to check for Secretkeeper nodes.
    If those 2 conditions are not met, Secretkeeper is assumed not supported
    and we skip over the nodes related to Secretkeeper.
    
    Bug: 394770084
    Test: TH
    Change-Id: I4846e1f3252615b4829f754c256696bb13903389

--- tests/backcompat_test/src/main.rs
+++ tests/backcompat_test/src/main.rs
@@ -25,6 +25,7 @@ use android_system_virtualizationservice::{
 use anyhow::anyhow;
 use anyhow::Context;
 use anyhow::Error;
+use anyhow::Result;
 use log::info;
 use std::fs::read_to_string;
 use std::fs::File;
@@ -46,11 +47,11 @@ fn test_device_tree_compat() -> Result<(), Error> {
 
 /// Runs a protected VM and validates it against a golden device tree.
 #[test]
-fn test_device_tree_protected_compat() -> Result<(), Error> {
+fn test_device_tree_protected_compat() -> Result<()> {
     run_test(true, GOLDEN_DEVICE_TREE_PROTECTED)
 }
 
-fn run_test(protected: bool, golden_dt: &str) -> Result<(), Error> {
+fn run_test(protected: bool, golden_dt: &str) -> Result<()> {
     let kernel = Some(open_payload(VMBASE_EXAMPLE_KERNEL_PATH)?);
     android_logger::init_once(
         android_logger::Config::default()
@@ -142,7 +143,8 @@ fn run_test(protected: bool, golden_dt: &str) -> Result<(), Error> {
     {
         return Err(anyhow!("failed to execute dtc"));
     }
-    let dtcompare_res = Command::new("./dtcompare")
+    let mut dtcompare_cmd = Command::new("./dtcompare");
+    dtcompare_cmd
         .arg("--dt1")
         .arg("dump_dt_golden.dtb")
         .arg("--dt2")
@@ -162,12 +164,23 @@ fn run_test(protected: bool, golden_dt: &str) -> Result<(), Error> {
         .arg("/chosen/linux,initrd-start")
         .arg("--ignore-path-value")
         .arg("/chosen/linux,initrd-end")
-        .arg("--ignore-path-value")
-        .arg("/avf/secretkeeper_public_key")
         .arg("--ignore-path")
-        .arg("/avf/name")
-        .output()
-        .context("failed to execute dtcompare")?;
+        .arg("/avf/name");
+    let board_first_api_lvl = board_first_api_level();
+    // If chipset qualifies for vendor freeze && vendor api level >= 202504
+    // or chipset does not qualify for vendor freeze && build sdk >= 36
+    if board_first_api_lvl.is_ok() && vsr_api_level()? >= 202504
+        || board_first_api_lvl.is_err() && build_version_sdk() >= 36
+    {
+        dtcompare_cmd.arg("--ignore-path-value").arg("/avf/secretkeeper_public_key");
+    } else {
+        dtcompare_cmd
+            .arg("--ignore-path")
+            .arg("/avf/secretkeeper_public_key")
+            .arg("--ignore-path")
+            .arg("/avf/untrusted/defer-rollback-protection");
+    }
+    let dtcompare_res = dtcompare_cmd.output().context("failed to execute dtcompare")?;
     if !dtcompare_res.status.success() {
         if !Command::new("./dtc_static")
             .arg("-I")
@@ -202,7 +215,25 @@ fn run_test(protected: bool, golden_dt: &str) -> Result<(), Error> {
     Ok(())
 }
 
-fn open_payload(path: &str) -> Result<ParcelFileDescriptor, Error> {
+fn open_payload(path: &str) -> Result<ParcelFileDescriptor> {
     let file = File::open(path).with_context(|| format!("Failed to open VM image {path}"))?;
     Ok(ParcelFileDescriptor::new(file))
+}
+
+fn board_first_api_level() -> Result<i32> {
+    get_sysprop_i32("ro.board.first_api_level")
+}
+
+fn vsr_api_level() -> Result<i32> {
+    get_sysprop_i32("ro.vendor.api_level")
+}
+
+fn build_version_sdk() -> Result<i32> {
+    get_sysprop_i32("ro.build.version.sdk")
+}
+
+fn get_sysprop_i32(prop: &str) -> Result<i32> {
+    let res = rustutils::system_properties::read(prop)?;
+    res.map(|val| val.parse::<i32>().with_context(|| format!("Failed to read {prop}")))
+        .unwrap_or(Ok(-1))
 }
