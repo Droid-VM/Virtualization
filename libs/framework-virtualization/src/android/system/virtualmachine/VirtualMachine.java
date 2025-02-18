9d72823005f275de7392e385530ad2450a644cc9
Shraddha Basantwani <basantwani@google.com>
2025-02-17 20:10:57 -0800

    Add support for enctypted storage expansion
    Bug: 381067202
    Test: atest com.android.microdroid.test.MicrodroidTests#encryptedStorageExpansionIsPersistent
    Change-Id: I6f5737ee601e7c511bdd316b180bf50e3d102ab1

--- libs/framework-virtualization/src/android/system/virtualmachine/VirtualMachine.java
+++ libs/framework-virtualization/src/android/system/virtualmachine/VirtualMachine.java
@@ -1575,6 +1575,12 @@ public class VirtualMachine implements AutoCloseable {
                                 ? createVirtualMachineConfigForRawFrom(vmConfig)
                                 : createVirtualMachineConfigForAppFrom(vmConfig, service);
 
+                if (vmConfig.isEncryptedStorageEnabled()) {
+                    service.maybeExtendEncryptedStorage(
+                        ParcelFileDescriptor.open(mEncryptedStoreFilePath, MODE_READ_WRITE),
+                        vmConfig.getEncryptedStorageBytes());
+                }
+
                 mVirtualMachine =
                         service.createVm(
                                 vmConfigParcel, consoleOutFd, consoleInFd, mLogWriter, null);
@@ -1927,14 +1933,14 @@ public class VirtualMachine implements AutoCloseable {
      * like the number of CPU and size of the RAM, depending on the situation (e.g. the size of the
      * application to run on the virtual machine, etc.)
      *
-     * <p>The new config must be {@linkplain VirtualMachineConfig#isCompatibleWith compatible with}
-     * the existing config.
+     * <p>The new config must be {@linkplain #checkIncrementalCompatibility incremental}
+     * to the existing config.
      *
      * <p>NOTE: This method may block and should not be called on the main thread.
      *
      * @return the old config
      * @throws VirtualMachineException if the virtual machine is not stopped, or the new config is
-     *     incompatible.
+     *     not incremental.
      * @hide
      */
     @SystemApi
@@ -1944,8 +1950,8 @@ public class VirtualMachine implements AutoCloseable {
             throws VirtualMachineException {
         synchronized (mLock) {
             VirtualMachineConfig oldConfig = mConfig;
-            if (!oldConfig.isCompatibleWith(newConfig)) {
-                throw new VirtualMachineException("incompatible config");
+            if (!checkIncrementalCompatibility(oldConfig, newConfig)) {
+                throw new VirtualMachineException("Must be incremental config update");
             }
             checkStopped();
 
@@ -2245,6 +2251,23 @@ public class VirtualMachine implements AutoCloseable {
         } catch (IOException e) {
             throw new VirtualMachineException("failed to transfer encryptedstore image", e);
         }
+    }
+
+    /** Returns true if the new config is incrementally compatible to the existing config. */
+    private boolean checkIncrementalCompatibility(@NonNull VirtualMachineConfig existingConfig, @NonNull VirtualMachineConfig newConfig) {
+        if (existingConfig == newConfig) {
+            return true;
+        }
+        return existingConfig.getDebugLevel() == newConfig.getDebugLevel()
+                && existingConfig.isProtectedVm() == newConfig.isProtectedVm()
+                && existingConfig.getEncryptedStorageBytes() <= newConfig.getEncryptedStorageBytes()
+                && existingConfig.isVmOutputCaptured() == newConfig.isVmOutputCaptured()
+                && existingConfig.isVmConsoleInputSupported() == newConfig.isVmConsoleInputSupported()
+                && existingConfig.isConnectVmConsole() == newConfig.isConnectVmConsole()
+                && existingConfig.getPayloadConfigPath() == newConfig.getPayloadConfigPath()
+                && existingConfig.getPayloadBinaryName() == newConfig.getPayloadBinaryName()
+                && existingConfig.getOs() == newConfig.getOs()
+                && existingConfig.getExtraApks() == newConfig.getExtraApks();
     }
 
     /** Map the raw AIDL (& binder) callbacks to what the client expects. */
