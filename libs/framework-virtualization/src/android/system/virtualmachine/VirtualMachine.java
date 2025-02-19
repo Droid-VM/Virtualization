60eb8655129a0f62f5e0086833ad2b83b19bb4b8
Shraddha Basantwani <basantwani@google.com>
2025-02-19 08:57:50 -0800

    Add support for encrypted storage expansion
    Capability to configure the encrypted storage size
    Partition resizing to the required size upon boot
    New unit tests to validate this functionality
    
    Bug: 381067202
    Test: atest MicrodroidTests
    Change-Id: I6f5737ee601e7c511bdd316b180bf50e3d102ab1

--- libs/framework-virtualization/src/android/system/virtualmachine/VirtualMachine.java
+++ libs/framework-virtualization/src/android/system/virtualmachine/VirtualMachine.java
@@ -1575,6 +1575,12 @@ public class VirtualMachine implements AutoCloseable {
                                 ? createVirtualMachineConfigForRawFrom(vmConfig)
                                 : createVirtualMachineConfigForAppFrom(vmConfig, service);
 
+                if (vmConfig.isEncryptedStorageEnabled()) {
+                    service.extendEncryptedStorage(
+                        ParcelFileDescriptor.open(mEncryptedStoreFilePath, MODE_READ_WRITE),
+                        vmConfig.getEncryptedStorageBytes());
+                }
+
                 mVirtualMachine =
                         service.createVm(
                                 vmConfigParcel, consoleOutFd, consoleInFd, mLogWriter, null);
@@ -1928,9 +1934,10 @@ public class VirtualMachine implements AutoCloseable {
      * application to run on the virtual machine, etc.)
      *
      * <p>The new config must be {@linkplain VirtualMachineConfig#isCompatibleWith compatible with}
-     * the existing config.
+     * or incremental to the existing config.
      *
-     * <p>NOTE: This method may block and should not be called on the main thread.
+     * <p>NOTE: Changing the size of the encrypted storage is an irreversible operation.
+     * This method may block and should not be called on the main thread.
      *
      * @return the old config
      * @throws VirtualMachineException if the virtual machine is not stopped, or the new config is
@@ -1944,7 +1951,8 @@ public class VirtualMachine implements AutoCloseable {
             throws VirtualMachineException {
         synchronized (mLock) {
             VirtualMachineConfig oldConfig = mConfig;
-            if (!oldConfig.isCompatibleWith(newConfig)) {
+            if (!oldConfig.isCompatibleWith(newConfig)
+                    && !checkIncrementalCompatibility(oldConfig, newConfig)) {
                 throw new VirtualMachineException("incompatible config");
             }
             checkStopped();
@@ -2245,6 +2253,25 @@ public class VirtualMachine implements AutoCloseable {
         } catch (IOException e) {
             throw new VirtualMachineException("failed to transfer encryptedstore image", e);
         }
+    }
+
+    /** Returns true if the new config is incrementally compatible to the existing config. */
+    private boolean checkIncrementalCompatibility(
+        @NonNull VirtualMachineConfig oldConfig,
+        @NonNull VirtualMachineConfig newConfig) {
+        if (oldConfig == newConfig) {
+            return true;
+        }
+        return oldConfig.getDebugLevel() == newConfig.getDebugLevel()
+                && oldConfig.isProtectedVm() == newConfig.isProtectedVm()
+                && oldConfig.getEncryptedStorageBytes() <= newConfig.getEncryptedStorageBytes()
+                && oldConfig.isVmOutputCaptured() == newConfig.isVmOutputCaptured()
+                && oldConfig.isVmConsoleInputSupported() == newConfig.isVmConsoleInputSupported()
+                && oldConfig.isConnectVmConsole() == newConfig.isConnectVmConsole()
+                && oldConfig.getPayloadConfigPath() == newConfig.getPayloadConfigPath()
+                && oldConfig.getPayloadBinaryName() == newConfig.getPayloadBinaryName()
+                && oldConfig.getOs() == newConfig.getOs()
+                && oldConfig.getExtraApks() == newConfig.getExtraApks();
     }
 
     /** Map the raw AIDL (& binder) callbacks to what the client expects. */
