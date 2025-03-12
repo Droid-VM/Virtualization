5eb6263673db402c9b477417fc31634b6c933f26
Jiyong Park <jiyong@google.com>
2025-03-12 23:59:12 +0900

    Give longer boot timeout when running in CF or goldfish
    Also, increase the normal boot timeout from 20s to 30s to match with the
    test.
    
    Bug: 402727622
    Test: run TerminalAppTests on CF
    Change-Id: I6c89b9bd1fb0d2e2c269ff4239e8aeff77e51d44

--- android/TerminalApp/java/com/android/virtualization/terminal/VmLauncherService.kt
+++ android/TerminalApp/java/com/android/virtualization/terminal/VmLauncherService.kt
@@ -31,6 +31,7 @@ import android.os.Looper
 import android.os.Parcel
 import android.os.Parcelable
 import android.os.ResultReceiver
+import android.os.SystemProperties
 import android.os.Trace
 import android.system.virtualmachine.VirtualMachine
 import android.system.virtualmachine.VirtualMachineCustomImageConfig
@@ -293,7 +294,7 @@ class VmLauncherService : Service() {
             },
         )
 
-        resolvedInfo.orTimeout(VM_BOOT_TIMEOUT_SECONDS.toLong(), TimeUnit.SECONDS)
+        resolvedInfo.orTimeout(bootTimeoutSeconds().toLong(), TimeUnit.SECONDS)
         return resolvedInfo
     }
 
@@ -479,8 +480,6 @@ class VmLauncherService : Service() {
         private const val KEY_TERMINAL_IPADDRESS = "address"
         private const val KEY_TERMINAL_PORT = "port"
 
-        private const val VM_BOOT_TIMEOUT_SECONDS = 20
-
         private const val INITIAL_MEM_BALLOON_PERCENT = 10
         private const val MAX_MEM_BALLOON_PERCENT = 50
         private const val MEM_BALLOON_INFLATE_INTERVAL_MILLIS = 60000L
@@ -488,6 +487,18 @@ class VmLauncherService : Service() {
 
         private fun getMyIntent(context: Context): Intent {
             return Intent(context.getApplicationContext(), VmLauncherService::class.java)
+        }
+
+        private fun bootTimeoutSeconds(): Int {
+            val deviceName = SystemProperties.get("ro.product.vendor.device", "")
+            val cuttlefish = deviceName.startsWith("vsoc_")
+            val goldfish = deviceName.startsWith("emu64")
+
+            if (cuttlefish || goldfish) {
+                return 3 * 60
+            } else {
+                return 30
+            }
         }
 
         fun run(
