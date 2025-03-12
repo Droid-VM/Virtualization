47a13eeaa952da87dfaed178694047c00e36949f
Keiichi Watanabe <keiichiw@google.com>
2025-03-12 23:47:05 +0900

    TerminalApp: Delete disk resizing feature from UI
    Bug: 382174138
    Test: run VM
    Change-Id: I480d841166c4185ee44a9a6a5ba37b5adce5a197

--- android/TerminalApp/java/com/android/virtualization/terminal/SplitInitializer.kt
+++ android/TerminalApp/java/com/android/virtualization/terminal/SplitInitializer.kt
@@ -15,19 +15,47 @@
  */
 package com.android.virtualization.terminal
 
+import android.content.ComponentName
 import android.content.Context
 import androidx.startup.Initializer
 import androidx.window.embedding.RuleController
+import androidx.window.embedding.SplitPairRule
+import com.android.system.virtualmachine.flags.Flags
 
 class SplitInitializer : Initializer<RuleController> {
 
     override fun create(context: Context): RuleController {
-        return RuleController.getInstance(context).apply {
-            setRules(RuleController.parseRules(context, R.xml.main_split_config))
+        val ruleController = RuleController.getInstance(context)
+        val rules = RuleController.parseRules(context, R.xml.main_split_config).toMutableSet()
+
+        // Remove SettingsDiskResizeActivity if storage ballooning is enabled.
+        if (Flags.terminalStorageBalloon()) {
+            val iterator = rules.iterator()
+            while (iterator.hasNext()) {
+                val rule = iterator.next()
+                if (rule is SplitPairRule) {
+                    val filters = rule.filters
+                    val filterToRemove =
+                        filters.firstOrNull { filter ->
+                            filter.secondaryActivityName ==
+                                ComponentName(context, SETTINGS_DISK_RESIZE_ACTIVITY_CLASS_NAME)
+                        }
+                    if (filterToRemove != null) {
+                        iterator.remove()
+                    }
+                }
+            }
         }
+        ruleController.setRules(rules)
+        return ruleController
     }
 
     override fun dependencies(): List<Class<out Initializer<*>>> {
         return emptyList()
+    }
+
+    companion object {
+        private const val SETTINGS_DISK_RESIZE_ACTIVITY_CLASS_NAME =
+            "com.android.virtualization.terminal.SettingsDiskResizeActivity"
     }
 }
