324cec6ecdfba429a48b191cba61f91a5437fc8e
Keiichi Watanabe <keiichiw@google.com>
2025-03-14 22:21:18 +0900

    Terminal: Resize sparse disk when storage balloon is turned off
    When the storage balloon feature is enabled via an aconfig flag,
    the rootfs is converted to a sparse file, increasing its apparent
    size. When the flag is disabled again afterwards, the disk needs
    to be converted back to non-sparse. Otherwise, the disk resize
    settings screen would display its apparent size which is too big.
    
    This CL addresses this issue by shrinking the disk size with
    resize2fs if the rootfs is a sparse file while the storage balloon
    feature is disabled
    
    Bug: 382174138
    Test: enable 'terminal_storage_balloon' and disable it. Check disk
          size setting.
    
    Change-Id: Ifadaa709b8f74dc99b68c9088d6a4fda06805a98

--- android/TerminalApp/java/com/android/virtualization/terminal/MainActivity.kt
+++ android/TerminalApp/java/com/android/virtualization/terminal/MainActivity.kt
@@ -52,6 +52,7 @@ import com.android.microdroid.test.common.DeviceProperties
 import com.android.system.virtualmachine.flags.Flags
 import com.android.virtualization.terminal.ErrorActivity.Companion.start
 import com.android.virtualization.terminal.InstalledImage.Companion.getDefault
+import com.android.virtualization.terminal.InstalledImage.Companion.roundUp
 import com.android.virtualization.terminal.VmLauncherService.Companion.run
 import com.android.virtualization.terminal.VmLauncherService.Companion.stop
 import com.android.virtualization.terminal.VmLauncherService.VmLauncherServiceCallback
@@ -375,25 +376,53 @@ public class MainActivity :
         return bootCompleted.block(timeoutMillis)
     }
 
+    private fun calculateSparseDiskSize(): Long {
+        // With storage ballooning enabledd, we create a sparse file with 95% of the total size.
+        val statFs = StatFs(filesDir.absolutePath)
+        val hostSize = statFs.totalBytes
+        return roundUp(hostSize * 95 / 100)
+    }
+
+    // Resize the rootfs disk as a non-sparse file.
     private fun resizeDiskIfNecessary(image: InstalledImage) {
         try {
+            val curApparentSize = image.getApparentSize()
+            val curPhysicalSize = image.getPhysicalSize()
+            Log.d(TAG, "Current disk size: apparent=$curApparentSize, physical=$curPhysicalSize")
+
+            // If storage ballooning was enabled via Flags.terminalStorageBalloon() before but it's
+            // now disabled, the disk is still a sparse file whose apparent size is too large.
+            // We need to shrink it to the minimum size.
+            //
+            // The disk file is considered sparse if its apparent disk size matches the expected
+            // sparse disk size.
+            // In addition, we consider it sparse if the physical size is 90% or
+            // more less of its apparent size. This additional condition is a fallback for cases
+            // where the logic of calculating the expected sparse disk size since the disk is
+            // created.
+            if (
+                curApparentSize == calculateSparseDiskSize() ||
+                    curPhysicalSize < curApparentSize * 90 / 100
+            ) {
+                Log.d(TAG, "A sparse disk is detected. Shrink it to the minimum size.")
+                val newSize = image.shrinkToMinimumSize()
+                Log.d(TAG, "Shrink the disk image: $curApparentSize -> $newSize")
+            }
+
             // TODO(b/382190982): Show snackbar message instead when it's recoverable.
-            image.resize(intent.getLongExtra(KEY_DISK_SIZE, image.getSize()))
+            image.resize(intent.getLongExtra(KEY_DISK_SIZE, image.getApparentSize()))
         } catch (e: IOException) {
             start(this, Exception("Failed to resize disk", e))
             return
         }
     }
 
+    // Resize the rootfs disk as a sparse file.
     private fun truncateDiskIfNecessary(image: InstalledImage) {
-        val curSize = image.getSize()
+        val curSize = image.getApparentSize()
         val physicalSize = image.getPhysicalSize()
 
-        // Change the rootfs disk's apparent size to 95% of the total disk size.
-        // Note that the physical size is not changed.
-        val statFs = StatFs(filesDir.absolutePath)
-        val hostSize = statFs.totalBytes
-        val expectedSize = hostSize * 95 / 100
+        val expectedSize = calculateSparseDiskSize()
         Log.d(
             TAG,
             "rootfs apparent size=$curSize, physical size=$physicalSize, expectedSize=$expectedSize",
